6e460360b2c82bd77162f473fdf6a9620e37f801
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2007-05-27 17:24:52 +0000

    initial EM1010 support
    git-svn-id: https://svn.fhem.de/fhem/trunk@64 2b470e98-0d58-463d-a4d8-8e2adae1ed80

---
+++ fhem/FHEM/60_EM.pm
@@ -0,0 +1,292 @@
+##############################################
+package main;
+
+use strict;
+use warnings;
+use Device::SerialPort;
+
+sub EM_Write($$);
+sub EmCrc($$);
+sub EmCrcCheck($$);
+sub EmEsc($);
+sub EmGetData($$);
+sub EmMakeMsg($);
+
+#####################################
+sub
+EM_Initialize($)
+{
+  my ($hash) = @_;
+
+
+# Provider
+  $hash->{WriteFn} = "EM_Write";
+  $hash->{Clients} = ":EMWZ:";
+
+# Consumer
+  $hash->{DefFn}   = "EM_Define";
+  $hash->{UndefFn} = "EM_Undef";
+  $hash->{GetFn}   = "EM_Get";
+  $hash->{SetFn}   = "EM_Set";
+  $hash->{AttrList}= "model:em1010pc loglevel:0,1,2,3,4,5,6";
+}
+
+#####################################
+sub
+EM_Define($$)
+{
+  my ($hash, $def) = @_;
+  my @a = split("[ \t][ \t]*", $def);
+
+  $hash->{STATE} = "Initialized";
+
+  delete $hash->{PortObj};
+  delete $hash->{FD};
+
+  my $dev = $a[2];
+  $attr{$a[0]}{savefirst} = 1;
+
+  if($dev eq "none") {
+    Log 1, "EM device is none, commands will be echoed only";
+    return undef;
+  }
+
+  Log 3, "EM opening device $dev";
+  my $po = new Device::SerialPort ($dev);
+  return "Can't open $dev: $!\n" if(!$po);
+  Log 3, "EM opened device $dev";
+  $po->close();
+
+  $hash->{DeviceName} = $dev;
+  return undef;
+}
+
+#####################################
+sub
+EM_Undef($$)
+{
+  my ($hash, $arg) = @_;
+  my $name = $hash->{NAME};
+
+  foreach my $d (sort keys %defs) {
+    if(defined($defs{$d}) &&
+       defined($defs{$d}{IODev}) &&
+       $defs{$d}{IODev} == $hash)
+      {
+        Log GetLogLevel($name,2), "deleting port for $d";
+        delete $defs{$d}{IODev};
+      }
+  }
+  return undef;
+}
+
+
+#####################################
+sub
+EM_Set($@)
+{
+  return undef;
+}
+
+#########################
+sub
+b($$)
+{
+  my ($t,$p) = @_;
+  return -1 if(length($t) < $p);
+  return ord(substr($t,$p,1));
+}
+
+sub
+w($$)
+{
+  my ($t,$p) = @_;
+  return b($t,$p+1)*256 + b($t,$p);
+}
+
+sub
+dw($$)
+{
+  my ($t,$p) = @_;
+  return w($t,$p+2)*65536 + w($t,$p);
+}
+
+#####################################
+sub
+EM_Get($@)
+{
+  my ($hash, @a) = @_;
+
+  return "\"get EM\" needs only one parameter" if(@a != 2);
+
+  my $v;
+  if($a[1] eq "time") {
+
+    my $d = EmGetData($hash->{DeviceName}, "74");
+    $v = sprintf "%4d-%02d-%02d %02d:%02d:%02d",
+              b($d,5)+2006, b($d,4), b($d,3),
+              b($d,0), b($d,1), b($d,2);
+
+  } elsif($a[1] eq "version") {
+
+    my $d = EmGetData($hash->{DeviceName},"76");
+    $v = sprintf "%d.%d", b($d,0), b($d,1);
+
+  } else {
+    return "Unknown argument $a[1], choose one of time,version";
+  }
+
+  $hash->{READINGS}{$a[1]}{VAL} = $v;
+  $hash->{READINGS}{$a[1]}{TIME} = TimeNow();
+
+  return "$a[0] $a[1] => $v";
+}
+
+#####################################
+sub
+EM_Write($$)
+{
+  my ($hash,$msg) = @_;
+
+  return EmGetData($hash->{DeviceName}, $msg);
+}
+
+#####################################
+sub
+EmCrc($$)
+{
+  my ($in, $val) = @_;
+  my ($crc, $bits) = (0, 8);
+  my $k = (($in >> 8) ^ $val) << 8;
+  while($bits--) {
+    if(($crc ^ $k) & 0x8000) {
+      $crc = ($crc << 1) ^ 0x8005;
+    } else {
+      $crc <<= 1;
+    }
+    $k <<= 1;
+  }
+  return (($in << 8) ^ $crc) & 0xffff;
+}
+
+#########################
+sub
+EmEsc($)
+{
+  my ($b) = @_;
+
+  my $out = "";
+  $out .= chr(0x10) if($b==0x02 || $b==0x03 || $b==0x10);
+  $out .= chr($b);
+}
+
+
+#####################################
+sub
+EmCrcCheck($$)
+{
+  my ($otxt, $len) = @_;
+  my $crc = 0x8c27;
+  for(my $l = 2; $l < $len+4; $l++) {
+    my $b = ord(substr($otxt,$l,1));
+    $crc = EmCrc($crc, 0x10) if($b==0x02 || $b==0x03 || $b==0x10);
+    $crc = EmCrc($crc, $b);
+  }
+  return ($crc == w($otxt, $len+4));
+}
+
+#########################
+sub
+EmMakeMsg($)
+{
+  my ($data) = @_;
+  my $len = length($data);
+  $data = chr($len&0xff) . chr(int($len/256)) . $data;
+
+  my $out = pack('H*', "0200");
+  my $crc = 0x8c27;
+  for(my $l = 0; $l < $len+2; $l++) {
+    my $b = ord(substr($data,$l,1));
+    $crc = EmCrc($crc, 0x10) if($b==0x02 || $b==0x03 || $b==0x10);
+    $crc = EmCrc($crc, $b);
+    $out .= EmEsc($b);
+  }
+  $out .= EmEsc($crc&0xff);
+  $out .= EmEsc($crc/256);
+  $out .= chr(0x03);
+  return $out;
+}
+
+#####################################
+# This is the only 
+sub
+EmGetData($$)
+{
+  my ($dev, $d) = @_;
+  $d = EmMakeMsg(pack('H*', $d));
+
+  my $serport = new Device::SerialPort ($dev);
+  die "Can't open $dev: $!\n" if(!$serport);
+  $serport->reset_error();
+  $serport->baudrate(38400);
+  $serport->databits(8);
+  $serport->parity('none');
+  $serport->stopbits(1);
+  $serport->handshake('none');
+
+  Log 5, "EM: Sending " . unpack('H*', $d) . "\n";
+
+  my $rm = "EM timeout reading the answer";
+  for(my $rep = 0; $rep < 3; $rep++) {
+
+    $serport->write($d);
+
+    my $retval = "";
+    my $esc = 0;
+    my $started = 0;
+    my $complete = 0;
+    for(;;) {
+      my ($rout, $rin) = ('', '');
+      vec($rin, $serport->FILENO, 1) = 1;
+      my $nfound = select($rout=$rin, undef, undef, 1.0);
+
+      if($nfound < 0) {
+        $rm = "EM Select error $nfound / $!";
+        goto DONE;
+      }
+      last if($nfound == 0);
+
+      my $buf = $serport->input();
+      if(!defined($buf) || length($buf) == 0) {
+        $rm = "EM EOF on $dev";
+        goto DONE;
+      }
+
+      for(my $i = 0; $i < length($buf); $i++) {
+        my $b = ord(substr($buf,$i,1));
+
+        if(!$started && $b != 0x02) { next; }
+        $started = 1;
+        if($esc) { $retval .= chr($b); $esc = 0; next; }
+        if($b == 0x10) { $esc = 1; next; }
+        $retval .= chr($b);
+        if($b == 0x03) { $complete = 1; last; }
+      }
+
+      if($complete) {
+        my $l = length($retval);
+        if($l < 8)                    { $rm = "EM Msg too short";   goto DONE; }
+        if(b($retval,1) != 0)         { $rm = "EM Bad second byte"; goto DONE; }
+        if(w($retval,2) != $l-7)      { $rm = "EM Length mismatch"; goto DONE; }
+        if(!EmCrcCheck($retval,$l-7)) { $rm = "EM Bad CRC";         goto DONE; }
+        return substr($retval, 4, $l-7);
+      }
+    }
+  }
+
+DONE:
+  $serport->close();
+  return undef;
+}
+
+1;
---
+++ fhem/FHEM/61_EMWZ.pm
@@ -0,0 +1,165 @@
+##############################################
+package main;
+
+use strict;
+use warnings;
+use Time::HiRes qw(gettimeofday);
+
+sub EMWZ_Get($@);
+sub EMWZ_Set($@);
+sub EMWZ_Define($$);
+sub EMWZ_GetStatus($);
+
+###################################
+sub
+EMWZ_Initialize($)
+{
+  my ($hash) = @_;
+
+  $hash->{GetFn}     = "EMWZ_Get";
+  $hash->{SetFn}     = "EMWZ_Set";
+  $hash->{DefFn}     = "EMWZ_Define";
+
+  $hash->{AttrList}  = "dummy:1,0 model;EM1000WZ loglevel:0,1,2,3,4,5,6";
+}
+
+
+###################################
+sub
+EMWZ_GetStatus($)
+{
+  my ($hash) = @_;
+
+  if(!$hash->{LOCAL}) {
+    InternalTimer(gettimeofday()+300, "EMWZ_GetStatus", $hash);
+  }
+
+  my $dnr = $hash->{DEVNR};
+  my $d = IOWrite($hash, sprintf("7a%02x", $dnr-1));
+
+  my $name = $hash->{NAME};
+
+  if($d eq ((pack('H*',"00") x 45) . pack('H*',"FF") x 6)) {
+    my $msg = "EMWZ no device no. $dnr present";
+    Log GetLogLevel($name,2), $msg;
+    return $msg;
+  }
+
+  my $pulses=w($d,13);
+  my $ec=w($d,49) / 10;
+  if($ec <= 0) {
+    my $msg = "EMWZ read error";
+    Log GetLogLevel($name,2), $msg;
+    return $msg;
+  }
+  my $cur_energy = $pulses / $ec;       # ec = U/kWh
+  my $cur_power = $cur_energy / 5 * 60; # 5minute interval scaled to 1h
+
+  if($cur_power > 100) {
+    my $msg = "EMWZ Bogus reading: curr. power is reported to be $cur_power";
+    Log GetLogLevel($name,2), $msg;
+    return $msg;
+  }
+
+  my %vals;
+  $vals{"5min_pulses"} = $pulses;
+  $vals{"energy"} = sprintf("%0.3f", $cur_energy);
+  $vals{"power"} = sprintf("%.3f", $cur_power);
+  $vals{"alarm_PA"} = w($d,45) . " Watt";
+  $vals{"price_CF"} = sprintf("%.3f", w($d,47)/10000);
+  $vals{"RperKW_EC"} = $ec;
+
+  my $tn = TimeNow();
+  my $idx = 0;
+  foreach my $k (keys %vals) {
+    my $v = $vals{$k};
+    $hash->{CHANGED}[$idx++] = "$k: $v";
+    $hash->{READINGS}{$k}{TIME} = $tn;
+    $hash->{READINGS}{$k}{VAL} = $v
+  }
+
+  if(!$hash->{LOCAL}) {
+    DoTrigger($name, undef) if($init_done);
+  }
+
+  $hash->{STATE} = "$cur_power kWh";
+
+  return $hash->{STATE};
+}
+
+###################################
+sub
+EMWZ_Get($@)
+{
+  my ($hash, @a) = @_;
+
+  return "argument is missing" if(int(@a) != 2);
+
+  my $d = $hash->{DEVNR};
+  my $msg;
+
+  if($a[1] ne "status") {
+    return "unknown get value, valid is status";
+  }
+  $hash->{LOCAL} = 1;
+  my $v = EMWZ_GetStatus($hash);
+  delete $hash->{LOCAL};
+
+  return "$a[0] $a[1] => $v";
+}
+
+sub
+EMWZ_Set($@)
+{
+  my ($hash, @a) = @_;
+
+  return "argument is missing" if(int(@a) != 3);
+
+  my $v = $a[2];
+  my $d = $hash->{DEVNR};
+  my $msg;
+
+  if($a[1] eq "price") {
+    $v *= 10000; # Make display and input the same
+    $msg = sprintf("79%02x2f02%02x%02x", $d-1, $v%256, int($v/256));
+  } elsif($a[1] eq "alarm") {
+    $msg = sprintf("79%02x2d02%02x%02x", $d-1, $v%256, int($v/256));
+  } elsif($a[1] eq "rperkw") {
+    $v *= 10; # Make display and input the same
+    $msg = sprintf("79%02x3102%02x%02x", $d-1, $v%256, int($v/256));
+  } else {
+    return "unknown set value, use one of price,alarm,rperkw";
+  }
+
+  my $ret = IOWrite($hash, $msg);
+  if(ord(substr($ret,0,1)) != 6) {
+    $ret = "EMWZ Error occured: " .  unpack('H*', $ret);
+    Log GetLogLevel($hash->{NAME},2), $ret;
+    return $ret;
+  }
+
+  return undef;
+}
+
+#############################
+sub
+EMWZ_Define($$)
+{
+  my ($hash, $def) = @_;
+  my @a = split("[ \t][ \t]*", $def);
+
+  return "syntax: define <name> EMWZ devicenumber"
+    if(@a != 3 || $a[2] !~ m,^[1-4]$,);
+  $hash->{DEVNR} = $a[2];
+  AssignIoPort($hash);
+
+
+  # InternalTimer blocks if init_done is not true
+  my $oid = $init_done;
+  $init_done = 1;
+  EMWZ_GetStatus($hash);
+  $init_done = $oid;
+  return undef;
+}
+
+1;
