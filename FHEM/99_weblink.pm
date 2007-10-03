a403ebd3ba86f255052b601e1d20e0ecbbdc5e5d
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2007-10-03 16:35:37 +0000

    fhemweb.pl tuning
    git-svn-id: https://fhem.svn.sourceforge.net/svnroot/fhem/trunk/fhem@92 2b470e98-0d58-463d-a4d8-8e2adae1ed80

---
+++ FHEM/99_weblink.pm
@@ -0,0 +1,34 @@
+##############################################
+package main;
+
+use strict;
+use warnings;
+
+#####################################
+sub
+weblink_Initialize($)
+{
+  my ($hash) = @_;
+
+  $hash->{DefFn} = "weblink_Define";
+}
+
+
+#####################################
+sub
+weblink_Define($$)
+{
+  my ($hash, $def) = @_;
+  my ($type, $name, $wltype, $link) = split("[ \t]+", $def, 4);
+  my %thash = ( link=>1, fileplot=>1 );
+  
+  if(!$link || !$thash{$wltype}) {
+    return "Usage: define <name> weblink [" . join("|",sort keys %thash) . "] <httplink>";
+  }
+  $hash->{WLTYPE} = $wltype;
+  $hash->{LINK} = $link;
+  $hash->{STATE} = "initial";
+  return undef;
+}
+
+1;
