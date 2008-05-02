e2e4ad5ca8ae23b6fd485ad6589595c60bdf9c74
neubert <neubert@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2008-05-02 11:53:42 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@165 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/dblog/fhemdb_get.pl
+++ fhem/contrib/dblog/fhemdb_get.pl
@@ -42,9 +42,9 @@
 #
 # global configuration
 #
-my $dbconn      = "mysql:database=fhem;host=localhost;port=3306";
-my $dbuser      = "root";
-my $dbpassword  = "britta";
+my $dbconn      = "mysql:database=fhem;host=db;port=3306";
+my $dbuser      = "fhemuser";
+my $dbpassword  = "fhempassword";
 
 #
 # nothing to change below this line
