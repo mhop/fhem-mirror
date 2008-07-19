673974523cd1c018a6e4f8c44f959f693d343cce
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2008-07-19 11:16:24 +0000

    min/max/abstime2rel added
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@211 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/99_Utils.pm
+++ fhem/FHEM/99_Utils.pm
@@ -17,4 +17,38 @@ time_str2num($)
   return mktime($a[5],$a[4],$a[3],$a[2],$a[1]-1,$a[0]-1900,0,0,-1);
 }
 
+sub
+min($$)
+{
+  my ($a,$b) = @_;
+  return $a if($a lt $b);
+  return $b;
+}
+
+sub
+max($$)
+{
+  my ($a,$b) = @_;
+  return $a if($a gt $b);
+  return $b;
+}
+
+sub
+abstime2rel($)
+{
+  my ($h,$m,$s) = split(":", shift);
+  $m = 0 if(!$m);
+  $s = 0 if(!$s);
+  my $t1 = 3600*$h+60*$m+$s;
+
+  my @now = localtime;
+  my $t2 = 3600*$now[2]+60*$now[1]+$now[0];
+  my $diff = $t1-$t2;
+  $diff += 86400 if($diff < 0);
+
+  return sprintf("%02d:%02d:%02d", $diff/3600, ($diff/60)%60, $diff%60);
+}
+
+
+
 1;
