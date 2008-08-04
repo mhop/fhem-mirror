aa1a583bd784ca6138f584427c758fdfdd726242
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2008-08-04 13:50:02 +0000

    HMS100-FIT added
    git-svn-id: https://fhem.svn.sourceforge.net/svnroot/fhem/trunk/fhem@222 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- FHEM/12_HMS.pm
+++ FHEM/12_HMS.pm
@@ -13,6 +13,7 @@ my %codes = (
   "5" => "HMS100TFK",
   "6" => "HMS100MG",
   "8" => "HMS100CO",
+  "e" => "HMS100FIT",
 );
 
 my %defptr;
@@ -30,14 +31,15 @@ HMS_Initialize($)
 #                        810e04d70213a001b16d000003000000 RM100-2
 #                        810e047f0214a001a81f000001000000 HMS100TFK
 #                        810e048f0295a0010155000001000000 HMS100TFK (jumper)
-#			 810e04330216a001b4c5000001000000 HMS100MG
+#                        810e04330216a001b4c5000001000000 HMS100MG
 #                        810e04210218a00186e0000000000000 HMS100CO
+#                        810e0448029ea00132d5000000000000 FI-Trenner
 
-  $hash->{Match}     = "^810e04....(1|5|9)[0-8]a001";
+  $hash->{Match}     = "^810e04....(1|5|9).a001";
   $hash->{DefFn}     = "HMS_Define";
   $hash->{UndefFn}   = "HMS_Undef";
   $hash->{ParseFn}   = "HMS_Parse";
-  $hash->{AttrList}  = "do_not_notify:0,1 showtime:0,1 model;hms100-t,hms100-tf,hms100-wd,hms100-mg,hms100-tfk,rm100-2,hms100-co loglevel:0,1,2,3,4,5,6";
+  $hash->{AttrList}  = "do_not_notify:0,1 showtime:0,1 model;hms100-t,hms100-tf,hms100-wd,hms100-mg,hms100-tfk,rm100-2,hms100-co,hms100-fit loglevel:0,1,2,3,4,5,6";
 }
 
 #####################################
@@ -195,6 +197,19 @@ HMS_Parse($$)
     $v[1] = "off";
     if ($status & 1) { $v[0] = "on"; }
     $val = "CO Detect: $v[0]";
+
+ } elsif ($type eq "HMS100FIT") {    # By PAN
+
+    @txt = ( "fi_triggered", "battery");
+    @sfx = ( "",             "");
+
+    # Battery-low condition detect is not yet properly
+    # implemented.
+    my $status = hex(substr($val, 1, 1));
+    $v[0] = ($status != "0") ? "on" : "off";
+    $v[1] = "off";
+    if ($status & 1) { $v[0] = "on"; }
+    $val = "FI triggered: $v[0]";
 
   } else {
 
