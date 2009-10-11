524e7a19fa8eb69104d9ee7f6dcd4f26f5513496
neubert <neubert@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2009-10-11 20:34:30 +0000

    finalized 09_BS.pm and documentation
    git-svn-id: https://fhem.svn.sourceforge.net/svnroot/fhem/trunk/fhem@446 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- FHEM/09_BS.pm
+++ FHEM/09_BS.pm
@@ -24,7 +24,7 @@ BS_Initialize($)
   $hash->{DefFn}     = "BS_Define";
   $hash->{UndefFn}   = "BS_Undef";
   $hash->{ParseFn}   = "BS_Parse";
-  $hash->{AttrList}  = "IODev do_not_notify:1,0 showtime:0,1 dummy:1,0 model:BSs loglevel:0,1,2,3,4,5,6";
+  $hash->{AttrList}  = "IODev do_not_notify:1,0 showtime:0,1 dummy:1,0 model:BS loglevel:0,1,2,3,4,5,6";
 
 }
 
@@ -36,20 +36,20 @@ BS_Define($$)
   my ($hash, $def) = @_;
   my @a = split("[ \t][ \t]*", $def);
 
-  my $u= "wrong syntax: define <name> BS <sensor> [factor]";
+  my $u= "wrong syntax: define <name> BS <sensor> [RExt]";
   return $u if((int(@a)< 3) || (int(@a)>4));
 
   my $name	= $a[0];
   my $sensor	= $a[2];
-  if($sensor !~ /[1..9]/) {
+  if($sensor !~ /[123456789]/) {
   	return "erroneous sensor specification $sensor, use one of 1..9";
   }
   $sensor= "0$sensor";
 
-  my $factor	= 1.0;
-  $factor= $a[3] if(int(@a)==4);
+  my $RExt	= 50000; # default is 50kOhm
+  $RExt= $a[3] if(int(@a)==4);
   $hash->{SENSOR}= "$sensor";
-  $hash->{FACTOR}= $factor;
+  $hash->{RExt}= $RExt;
 
   my $dev= "a5cf $sensor";
   $hash->{DEF}= $dev;
@@ -96,10 +96,16 @@ BS_Parse($$)
   my $flags= hex(substr($msg, 24, 1)) & 0xdc;
   my $value= hex(substr($msg, 25, 3)) & 0x3ff;
 
-  my $factor= $def->{FACTOR};
-  my $brightness= $value/10.24*$factor;
+  my $RExt= $def->{RExt};
+  my $brightness= $value/10.24; # Vout in percent of reference voltage 1.1V
 
-  my $state= sprintf("brightness: %.2f flags: %d", $brightness, $flags);
+  # brightness in lux= 100lux*(VOut/RExt/1.8muA)^2;
+  my $VOut= $value*1.1/1024.0;
+  my $temp= $VOut/$RExt/1.8E-6;
+  my $lux= 100.0*$temp*$temp;
+
+  my $state= sprintf("brightness: %.2f  lux: %.0f  flags: %d",
+  	$brightness, $lux, $flags);
 
   $def->{CHANGED}[0] = $state;
   $def->{STATE} = $state;
@@ -109,6 +115,8 @@ BS_Parse($$)
 
   $def->{READINGS}{brightness}{TIME} = $t;
   $def->{READINGS}{brightness}{VAL} = $brightness;
+  $def->{READINGS}{lux}{TIME} = $t;
+  $def->{READINGS}{lux}{VAL} = $lux;
   $def->{READINGS}{flags}{TIME} = $t;
   $def->{READINGS}{flags}{VAL} = $flags;
 
