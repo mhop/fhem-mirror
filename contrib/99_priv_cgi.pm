cd482a4703ed1113735b24547414db752e40050c
parix <parix@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2009-12-08 22:09:12 +0000

    TEST-COMMIT
    git-svn-id: https://fhem.svn.sourceforge.net/svnroot/fhem/trunk/fhem@500 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- contrib/99_priv_cgi.pm
+++ contrib/99_priv_cgi.pm
@@ -6,10 +6,7 @@
 # 08/2009 READINGS -> Übersicht aller READIMGS nach Datum -> READING -> Device
 # 08/2009 Excute FHEMCommands /privcgi?Task=EXEC&cmd=FHEMCOMMAND&dev=DEVICENAME&attr=ATTRIBUTE&val=Value
 ################################################################################
-# Danke an Rudi für die Schnittstelle....
-# Dies Modul als Beispiel für die Nutzung der Schnittstelle...
-# Das Modul verändert nichts an FHEM
-################################################################################
+# TEST CVS *****
 # Beschreibung
 # Es werden lediglich vorhanden Information aus FHEM in eigenen Ansichten/Listen dargestellt.
 #
