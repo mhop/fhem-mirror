2afa6023a13991eb9b164d9e868e10e054211b2e
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2009-12-27 18:12:13 +0000

    FileLog get bug (or speed optimization)
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@524 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/92_FileLog.pm
+++ fhem/FHEM/92_FileLog.pm
@@ -390,7 +390,7 @@ seekTo($$$$)
 
       # If the second line is longer then the first,
       # binary search will never get it: 
-      if($next eq $last && $data ge $ts && $div < 8192) {
+      if($next eq $last && $data ge $ts && $div < 8192 && $next < 1024) {
         $last = 0;
         $div *= 2;
       }
