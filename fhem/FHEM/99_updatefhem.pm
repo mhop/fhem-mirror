621165092675db6bb3413b877093645e6c6ddadf
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2011-07-05 18:04:27 +0000

    CULflash
    git-svn-id: https://svn.fhem.de/fhem/trunk@928 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/99_updatefhem.pm
+++ fhem/FHEM/99_updatefhem.pm
@@ -4,19 +4,25 @@ use warnings;
 use IO::Socket;
 
 sub CommandUpdatefhem($$);
+sub CommandCULflash($$);
 
 my $server = "fhem.de:80";
 my $sdir   = "/fhemupdate";
 my $ftime  = "filetimes.txt";
+my $dfu    = "dfu-programmer";
 
 
 #####################################
 sub
 updatefhem_Initialize($$)
 {
-  my %lhash = ( Fn=>"CommandUpdatefhem",
-                Hlp=>",update fhem from the nightly CVS checkout" );
-  $cmds{updatefhem} = \%lhash;
+  my %fhash = ( Fn=>"CommandUpdatefhem",
+                Hlp=>",update fhem from the nightly CVS" );
+  $cmds{updatefhem} = \%fhash;
+
+  my %chash = ( Fn=>"CommandCULflash",
+                Hlp=>"<cul> <type>,flash the CUL from the nightly CVS" );
+  $cmds{CULflash} = \%chash;
 }
 
 
@@ -27,7 +33,6 @@ CommandUpdatefhem($$)
   my ($cl, $param) = @_;
   my $lt = "";
   my $ret = "";
-
   #my $moddir = "$attr{global}{modpath}/FHEM";
   my $moddir = "XXX";
 
@@ -62,6 +67,7 @@ CommandUpdatefhem($$)
       next if($f ne $param);
     } else {
       next if($oldtime{$f} && $filetime{$f} eq $oldtime{$f});
+      next if($f =~ m/.hex$/);  # skip firmware files
     }
     my $localfile = "$moddir/$f";
     my $remfile = $f;
@@ -99,6 +105,68 @@ CommandUpdatefhem($$)
   }
   
   return $ret;
+}
+
+sub
+CommandCULflash($$)
+{
+  my ($cl, $param) = @_;
+  #my $moddir = "$attr{global}{modpath}/FHEM";
+  my $moddir = "XXX";
+
+  my %ctypes = (
+    CUL_V2     => "at90usb162",
+    CUL_V2_HM  => "at90usb162",
+    CUL_V3     => "atmega32u4",
+    CUL_V4     => "atmega32u2",
+  );
+  my @a = split("[ \t]+", $param);
+  return "Usage: CULflash <Fhem-CUL-Device> <CUL-type>, ".
+                "where <CUL-type> is one of ". join(" ", sort keys %ctypes)
+      if(int(@a) != 2 ||
+         !$defs{$a[0]} ||
+         $defs{$a[0]}{TYPE} ne "CUL" ||
+         !$ctypes{$a[1]});
+
+  my $cul  = $a[0];
+  my $target = $a[1];
+
+  ################################
+  # First get the index file to prove the file size
+  my $filetimes = GetHttpFile($server, "$sdir/$ftime");
+  return "Can't get $ftime from $server" if(!$filetimes);
+
+  my (%filetime, %filesize);
+  foreach my $l (split("[\r\n]", $filetimes)) {
+    chomp($l);
+    return "Corrupted filetimes.txt file"
+        if($l !~ m/^20\d\d-\d\d-\d\d_\d\d:\d\d:\d\d /);
+    my ($ts, $fs, $file) = split(" ", $l, 3);
+    $filetime{$file} = $ts;
+    $filesize{$file} = $fs;
+  }
+
+  ################################
+  # Now get the firmware file:
+  my $content = GetHttpFile($server, "$sdir/$target.hex");
+  return "File size for $target.hex does not correspond to filetimes.txt entry"
+          if(length($content) ne $filesize{"$target.hex"});
+  my $localfile = "$moddir/$target.hex";
+  open(FH,">$localfile") || return "Can't write $localfile";
+  print FH $content;
+  close(FH);
+
+  my $cmd = "($dfu MCU erase && $dfu MCU flash TARGET && $dfu MCU start) 2>&1";
+  my $mcu = $ctypes{$target};
+  $cmd =~ s/MCU/$mcu/g;
+  $cmd =~ s/TARGET/$localfile/g;
+
+  CUL_SimpleWrite($defs{CUL}, "B01");
+  sleep(4);     # B01 needs 2 seconds for the reset
+  Log 1, $cmd;
+  my $result = `$cmd`;
+  Log 1, $result;
+  return $result;
 }
 
 sub
