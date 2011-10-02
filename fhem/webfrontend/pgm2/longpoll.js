767c882f51eb9dcdf1be940ef4746f8884c488d4
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2011-10-02 12:26:43 +0000

    longpoll
    git-svn-id: https://svn.fhem.de/fhem/trunk@1039 2b470e98-0d58-463d-a4d8-8e2adae1ed80

---
+++ fhem/webfrontend/pgm2/longpoll.js
@@ -0,0 +1,39 @@
+var pollConn;
+
+function
+cmd(arg)
+{
+  var req = new XMLHttpRequest();
+  req.open("GET", arg, true);
+  req.send(null);
+}
+
+function
+doUpdate()
+{
+  if(pollConn.readyState != 4)
+    return;
+  var lines = pollConn.responseText.split("\n");
+  for(var i=0; i < lines.length; i++) {
+    var d = lines[i].split(";", 3);    // Complete arg
+    if(d.length != 3)
+      continue;
+    var el = document.getElementById(d[0]);
+    if(el)
+      el.innerHTML=d[2];
+  }
+  pollConn.abort();
+  longpoll();
+}
+
+function
+longpoll()
+{
+  pollConn = new XMLHttpRequest();
+  pollConn.open("GET", document.location.pathname+document.location.search+
+                "&XHR=1&inform=1", true);
+  pollConn.onreadystatechange = doUpdate;
+  pollConn.send(null);
+}
+
+window.onload = longpoll;
