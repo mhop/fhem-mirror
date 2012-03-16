71ec7c64dc70caf92662a8596a2e3d1b58a1b3f6
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-03-16 06:50:47 +0000

    Hint from Peter
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@1354 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/00_CUL.pm
+++ fhem/FHEM/00_CUL.pm
@@ -47,7 +47,7 @@ my $clientsSlowRF = ":FS20:FHT:FHT8V:KS300:USF1000:BS:HMS: " .
                     ":CUL_EM:CUL_WS:CUL_FHTTK:CUL_RFR:CUL_HOERMANN: " .
                     ":ESA2000:CUL_IR:CUL_TX";
 
-my $clientsHomeMatic = ":CUL_HM:HMS:";  # OneWire emulated as HMS on a CUNO
+my $clientsHomeMatic = ":CUL_HM:HMS:CUL_IR:";  # OneWire emulated as HMS on a CUNO
 
 my %matchListSlowRF = (
     "1:USF1000"   => "^81..(04|0c)..0101a001a5ceaa00....",
