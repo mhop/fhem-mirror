8ab96feb67ab4ac2649f92d2579d51d824c60f80
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-04-08 08:29:00 +0000

    setList added to dummy
    git-svn-id: https://svn.fhem.de/fhem/trunk@1422 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_dummy.pm
+++ fhem/FHEM/98_dummy.pm
@@ -12,7 +12,7 @@ dummy_Initialize($)
 
   $hash->{SetFn}     = "dummy_Set";
   $hash->{DefFn}     = "dummy_Define";
-  $hash->{AttrList}  = "loglevel:0,1,2,3,4,5,6";
+  $hash->{AttrList}  = "loglevel:0,1,2,3,4,5,6 setList";
 }
 
 ###################################
@@ -20,13 +20,13 @@ sub
 dummy_Set($@)
 {
   my ($hash, @a) = @_;
-
-  return "no set value specified" if(int(@a) < 2);
-  return "Unknown argument $a[1], choose one of *" if($a[1] eq "?");
-
   my $name = shift @a;
-  my $v = join(" ", @a);
 
+  return "no set value specified" if(int(@a) < 1);
+  my $setList = AttrVal($name, "setList", "*");
+  return "Unknown argument ?, choose one of $setList" if($a[0] eq "?");
+
+  my $v = join(" ", @a);
   Log GetLogLevel($name,2), "dummy set $name $v";
 
   $hash->{CHANGED}[0] = $v;
