9b60c79869b2a88e9ec82601d7d77b217d9e6805
wherzig <wherzig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-04-09 15:40:35 +0000

    cleanups
    git-svn-id: https://fhem.svn.sourceforge.net/svnroot/fhem/trunk/fhem@1430 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- FHEM/46_TRX_ELSE.pm
+++ FHEM/46_TRX_ELSE.pm
@@ -1,12 +1,26 @@
 #################################################################################
 # 46_TRX_ELSE.pm
-# Modul for FHEM for unkown RFXTRX messages
 #
-#  This script is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# FHEM module unkown RFXtrx433 messages
 #
-##################################
+# Copyright (C) 2012 Willi Herzig
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# The GNU General Public License may also be found at http://www.gnu.org/licenses/gpl-2.0.html .
+###################################
 #
 # values for "set global verbose"
 # 4: log unknown protocols
