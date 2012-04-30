f6b03092dc28189b4e5449c4443e40721d9a2d54
wherzig <wherzig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-04-30 20:24:11 +0000

    Match fixed. new sensors for TRX-WEATHER. fixed rain sensor
    git-svn-id: https://svn.fhem.de/fhem/trunk@1509 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/46_TRX_LIGHT.pm
+++ fhem/FHEM/46_TRX_LIGHT.pm
@@ -90,8 +90,7 @@ TRX_LIGHT_Initialize($)
     $light_device_c2b{$light_device_codes{$k}->[0]} = $k;
   }
 
-  #$hash->{Match}     = "^\\).*"; # 0x29
-  $hash->{Match}     = "^(\\ |\\)).*"; # 0x20 or 0x29
+  $hash->{Match}     = "^..(10|11|12).*";
   $hash->{SetFn}     = "TRX_LIGHT_Set";
   $hash->{DefFn}     = "TRX_LIGHT_Define";
   $hash->{UndefFn}   = "TRX_LIGHT_Undef";
