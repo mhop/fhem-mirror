bc177d47197d3b834107f047d596d394480ab2a9
mfr69bs <mfr69bs@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-06-25 08:20:00 +0000

    FhemUtils/release.pm for the new update process added.
    git-svn-id: https://fhem.svn.sourceforge.net/svnroot/fhem/trunk/fhem@1651 2b470e98-0d58-463d-a4d8-8e2adae1ed80

---
+++ FHEM/FhemUtils/release.pm
@@ -0,0 +1,17 @@
+################################################################
+# $Id:$
+package main;
+use strict;
+use warnings;
+
+use vars qw($DISTRIB_ID);
+use vars qw($DISTRIB_RELEASE);
+use vars qw($DISTRIB_BRANCH);
+use vars qw($DISTRIB_DESCRIPTION);
+
+$DISTRIB_ID="Fhem";
+$DISTRIB_RELEASE="5.2";
+$DISTRIB_BRANCH="EXPERIMENTAL";
+$DISTRIB_DESCRIPTION="$DISTRIB_ID $DISTRIB_RELEASE ($DISTRIB_BRANCH)";
+
+1;
