83fb171e09267a33be804e26e90e6e3213ac8106
mfr69bs <mfr69bs@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-07-01 19:07:01 +0000

    release.pm enhanced
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@1677 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/FhemUtils/release.pm
+++ fhem/FHEM/FhemUtils/release.pm
@@ -8,10 +8,15 @@ use vars qw($DISTRIB_ID);
 use vars qw($DISTRIB_RELEASE);
 use vars qw($DISTRIB_BRANCH);
 use vars qw($DISTRIB_DESCRIPTION);
+use vars qw(%UPDATE);
 
 $DISTRIB_ID="Fhem";
 $DISTRIB_RELEASE="5.2";
 $DISTRIB_BRANCH="EXPERIMENTAL";
 $DISTRIB_DESCRIPTION="$DISTRIB_ID $DISTRIB_RELEASE ($DISTRIB_BRANCH)";
+
+$UPDATE{server}   = "http://fhem.de";
+$UPDATE{path}     = "fhemupdate3";
+$UPDATE{packages} = "FHEM PGM2";
 
 1;
