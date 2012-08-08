02530605d020e1cc34ea86866238518638c9fbf3
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-08-08 06:57:21 +0000

    Document dimmer command from Marc, svn check-in 1154
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@1786 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/10_EnOcean.pm
+++ fhem/FHEM/10_EnOcean.pm
@@ -140,30 +140,30 @@ EnOcean_Set($@)
       $hash->{READINGS}{$cmd}{TIME} = $tn;
       $hash->{READINGS}{$cmd}{VAL} = $arg;
 
-   } elsif($st eq "dimmCtrl") { # Tested for Eltako-Dimmer
-     if($cmd eq "teach") {
-      my $data=sprintf("A502000000%s00", $hash->{DEF});
-      Log $ll2, "dimmCtrl.Teach: " . $data;
-       IOWrite($hash, "000A0001", $data); # len:000a optlen:00 pakettype:1(radio)
+    } elsif($st eq "dimmCtrl") { # Tested for Eltako-Dimmer
+      if($cmd eq "teach") {
+        my $data=sprintf("A502000000%s00", $hash->{DEF});
+        Log $ll2, "dimmCtrl.Teach: " . $data;
+        IOWrite($hash, "000A0001", $data); # len:000a optlen:00 pakettype:1(radio)
 
-     } elsif($cmd eq "dimm") {
-       return "Usage: dimm percent [time 01-FF FF:slowest] [on/off]" if(@a<2);
-       my $time=0;
-       my $onoff=1;
-       # for eltako relative (0-100) (but not compliant to EEP because DB0.2 is 0)
-       my $dimVal=$a[1];
-       shift(@a);
-       if(defined($a[1])) { $time=$a[1]; shift(@a); }
-       if(defined($a[1])) { $onoff=($a[1] eq "off") ? 0 : 1; shift(@a); }
-       # EEP: A5/38/08 Central Command ->Typ 0x02: Dimming
-       my $data=sprintf("A502%02X%02X%02X%s00", $dimVal, $time, $onoff|0x08, $hash->{DEF});
-       IOWrite($hash, "000A0001", $data);
-       Log $ll2, "dimmCtrl.dimm: " . $data;
+      } elsif($cmd eq "dimm") {
+        return "Usage: dimm percent [time 01-FF FF:slowest] [on/off]" if(@a<2);
+        my $time=0;
+        my $onoff=1;
+        # for eltako relative (0-100) (but not compliant to EEP because DB0.2 is 0)
+        my $dimVal=$a[1];
+        shift(@a);
+        if(defined($a[1])) { $time=$a[1]; shift(@a); }
+        if(defined($a[1])) { $onoff=($a[1] eq "off") ? 0 : 1; shift(@a); }
+        # EEP: A5/38/08 Central Command ->Typ 0x02: Dimming
+        my $data=sprintf("A502%02X%02X%02X%s00", $dimVal, $time, $onoff|0x08, $hash->{DEF});
+        IOWrite($hash, "000A0001", $data);
+        Log $ll2, "dimmCtrl.dimm: " . $data;
+ 
+      } else {
+        return "Unknown argument $cmd, choose one of: teach, dimm:slider,0,1,100"
 
-     } else {
-      return "Unknown argument $cmd, choose one of: teach, dimm"
-
-     }
+      }
 
     ###########################
     } else {                                          # Simulate a PTM
