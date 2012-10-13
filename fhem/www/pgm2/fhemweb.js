f94ce1cb958ec6f2966bc63c38616de9c7122ed7
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-10-13 08:12:33 +0000

    Slider-Bug
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@1947 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/www/pgm2/fhemweb.js
+++ fhem/www/pgm2/fhemweb.js
@@ -56,6 +56,7 @@ Slider(slider, min, stp, max, curr, cmd)
 {
   var sh = slider.firstChild;
   var lastX=-1, offX=0, maxX=0, val=-1;
+  min = parseInt(min); max = parseInt(max); stp = parseInt(stp);
 
   function
   init()
