4bcb0171b30adb2ef2a9bf9044d2ef45ef7e3946
dennis1980 <dennis1980@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-10-30 04:27:36 +0000

    -feature: remove device from DeviceMonitor is attr device_timeout is set to 0 or removed -feature: get function implemented, get <name> <criteria1>_<criteria2>  criteria1 can be dead, alive or total  criteria2 can be count, text or html
    git-svn-id: https://svn.fhem.de/fhem/trunk@2038 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/DeviceMonitor/98_DeviceMonitor.pm
+++ fhem/contrib/DeviceMonitor/98_DeviceMonitor.pm
@@ -13,12 +13,14 @@
 package main;
 use strict;
 use warnings;
-
+			
 sub DeviceMonitor_Initialize($)
 {
 	my ($hash) = @_;
 	$hash->{DefFn}     = "DeviceMonitor_Define";
+	$hash->{UndefFn}   = "DeviceMonitor_Undef";
 	$hash->{NotifyFn}  = "DeviceMonitor_Notify";
+	$hash->{GetFn}     = "DeviceMonitor_Get";
 	$hash->{AttrList}  = "disable:0,1";
 	addToAttrList("device_timeout"); 
 }
@@ -32,6 +34,33 @@ sub DeviceMonitor_Define($$)
   	$hash->{CHANGED}[0] = "DEFINED";
 	$hash->{STATE}      = "DEFINED";	
 	return undef;
+}
+
+sub
+DeviceMonitor_Undef($$)
+{
+  my ($hash, $arg) = @_;
+
+  return DeviceMonitor_Remove($hash,"all");
+  return undef;
+}
+
+
+sub DeviceMonitor_Get($@)
+{
+#*******************************************************************************
+# Purpose: Get some results
+# Author : Dennis Gnoyke
+# Date   : 27.10.2012
+# Changes: 
+# Remarks: 
+#**************************** Begin of Code *************************************	
+	my ($hash, @a) = @_;
+	return "argument is missing" if(int(@a) != 2);
+	
+    my ($criteria, $output) = split ("_",lc($a[1]));   
+	return DeviceMonitor_GetResult($a[0],$criteria,$output);	
+#**************************** End of Code *****************************************	
 }
 
 sub DeviceMonitor_Notify($$)
@@ -51,11 +80,9 @@ sub DeviceMonitor_Notify($$)
 	my $timeoutinterval = 0;
 	my $devState = "unknown";
 	
-	$timeoutinterval = AttrVal($devName, "device_timeout", "undef"); #Timeout configured ?
-	return "" if ($timeoutinterval eq "undef");
-	
-	$devhash->{HEALTH_MONITORED_BY} = $ownName;	
-		
+	$timeoutinterval = AttrVal($devName, "device_timeout", 0); #Timeout configured ?
+	return "" if ($timeoutinterval < 1 && !defined($defs{$devName}{HEALTH_MONITORED_BY})); #Leave NTFY if device is not configured for DeviceMonitor
+
 	if(AttrVal($devName, "disable", 0) > 0){$timeoutinterval = 0}; #Device Enabled ?
 	if(AttrVal($ownName, "disable", 0) > 0)  #DeviceMonitor Enabled ?
 		{
@@ -66,24 +93,16 @@ sub DeviceMonitor_Notify($$)
 		{
 			$ownhash->{STATE} = "ENABLED";
 		}
-			
+	
+	Log 4, "'$devName' is now monitored by '$ownName'" if(!defined($defs{$devName}{HEALTH_MONITORED_BY}));
+	$devhash->{HEALTH_MONITORED_BY} = $ownName;	
 	# Get current HealthState		
-	if (!defined($devhash->{HEALTH_STATE}))
-		{
-			$devState = "unknown";
-		}
-	else
-		{
-			$devState = $devhash->{HEALTH_STATE};
-		}
+	if (!defined($devhash->{HEALTH_STATE})){$devState = "unknown"}else{$devState = $devhash->{HEALTH_STATE}}
 		
-	if ($timeoutinterval < 1) #device_timeout set to 0
+	if ($timeoutinterval < 1) #device_timeout set to 0, remove it from monitor
 		{
-			if ($devState ne "unknown"){DoTrigger($devName,"health_state: unknown")};
-			$devhash->{HEALTH_STATE} = "unknown";
-			$devhash->{HEALTH_TIME} = TimeNow();
-			$ownhash->{READINGS}{$devName}{VAL} = "unknown";
-			$ownhash->{READINGS}{$devName}{TIME} = TimeNow();
+			DeviceMonitor_Remove($ownName,$devName);
+			RemoveInternalTimer($devhash);
 		}
 	else 
 		{
@@ -106,7 +125,7 @@ sub DeviceMonitor_Timer($)
 # Author : Dennis Gnoyke
 # Date   : 21.10.2012
 # Changes: 22.10.2012 GN Typo Death -> Dead
-# Remarks: EXPERIMENTAL VERSION !!!!!!!
+# Remarks:
 # 
 #**************************** Begin of Code *************************************	
 	#set the HEALTH_STATE to "dead" if not already set
@@ -114,11 +133,134 @@ sub DeviceMonitor_Timer($)
 	my $ownName = $devhash->{HEALTH_MONITORED_BY};
 	my $devName = $devhash->{NAME};
   
-    DoTrigger($devName,"health_state: dead");
-	$devhash->{HEALTH_STATE} = "dead";
-	$devhash->{HEALTH_TIME} = TimeNow();	
-	$defs{$ownName}{READINGS}{$devName}{TIME} = TimeNow();
-	$defs{$ownName}{READINGS}{$devName}{VAL} = "dead";
+    if(AttrVal($devName, "device_timeout", 0) < 1) #device_timeout set to 0, remove it from monitor
+		{
+			DeviceMonitor_Remove($ownName,$devName);
+		}
+	else
+		{
+			DoTrigger($devName,"health_state: dead");
+			$devhash->{HEALTH_STATE} = "dead";
+			$devhash->{HEALTH_TIME} = TimeNow();	
+			$defs{$ownName}{READINGS}{$devName}{TIME} = TimeNow();
+			$defs{$ownName}{READINGS}{$devName}{VAL} = "dead";
+		}
+	RemoveInternalTimer($devhash);
 #**************************** End of Code *****************************************
 }
+
+sub DeviceMonitor_GetResult($$$)
+{
+#*******************************************************************************
+# Purpose: Get results from DeviceMonitor readings
+# Author : Dennis Gnoyke
+# Date   : 27.10.2012
+# Changes: 
+# Remarks: 	$_[0] = DeviceMonitor
+#			$_[1] = Criteria dead|alive|total
+#			$_[2] = Output as count|text|html
+#**************************** Begin of Code *************************************	
+	my $hash = $defs{$_[0]}{READINGS};
+	my $cnt = 0; 		#counter
+	my @result ; 		#result array
+	$result[0] ="null";	#default
+	my $tmp = "";		#temp var
+	
+	foreach my $readings_name (sort keys %{$hash}) #Loop through Readings
+		{
+			my $val = $hash->{$readings_name};
+			if(ref($val)) 
+				{
+					my $readings_value = $val->{VAL};
+					my $readings_time = $val->{TIME};
+					if ($readings_value eq $_[1]) #value like dead or alive
+						{
+							$result["$cnt"] = $readings_name.','."has health_state '$readings_value' reported at $readings_time";
+							$cnt = $cnt + 1
+						}
+					elsif($_[1] eq "total")
+						{
+							$result["$cnt"] = $readings_name.','."has health_state '$readings_value' reported at $readings_time";
+							$cnt = $cnt + 1
+						}
+				}
+		}
+	if ($result[0] eq "null")
+		{
+			return 0 if($_[2] eq "count");
+			return "$_[0]: There was no match for your criteria '$_[1]'" if($_[2] eq "text");
+			return "$_[0]: There was no match for your criteria '$_[1]'" if($_[2] eq "html");
+			return "$_[0]: Unknown argument $_[2] , syntax is <criteria>_<output> criteria=dead,alive,total output=count,text,html";
+		}
+	else
+		{
+			if($_[2] eq "count"){return $cnt}
+			elsif($_[2] eq "text")
+				{
+					foreach (@result) 
+						{
+							my ($device, $value) = split (",",$_);
+							$tmp .= "Device $device $value\n";
+						}
+					return $tmp;
+				}
+			elsif($_[2] eq "html")
+				{
+					$tmp = "<table>\n";
+					foreach (@result)
+						{
+							my ($device, $value) = split (",",$_);
+							$tmp .= "<tr><td>Device <a href='/fhem?detail=$device'>$device</a> $value</td></tr>\n";
+						};
+					$tmp .= "</table></div>";
+					return $tmp;
+				}
+			else{return "$_[0]: Unknown argument $_[2] , syntax is <criteria>_<output> criteria=dead,alive,total output=count,text,html"}
+		}
+#**************************** End of Code *****************************************
+}
+
+
+sub DeviceMonitor_Remove($$)
+{
+#*******************************************************************************
+# Purpose: Resets DeviceMonitor readings
+# Author : Dennis Gnoyke
+# Date   : 28.10.2012
+# Changes: 
+# Remarks: 	$_[0] = DeviceMonitor
+#			$_[1] = Criteria devicename|all
+#	
+#**************************** Begin of Code *************************************	
+	my $hash = $defs{$_[0]}{READINGS};
+		
+	if(defined($hash->{$_[1]})) #remove single device from monitor
+		{
+			 delete $hash->{$_[1]};
+			 if(defined($defs{$_[1]}{HEALTH_MONITORED_BY})){delete $defs{$_[1]}{HEALTH_MONITORED_BY}};
+			 if(defined($defs{$_[1]}{HEALTH_STATE})){delete $defs{$_[1]}{HEALTH_STATE}};
+			 if(defined($defs{$_[1]}{HEALTH_TIME})){delete $defs{$_[1]}{HEALTH_TIME}};
+			 RemoveInternalTimer($defs{$_[1]});
+			 Log 4, "'$_[1]' is no longer monitored by '$_[0]'";
+		}
+	elsif(uc($_[1]) eq "ALL") #remove all devices from monitor
+		{
+			foreach my $readings_name (sort keys %{$hash}) #Loop through Readings
+			{
+				delete $hash->{$readings_name};
+				if(defined($defs{$readings_name}{HEALTH_MONITORED_BY})){delete $defs{$readings_name}{HEALTH_MONITORED_BY}};
+				if(defined($defs{$readings_name}{HEALTH_STATE})){delete $defs{$readings_name}{HEALTH_STATE}};
+				if(defined($defs{$readings_name}{HEALTH_TIME})){delete $defs{$readings_name}{HEALTH_TIME}};
+				RemoveInternalTimer($defs{$readings_name});
+			}
+			Log 4, "all devices removed from '$_[0]', please note that devices will appear again if their device_timeout attr is still set and '$_[0]' is defined and enabled!";
+		}
+	else
+		{
+			return "$_[0]: Can`t remove $_[1] check spelling and upper/lower cases";
+		}
+
+#**************************** End of Code *****************************************
+}
+
 1;
