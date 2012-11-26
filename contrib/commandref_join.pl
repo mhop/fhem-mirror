060c9a0b4f9b8e99435cf27d3507bacd17c8d03d
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-11-26 15:10:37 +0000

    Multi-lang support
    git-svn-id: https://fhem.svn.sourceforge.net/svnroot/fhem/trunk/fhem@2197 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- contrib/commandref_join.pl
+++ contrib/commandref_join.pl
@@ -4,16 +4,9 @@ use strict;
 use warnings;
 
 # $Id$
-
-my $docIn  = "docs/commandref_frame.html";
-my $docOut = "docs/commandref.html";
-my @modDir = ("FHEM");
 use constant TAGS => qw{ul li code};
-
-open(IN, "$docIn")    || die "Cant open $docIn: $!\n";
-open(OUT, ">$docOut") || die "Cant open $docOut: $!\n";
-
 my %mods;
+my @modDir = ("FHEM");
 foreach my $modDir (@modDir) {
   opendir(DH, $modDir) || die "Cant open $modDir: $!\n";
   while(my $l = readdir DH) {
@@ -25,63 +18,76 @@ foreach my $modDir (@modDir) {
   }
 }
 
-# First run: check what is a command and what is a helper module
-my $status;
-my %noindex;
-while(my $l = <IN>) {
-  last if($l =~ m/<h3>Introduction/);
-  $noindex{$1} = 1 if($l =~ m/href="#(.*)"/);
-}
-seek(IN,0,0);
 
-# Second run: create the file
-# Header
-while(my $l = <IN>) {
-  print OUT $l;
-  last if($l =~ m/#global/);
-}
+my @lang = ("EN", "DE");
 
-# index for devices.
-foreach my $mod (sort keys %mods) {
-  next if($noindex{$mod});
-  print OUT "      <a href='#$mod'>$mod</a> &nbsp;\n";
-}
+foreach my $lang (@lang) {
+  my $suffix = ($lang eq "EN" ? "" : "_$lang");
+  my $docIn  = "docs/commandref_frame$suffix.html";
+  my $docOut = "docs/commandref$suffix.html";
 
-# Copy the middle part
-while(my $l = <IN>) {
-  last if($l =~ m/name="perl"/);
-  print OUT $l;
-}
+  open(IN, "$docIn")    || die "Cant open $docIn: $!\n";
+  open(OUT, ">$docOut") || die "Cant open $docOut: $!\n";
 
-# Copy the doc part from the module
-foreach my $mod (sort keys %mods) {
-  my $tag;
-  my %tagcount= ();
-  open(MOD, $mods{$mod}) || die("Cant open $mods{$mod}:$!\n");
-  my $skip = 1;
-  while(my $l = <MOD>) {
-    if($l =~ m/^=begin html/) {
-      $skip = 0;
-    } elsif($l =~ m/^=end html/) {
-      $skip = 1;
-    } elsif(!$skip) {
-      # here we copy line by line from the module
-      print OUT $l;
-      foreach $tag (TAGS) {
-        $tagcount{$tag}+= ($l =~ /<$tag>/i);
-        $tagcount{$tag}-= ($l =~ /<\/$tag>/i);
+  # First run: check what is a command and what is a helper module
+  my $status;
+  my %noindex;
+  while(my $l = <IN>) {
+    last if($l =~ m/<h3>Introduction/);
+    $noindex{$1} = 1 if($l =~ m/href="#(.*)"/);
+  }
+  seek(IN,0,0);
+
+  # Second run: create the file
+  # Header
+  while(my $l = <IN>) {
+    print OUT $l;
+    last if($l =~ m/#global/);
+  }
+
+  # index for devices.
+  foreach my $mod (sort keys %mods) {
+    next if($noindex{$mod});
+    print OUT "      <a href='#$mod'>$mod</a> &nbsp;\n";
+  }
+
+  # Copy the middle part
+  while(my $l = <IN>) {
+    last if($l =~ m/name="perl"/);
+    print OUT $l;
+  }
+
+  # Copy the doc part from the module
+  foreach my $mod (sort keys %mods) {
+    my $tag;
+    my %tagcount= ();
+    open(MOD, $mods{$mod}) || die("Cant open $mods{$mod}:$!\n");
+    my $skip = 1;
+    while(my $l = <MOD>) {
+      if($l =~ m/^=begin html$suffix$/) {
+        $l = <MOD>;    # skip one line, to be able to repeat join+split
+        $skip = 0;
+      } elsif($l =~ m/^=end html$suffix/) {
+        $skip = 1;
+      } elsif(!$skip) {
+        # here we copy line by line from the module
+        print OUT $l;
+        foreach $tag (TAGS) {
+          $tagcount{$tag}+= ($l =~ /<$tag>/i);
+          $tagcount{$tag}-= ($l =~ /<\/$tag>/i);
+        }
       }
     }
+    close(MOD);
+    foreach $tag (TAGS) {
+      print("$lang $mods{$mod}: Unbalanced $tag\n") if($tagcount{$tag});
+    }
   }
-  close(MOD);
-  foreach $tag (TAGS) {
-    print("$mods{$mod}: Unbalanced $tag\n") if($tagcount{$tag});
-  }
-}
 
-# Copy the tail
-print OUT '<a name="perl"></a>',"\n";
-while(my $l = <IN>) {
-  print OUT $l;
+  # Copy the tail
+  print OUT '<a name="perl"></a>',"\n";
+  while(my $l = <IN>) {
+    print OUT $l;
+  }
+  close(OUT);
 }
-close(OUT);
