662ed3ff44592539e8166a1b6f9fd9b501d55a44
borisneubert <borisneubert@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2012-12-01 20:00:04 +0000

    fix for leap years in fhemTimeGm
    git-svn-id: https://svn.fhem.de/fhem/trunk@2246 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/fhem.pl
+++ fhem/fhem.pl
@@ -3003,8 +3003,9 @@ fhemTimeGm($$$$$$) {
     
     $year+= 1900;
     my $isleapyear= $year % 4 ? 0 : $year % 100 ? 1 : $year % 400 ? 0 : 1;
-    my $leapyears= int((($year-1)-1968)/4 - (($year-1)-1900)/100 + (($year-1)-1600)/400);
-    #Debug sprintf("%02d.%02d.%04d %02d:%02d:%02d", $mday,$month+1,$year,$hour,$min,$sec);
+    # here the Wikipedia as at 2012-12-01 is wrong and that code line is right
+    my $leapyears= int(($year-1968)/4 - ($year-1900)/100 + ($year-1600)/400);
+    #Debug sprintf("%02d.%02d.%04d %02d:%02d:%02d %d leap years, is leap year: %d", $mday,$month+1,$year,$hour,$min,$sec,$leapyears,$isleapyear);
 
     if ( $^O eq 'MacOS' ) {
       $year-= 1904;
