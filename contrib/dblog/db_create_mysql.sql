cff10b87834d34fc322cb227bab74405040f5945
tobiasfaust <tobiasfaust@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2013-01-31 04:03:12 +0000

    93_DbLog.pm: MySQL: neue Definition eines Indices auf history zur schnelleren              Generierung von Plots, SQL darauf angepasst
    git-svn-id: https://fhem.svn.sourceforge.net/svnroot/fhem/trunk/fhem@2614 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- contrib/dblog/db_create_mysql.sql
+++ contrib/dblog/db_create_mysql.sql
@@ -3,5 +3,5 @@ CREATE USER 'fhemuser'@'%' IDENTIFIED BY 'fhempassword';
 CREATE TABLE history (TIMESTAMP TIMESTAMP, DEVICE varchar(32), TYPE varchar(32), EVENT varchar(64), READING varchar(32), VALUE varchar(32), UNIT varchar(32));
 CREATE TABLE current (TIMESTAMP TIMESTAMP, DEVICE varchar(32), TYPE varchar(32), EVENT varchar(64), READING varchar(32), VALUE varchar(32), UNIT varchar(32));
 GRANT SELECT, INSERT, DELETE ON `fhem` .* TO 'fhemuser'@'%';
-
+CREATE INDEX Search_Idx ON history (DEVICE, READING, TIMESTAMP);
 
