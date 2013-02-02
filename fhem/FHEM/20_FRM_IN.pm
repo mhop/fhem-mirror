a81e75a4fc3f359ba9ae27e96ce65a411896208f
ntruchsess <ntruchsess@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2013-02-02 22:48:54 +0000

    add counter to FRM_IN, add thresholds and alarms to FRM_IN and FRM_AD
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@2638 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/20_FRM_IN.pm
+++ fhem/FHEM/20_FRM_IN.pm
@@ -7,17 +7,30 @@ use Device::Firmata;
 use Device::Firmata::Constants  qw/ :all /;
 
 #####################################
+
+my %sets = (
+  "alarm" => "",
+);
+
+my %gets = (
+  "reading" => "",
+  "state"   => "",
+  "count"   => 0,
+  "alarm"   => "off"
+);
+
 sub
 FRM_IN_Initialize($)
 {
   my ($hash) = @_;
 
+  $hash->{SetFn}     = "FRM_IN_Set";
   $hash->{GetFn}     = "FRM_IN_Get";
   $hash->{DefFn}     = "FRM_Client_Define";
   $hash->{InitFn}    = "FRM_IN_Init";
   $hash->{UndefFn}   = "FRM_IN_Undef";
   
-  $hash->{AttrList}  = "IODev loglevel:0,1,2,3,4,5 $main::readingFnAttributes";
+  $hash->{AttrList}  = "IODev count-mode count-threshold loglevel:0,1,2,3,4,5 $main::readingFnAttributes";
 }
 
 sub
@@ -27,6 +40,9 @@ FRM_IN_Init($$)
 	if (FRM_Init_Pin_Client($hash,$args,PIN_INPUT)) {
 		my $firmata = $hash->{IODev}->{FirmataDevice};
 		$firmata->observe_digital($hash->{PIN},\&FRM_IN_observer,$hash);
+		if (! (defined AttrVal($hash->{NAME},"stateFormat",undef))) {
+			$main::attr{$hash->{NAME}}{"stateFormat"} = "reading";
+		}
 		main::readingsSingleUpdate($hash,"state","Initialized",1);
 		return undef;
 	}
@@ -39,27 +55,65 @@ FRM_IN_observer
 {
 	my ($pin,$old,$new,$hash) = @_;
 	main::Log(6,"onDigitalMessage for pin ".$pin.", old: ".(defined $old ? $old : "--").", new: ".(defined $new ? $new : "--"));
-	main::readingsSingleUpdate($hash,"state",$new == PIN_HIGH ? "on" : "off", 1);
+	my $name = $hash->{NAME};
+	my $mode = AttrVal($name,"count-mode","rising");
+	my $count = ReadingsVal($name,"count",0);
+	main::readingsBeginUpdate($hash);
+	if ( ($old != $new) 
+	  and (($mode eq "rising" and $old == PIN_LOW) 
+	    or ($mode eq "falling" and $old == PIN_HIGH)
+	    or ($mode eq "both"))) {
+	  $count++;
+	  my $threshold = AttrVal($name,"count-threshold",0);
+      if ( $count >= $threshold ) {
+      	main::readingsBulkUpdate($hash,"alarm","on",1);
+      	$count=0;
+      }
+	  main::readingsBulkUpdate($hash,"count",$count,1); 
+	};
+	main::readingsBulkUpdate($hash,"reading",$new == PIN_HIGH ? "on" : "off", 1);
+	main::readingsEndUpdate($hash,0);
+}
+
+sub
+FRM_IN_Set
+{
+  my ($hash, @a) = @_;
+  return "Need at least one parameters" if(@a < 2);
+  return "Unknown argument $a[1], choose one of " . join(" ", sort keys %sets)
+  	if(!defined($sets{$a[1]}));
+  my $command = $a[1];
+  my $value = $a[2];
+  COMMAND_HANDLER: {
+    $command eq "alarm" and do {
+      return undef if (!($value eq "off" or $value eq "on"));
+      main::readingsSingleUpdate($hash,"alarm",$value,1);
+      last;
+    }
+  }
 }
 
 sub
 FRM_IN_Get($)
 {
-  my ($hash,@a) = @_;
-  my $iodev = $hash->{IODev};
+  my ($hash, @a) = @_;
+  return "Need at least one parameters" if(@a < 2);
+  return "Unknown argument $a[1], choose one of " . join(" ", sort keys %gets)
+  	if(!defined($gets{$a[1]}));
   my $name = shift @a;
-  return $name." no IODev assigned" if (!defined $iodev);
-  return $name.", ".$iodev->{NAME}." is not connected" if (!(defined $iodev->{FirmataDevice} and defined $iodev->{FD}));
   my $cmd = shift @a;
-  my $ret;
   ARGUMENT_HANDLER: {
     $cmd eq "reading" and do {
-  	  my $ret = $iodev->{FirmataDevice}->digital_read($hash->{PIN});
-  	  return $ret == PIN_HIGH ? "on" : "off";
+      my $iodev = $hash->{IODev};
+      return $name." no IODev assigned" if (!defined $iodev);
+      return $name.", ".$iodev->{NAME}." is not connected" if (!(defined $iodev->{FirmataDevice} and defined $iodev->{FD}));
+  	  return $iodev->{FirmataDevice}->digital_read($hash->{PIN}) == PIN_HIGH ? "on" : "off";
     };
-    $ret = "unknown command ".$cmd;
+    ( $cmd eq "count" or $cmd eq "alarm" or $cmd eq "state" ) and do {
+      return main::ReadingsVal($name,"count",$gets{$cmd});
+    };
   }
-  return $ret;
+  return undef;
 }
 
 sub
@@ -92,17 +146,33 @@ FRM_IN_Undef($$)
   <a name="FRM_INset"></a>
   <b>Set</b><br>
   <ul>
-  N/A<br>
+    <li>alarm on|off<br>
+    set the alarm to on or off. Used to clear the alarm.<br>
+    The alarm is set to 'on' whenever the count reaches the threshold and doesn't clear itself.</li>
   </ul>
   <a name="FRM_INget"></a>
   <b>Get</b>
   <ul>
-  <li>reading<br>
-  returns the state of the arduino-pin. Values are 'on' and 'off'.<br></li>
+    <li>reading<br>
+    returns the logical state of the arduino-pin. Values are 'on' and 'off'.<br></li>
+    <li>count<br>
+    returns the current count. Contains the number of toggles of the arduino-pin.<br>
+    Depending on the attribute 'count-mode' every rising or falling edge (or both) is counted.</li>
+    <li>alarm<br>
+    returns the current state of 'alarm'. Values are 'on' and 'off' (Defaults to 'off')<br>
+    'alarm' doesn't clear itself, has to be set to 'off' eplicitly./li>
+    <li>state<br>
+    returns the 'state' reading</li>
   </ul><br>
   <a name="FRM_INattr"></a>
   <b>Attributes</b><br>
   <ul>
+      <li>count-mode rising|falling|both<br>
+      Determines whether 'rising' (transitions from 'off' to 'on') of falling (transitions from 'on' to 'off')<br>
+      edges (or 'both') are counted. Defaults to 'rising'</li>
+      <li>count-threshold &lt;number&gt;<br>
+      sets the theshold-value for the counter. Whenever 'count' reaches the 'count-threshold' 'alarm' is<br>
+      set to 'on' and count is reset to 0. Use 'set alarm off' to clear the alarm.</li>
       <li><a href="#IODev">IODev</a><br>
       Specify which <a href="#FRM">FRM</a> to use. (Optional, only required if there is more
       than one FRM-device defined.)
