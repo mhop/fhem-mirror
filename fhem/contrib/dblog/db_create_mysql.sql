371e9e7aca5a37b43555efd31c388656fd4a03f7
johannnes <johannnes@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2013-02-19 19:28:47 +0000

    extended the event column for mysql from 64 to 512 characters   added example SQL for SQLite
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@2770 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/dblog/db_create_mysql.sql
+++ fhem/contrib/dblog/db_create_mysql.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE `fhem` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE USER 'fhemuser'@'%' IDENTIFIED BY 'fhempassword';
-CREATE TABLE `fhem`.`history` (TIMESTAMP TIMESTAMP, DEVICE varchar(32), TYPE varchar(32), EVENT varchar(64), READING varchar(32), VALUE varchar(32), UNIT varchar(32));
-CREATE TABLE `fhem`.`current` (TIMESTAMP TIMESTAMP, DEVICE varchar(32), TYPE varchar(32), EVENT varchar(64), READING varchar(32), VALUE varchar(32), UNIT varchar(32));
+CREATE TABLE `fhem`.`history` (TIMESTAMP TIMESTAMP, DEVICE varchar(32), TYPE varchar(32), EVENT varchar(512), READING varchar(32), VALUE varchar(32), UNIT varchar(32));
+CREATE TABLE `fhem`.`current` (TIMESTAMP TIMESTAMP, DEVICE varchar(32), TYPE varchar(32), EVENT varchar(512), READING varchar(32), VALUE varchar(32), UNIT varchar(32));
 GRANT SELECT, INSERT, DELETE ON `fhem`.* TO 'fhemuser'@'%';
 CREATE INDEX Search_Idx ON `fhem`.`history` (DEVICE, READING, TIMESTAMP);
 
