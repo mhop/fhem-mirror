a9c56fb6d2c2ddbaa6fa6417d3798ac118727b44
johannnes <johannnes@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2013-03-08 12:52:42 +0000

    - Bugfixes in der Darstellung der gespeicherten Farben eines Charts   - Implementation eine Konfigurationsdatei für den Benutzer   - Steuerung über diese Datei ermöglicht z.B. die Konversion in Charts von "on" zu 1, "closed" zu 0 usw.
    git-svn-id: https://svn.fhem.de/fhem/trunk@2870 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/www/frontend/www/frontend/app/model/ChartModel.js
+++ fhem/www/frontend/www/frontend/app/model/ChartModel.js
@@ -1,6 +1,7 @@
 /**
  * Model for Charts
  */
+
 Ext.define('FHEM.model.ChartModel', {
     extend: 'Ext.data.Model',
     fields: [
@@ -21,13 +22,44 @@ Ext.define('FHEM.model.ChartModel', {
         },
         {
             name: 'VALUE',
-            type: 'float'
+            type: 'float',
+            convert: function( v, record ) {
+                return record.parseToNumber(v);
+            }
         },{
             name: 'VALUE2',
-            type: 'float'
+            type: 'float',
+            convert: function( v, record ) {
+                return record.parseToNumber(v);
+            }
         },{
             name: 'VALUE3',
-            type: 'float'
+            type: 'float',
+            convert: function( v, record ) {
+                return record.parseToNumber(v);
+            }
         }
-    ]
+    ],
+    parseToNumber: function(value) {
+        
+        if (value === "") {
+            return 0;
+        } else if (parseFloat(value, 10) === "NaN") {
+            if (Ext.isDefined(FHEM) && Ext.isDefined(FHEM.userconfig)) {
+                
+                var convertednumber = 0;
+                Ext.iterate(FHEM.userconfig.chartkeys, function(k, v) {
+                    if (value === k) {
+                        //return the value for the given key from userconfig
+                        convertednumber = v;
+                    }
+                });
+                return parseFloat(convertednumber, 10);
+            } else {
+                return value;
+            }
+        } else {
+            return parseFloat(value, 10);
+        }
+    }
 });
