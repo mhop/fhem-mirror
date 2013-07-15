7c9958bdffef145deed66930f5a76dad301e5d93
justme1968 <justme1968@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2013-07-15 21:39:00 +0000

    use widget module to update colorpicker in longpoll
    git-svn-id: https://svn.fhem.de/fhem/trunk@3432 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/www/jscolor/jscolor.js
+++ fhem/www/jscolor/jscolor.js
@@ -992,17 +992,4 @@ var jscolor = {
 
 };
 
-function
-setColor(el,mode,cmd)
-{
-  var v = el.color;
-  if(mode=="HSV") {
-    v = (0x100 | Math.round(42*el.color.hsv[0])).toString(16).substr(1) +
-        (0x100 | Math.round(255*el.color.hsv[1])).toString(16).substr(1) +
-        (0x100 | Math.round(255*el.color.hsv[2])).toString(16).substr(1);
-  }
-  if(cmd)
-    document.location = cmd.replace('%',v);
-}
-
 jscolor.install();
