2804ed37a6ccf59546bd0985316dbf9034344a17
martinp876 <martinp876@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2013-10-10 21:25:10 +0000

    correct off-time detection for wakeup devices
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@4030 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/00_HMLAN.pm
+++ fhem/FHEM/00_HMLAN.pm
@@ -526,7 +526,7 @@ sub HMLAN_Parse($$) {##########################################################
 
 	# HMLAN sends ACK for flag 'A0' but not for 'A4'(config mode)- 
 	# we ack ourself an long as logic is uncertain - also possible is 'A6' for RHS
-	if (hex($flg)&0x4){#not sure: 4 oder 2 ? 
+	if (hex($flg)&0x2){#not sure: 4 oder 2 ? 02 works for VD!
 	  my $wait = 0.100 - $dly/1000;
 	  $hash->{helper}{$src}{nextSend} = gettimeofday() + $wait if ($wait > 0);
 	}
