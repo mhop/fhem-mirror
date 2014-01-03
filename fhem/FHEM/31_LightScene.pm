ce2918190a07c7be4e03a87501ad796591307205
justme1968 <justme1968@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-01-03 16:47:19 +0000

    changed #{...} to @{...}
    git-svn-id: https://svn.fhem.de/fhem/trunk@4548 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/31_LightScene.pm
+++ fhem/FHEM/31_LightScene.pm
@@ -384,7 +384,7 @@ LightScene_Set($@)
               $get = $2 if( $2 );
               $set = $5 if( $5 );
             }
-            ($get,$regex) = split('#', $get, 2);
+            ($get,$regex) = split('@', $get, 2);
             $set = "state" if( $set eq "STATE" );
 
             $saved .= "$set " if( $set ne "state" );
@@ -612,14 +612,14 @@ LightScene_Get($@)
       <li>lightSceneParamsToSave<br>
       this attribute can be set on the devices to be included in a scene. it is set to a comma separated list of readings
       that will be saved. multiple readings separated by : are collated in to a single set command (this has to be supported
-      by the device). each reading can have a perl expression appended with '#' that will be used to alter the $value used for
+      by the device). each reading can have a perl expression appended with '@' that will be used to alter the $value used for
       the set command. this can for example be used to strip a trailing % from a dimmer state.
       in addition to reading names the list can also contain expressions of the form <code>abc -> xyz</code>
       or <code>get cba -> set uvw</code> to map reading abc to set xyz or get cba to set uvw. the list can be given as a
       string or as a perl expression enclosed in {} that returns this string.<br>
       <code>attr myReceiver lightSceneParamsToSave volume,channel</code></br>
       <code>attr myHueDevice lightSceneParamsToSave {(Value($DEVICE) eq "off")?"state":"bri : xy"}</code></li>
-      <code>attr myDimmer lightSceneParamsToSave state#{if($value=~m/(\d+)/){$1}else{$value}}</code></br>
+      <code>attr myDimmer lightSceneParamsToSave state@{if($value=~m/(\d+)/){$1}else{$value}}</code></br>
     </ul><br>
 </ul>
 
