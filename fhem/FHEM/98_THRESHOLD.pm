800005ba5e8d499a9060eef6e178e12e7f0a887c
damian-s <damian-s@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-01-08 19:46:18 +0000

    Id
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@4591 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_THRESHOLD.pm
+++ fhem/FHEM/98_THRESHOLD.pm
@@ -16,7 +16,7 @@
 #     You should have received a copy of the GNU General Public License
 #     along with fhem.  If not, see <http://www.gnu.org/licenses/>.
 #
-# $Id: 98_THRESHOLD.PM Damian $
+#     $Id$
 ##############################################################################
 
 
