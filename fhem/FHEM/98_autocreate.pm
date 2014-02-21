0c3600f6ff18c802aaceedd2edf96624d46424a5
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-02-21 20:38:59 +0000

    98_autocreate.pm: revert last CURRENT patch
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@5015 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_autocreate.pm
+++ fhem/FHEM/98_autocreate.pm
@@ -203,7 +203,7 @@ autocreate_Notify($$)
         $wlname .= "_$wnr" if($wnr > 1);
         $wnr++;
         delete($defs{$wlname});   # If we are re-creating it with createlog.
-        $cmd = "$wlname SVG $flname:$gplotfile:";
+        $cmd = "$wlname SVG $flname:$gplotfile:CURRENT";
         Log3 $me, 2, "autocreate: define $cmd";
         $ret = CommandDefine(undef, $cmd);
         if($ret) {
