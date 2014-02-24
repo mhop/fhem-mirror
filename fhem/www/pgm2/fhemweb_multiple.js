b4c7b747c346a069e53c67bc97619bb10c68a187
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-02-24 10:36:47 +0000

    FHEMWEB: allow space in room selector (from justme1968)
    git-svn-id: https://svn.fhem.de/fhem/trunk@5037 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/www/pgm2/fhemweb_multiple.js
+++ fhem/www/pgm2/fhemweb_multiple.js
@@ -8,7 +8,8 @@ FW_multipleSelChange(name, devName, vArr)
   o.newEl = document.createElement('select');
   o.newEl.setAttribute('multiple', true);
   for(var j=1; j < vArr.length; j++) {
-    o.newEl.options[j-1] = new Option(vArr[j], vArr[j]);
+    var s = vArr[j].replace(/#/g," ");
+    o.newEl.options[j-1] = new Option(s, s);
   }
   o.qFn = 'FW_multipleSetSelected(qArg, "%")';
   o.qArg = o.newEl;
@@ -26,7 +27,7 @@ FW_multipleSetSelected(el, val)
       for(var i=0;i<l.length;i++)
         if(el.options[j].value == l[i])
            el.options[j].selected = true;
-         
+
     if(el.onchange)
       el.onchange();
 }
