322eca1c0cb4a86a4ca6656ff63acb4c9e2b0ca1
dietmar63 <dietmar63@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-03-16 22:44:28 +0000

    improvements in Twilight    a new way to set unset internal timer
    Heating_Control
       bug repaired when using times specified in Perlcode(update at midnight)
       autodetect HM-TC-IT-WM-W-EU as  heating
       device is no longer checked to get more flexibility
       support for $NAME, $EVENT beside of @, %(some obsolet) 
    
    Weekday_Timer
       code optimisation
       bug fixing
       bug repaired when using times specified in Perlcode(update at midnight)
       support for $NAME, $EVENT beside of @, %(some obsolet) 
    
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@5241 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_WeekdayTimer.pm
+++ fhem/FHEM/98_WeekdayTimer.pm
@@ -27,8 +27,7 @@ use warnings;
 use POSIX;
 
 ##################################### 
-sub
-WeekdayTimer_Initialize($)
+sub WeekdayTimer_Initialize($)
 {
   my ($hash) = @_;
 
@@ -46,14 +45,12 @@ WeekdayTimer_Initialize($)
                         $readingFnAttributes;
 }
 
-sub
-WeekdayTimer_Get($@) {
+sub WeekdayTimer_Get($@) {
    return Heating_Control_Get($@);
 }
 
 
-sub
-WeekdayTimer_Define($$){
+sub WeekdayTimer_Define($$){
   my ($hash, $def) = @_;
 
   my $ret = Heating_Control_Define($hash, $def);
@@ -61,22 +58,18 @@ WeekdayTimer_Define($$){
   return $ret;
 }
 
-sub
-WeekdayTimer_Undef($$){
+sub WeekdayTimer_Undef($$){
   my ($hash, $arg) = @_;
   return Heating_Control_Undef($hash, $arg);
 }
 
-sub
-WeekdayTimer_UpdatePerlTime($)
-{
+sub WeekdayTimer_UpdatePerlTime($) {
     my ($hash) = @_;
 
-    Heating_Control_Define($hash, $hash->{NAME} . " " . $hash->{TYPE} . " " . $hash->{DEF} );
+    Heating_Control_UpdatePerlTime($hash);
 }
 
-sub
-WeekdayTimer_Update($){
+sub WeekdayTimer_Update($){
 my ($hash) = @_;
   return Heating_Control_Update($hash);
 }
