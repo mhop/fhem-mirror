6501491272887613b708b357142709676f8e909c
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-04-08 11:29:41 +0000

    CUL: UNIRoll added as client
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@5487 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/00_CUL.pm
+++ fhem/FHEM/00_CUL.pm
@@ -43,9 +43,10 @@ my %sets = (
 
 my @ampllist = (24, 27, 30, 33, 36, 38, 40, 42); # rAmpl(dB) 
 
-my $clientsSlowRF    = ":FS20:FHT.*:KS300:USF1000:BS:HMS: " .
-                       ":CUL_EM:CUL_WS:CUL_FHTTK:CUL_RFR:CUL_HOERMANN: " .
-                       ":ESA2000:CUL_IR:CUL_TX:Revolt:IT:STACKABLE_CC:";
+my $clientsSlowRF    = ":FS20:FHT.*:KS300:USF1000:BS:HMS: ".
+                       ":CUL_EM:CUL_WS:CUL_FHTTK:CUL_HOERMANN: ".
+                       ":ESA2000:CUL_IR:CUL_TX:Revolt:IT:UNIRoll: ".
+                       ":STACKABLE_CC:CUL_RFR:";
 my $clientsHomeMatic = ":CUL_HM:HMS:CUL_IR:STACKABLE_CC:"; 
 my $clientsMAX       = ":CUL_MAX:HMS:CUL_IR:STACKABLE_CC:";
 
@@ -67,6 +68,7 @@ my %matchListSlowRF = (
     "F:Revolt"    => "^r......................\$",
     "G:IT"        => "^i......\$",
     "H:STACKABLE_CC"=>"^\\*",
+    "I:UNIRoll"   => "^[0-9A-F]{5}(B|D|E)",
 );
 my %matchListHomeMatic = (
     "1:CUL_HM" => "^A....................",
