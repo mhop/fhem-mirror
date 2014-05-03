89ec3ed15ff133ff7cc9bcbe10dee1fd423b0e20
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-05-03 11:57:12 +0000

    fhemweb_multiple.js rewrite as jQuery-ui-dialog + default .css reorg
    git-svn-id: https://svn.fhem.de/fhem/trunk@5731 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/www/pgm2/fhemweb_multiple.js
+++ fhem/www/pgm2/fhemweb_multiple.js
@@ -3,17 +3,63 @@ FW_multipleSelChange(name, devName, vArr)
 {
   if(vArr.length < 2 || vArr[0] != "multiple")
     return undefined;
-
+  
   var o = new Object();
-  o.newEl = document.createElement('select');
-  o.newEl.setAttribute('multiple', true);
-  for(var j=1; j < vArr.length; j++) {
-    var s = vArr[j].replace(/#/g," ");
-    o.newEl.options[j-1] = new Option(s, s);
-  }
+  o.newEl = document.createElement('input');
+  o.newEl.type='text';
+  o.newEl.size=30;
   o.qFn = 'FW_multipleSetSelected(qArg, "%")';
   o.qArg = o.newEl;
+  o.newEl.setAttribute('onFocus', 'FW_multipleSelect(this)');
+  o.newEl.setAttribute('allVals', vArr);
+  o.newEl.setAttribute('readonly', 'readonly');
   return o;
+}
+
+function
+FW_multipleSelect(el)
+{
+  loadLink("pgm2/jquery-ui.min.css");
+  loadScript("pgm2/jquery.min.js", function(){
+    loadScript("pgm2/jquery-ui.min.js", function() {
+
+      var sel = $(el).val().split(","), selObj={};
+      for(var i1=0; i1<sel.length; i1++)
+        selObj[sel[i1]] = 1;
+
+      var vArr = $(el).attr("allVals").split(",");
+      var table = "";
+      for(var i1=1; i1<vArr.length; i1++) {
+        var v = vArr[i1];
+        table += '<tr>'+
+          '<td><input name="'+v+'" type="checkbox"'+
+                          (selObj[v] ? " checked" : "")+'/></td>'+
+          '<td><label for="' +v+'">'+v+'</label></td></tr>';
+      }
+
+      $('body').append(
+        '<div id="multidlg" style="display:none">'+
+          '<table>'+table+'</table><input id="md_freeText"></input>'+
+        '</div>');
+
+      $('#multidlg').dialog(
+        { modal:true, closeOnEscape:false, maxHeight:$(window).height()*3/4,
+          buttons:[
+          { text:"Cancel", click:function(){ $('#multidlg').remove(); }},
+          { text:"OK", click:function(){
+            var res=[];
+            if($("#md_freeText").val())
+              res.push($("#md_freeText").val());
+            $("#multidlg table input").each(function(){
+              if($(this).prop("checked"))
+                res.push($(this).attr("name"));
+            });
+            $(el).val(res.join(","));
+            $('#multidlg').remove();
+          }}]});
+    });
+  });
+  return false;
 }
 
 function
@@ -21,16 +67,9 @@ FW_multipleSetSelected(el, val)
 {
   if(typeof el == 'string')
     el = document.getElementById(el);
-
-    var l = val.split(",");
-    for(var j=0;j<el.options.length;j++)
-      for(var i=0;i<l.length;i++)
-        if(el.options[j].value == l[i])
-           el.options[j].selected = true;
-
-    if(el.onchange)
-      el.onchange();
+  el.value=val;
 }
+
 
 FW_widgets['multiple'] = {
   selChange:FW_multipleSelChange
