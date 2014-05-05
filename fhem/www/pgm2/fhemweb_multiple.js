844702a00e9e8e63df3eede5245e68e1722fcc5b
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-05-05 15:14:23 +0000

    fhemweb_multiple.js: fix space bug (forum #23209) and New-data-Edit bug (forum #20564)
    git-svn-id: https://svn.fhem.de/fhem/trunk@5751 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/www/pgm2/fhemweb_multiple.js
+++ fhem/www/pgm2/fhemweb_multiple.js
@@ -27,7 +27,7 @@ FW_multipleSelect(el)
       for(var i1=0; i1<sel.length; i1++)
         selObj[sel[i1]] = 1;
 
-      var vArr = $(el).attr("allVals").split(",");
+      var vArr = $(el).attr("allVals").replace("#", " ").split(",");
       var table = "";
       for(var i1=1; i1<vArr.length; i1++) {
         var v = vArr[i1];
@@ -35,11 +35,13 @@ FW_multipleSelect(el)
           '<td><input name="'+v+'" type="checkbox"'+
                           (selObj[v] ? " checked" : "")+'/></td>'+
           '<td><label for="' +v+'">'+v+'</label></td></tr>';
+        delete(selObj[v]);
       }
 
       $('body').append(
         '<div id="multidlg" style="display:none">'+
-          '<table>'+table+'</table><input id="md_freeText"></input>'+
+          '<table>'+table+'</table><input id="md_freeText" '+
+                'value="'+Object.keys(selObj).join(',')+'"/>'+
         '</div>');
 
       $('#multidlg').dialog(
