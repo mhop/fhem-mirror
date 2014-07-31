e9cf9bb200f8befafdd0678707bd5df167450511
justme1968 <justme1968@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-07-31 20:26:38 +0000

    fixed typo
    git-svn-id: https://svn.fhem.de/fhem/trunk@6336 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/32_mailcheck.pm
+++ fhem/FHEM/32_mailcheck.pm
@@ -166,7 +166,7 @@ mailcheck_Connect($)
       $hash->{LAST_LOGIN} = FmtDateTime( gettimeofday() );
 
       $hash->{CLIENT} = $client;
-      Log3 $name, 3, "$name: logged in to $hash->{User}";
+      Log3 $name, 3, "$name: logged in to $hash->{user}";
 
       $hash->{HAS_IDLE} = $client->has_capability("IDLE");
 
