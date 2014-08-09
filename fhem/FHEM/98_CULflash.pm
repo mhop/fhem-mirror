f829faf1b63ff8c5be82c910b7c4e997dfc7b178
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-08-09 11:36:20 +0000

    CULflash generalization
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@6385 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_CULflash.pm
+++ fhem/FHEM/98_CULflash.pm
@@ -1,6 +1,5 @@
 ##############################################
 # $Id$
-# modified by M. Fischer
 package main;
 use strict;
 use warnings;
@@ -8,10 +7,7 @@ use HttpUtils;
 
 sub CommandCULflash($$);
 
-my $host = "http://fhem.de";
-my $path = "/fhemupdate4/svn/";
-my $file = "controls_fhem.txt";
-my $dfu    = "dfu-programmer";
+my $urlbase = "http://fhem.de/fhemupdate4/svn/";
 
 #####################################
 sub
@@ -28,65 +24,70 @@ sub
 CommandCULflash($$)
 {
   my ($cl, $param) = @_;
-  my $modpath = (-d "updatefhem.dir" ? "updatefhem.dir":$attr{global}{modpath});
-  my $moddir = "$modpath/FHEM";
-
   my %ctypes = (
-    CUL_V2     => "at90usb162",
-    CUL_V2_HM  => "at90usb162",
-    CUL_V3     => "atmega32u4",
-    CUL_V4     => "atmega32u2",
+    CUL_V2 =>
+      { cmd => 'dfu-programmer at90usb162 erase && '.
+               'dfu-programmer at90usb162 flash $filepath && '.
+               'dfu-programmer at90usb162 start',
+        flash => 'CUL_SimpleWrite($defs{$device},"B01");sleep(4); undef;' },
+    CUL_V2_HM =>
+      { cmd => 'dfu-programmer at90usb162 erase && '.
+               'dfu-programmer at90usb162 flash $filepath && '.
+               'dfu-programmer at90usb162 start',
+        flash => 'CUL_SimpleWrite($defs{$device},"B01");sleep(4); undef;' },
+    CUL_V3 => 
+      { cmd => 'dfu-programmer atmega32u4 erase && '.
+               'dfu-programmer atmega32u4 flash $filepath && '.
+               'dfu-programmer atmega32u4 start',
+        flash => 'CUL_SimpleWrite($defs{$device},"B01");sleep(4); undef;' },
+    CUL_V4 =>
+      { cmd => 'dfu-programmer atmega32u2 erase && '.
+               'dfu-programmer atmega32u2 flash $filepath && '.
+               'dfu-programmer atmega32u2 start',
+        flash => 'CUL_SimpleWrite($defs{$device},"B01");sleep(4); undef' },
   );
+
   my @a = split("[ \t]+", $param);
-  return "Usage: CULflash <Fhem-CUL-Device> <CUL-type>, ".
-                "where <CUL-type> is one of ". join(" ", sort keys %ctypes)
-      if(!(int(@a) == 2 &&
-          ($a[0] eq "none" || ($defs{$a[0]} && $defs{$a[0]}{TYPE} eq "CUL")) &&
-          $ctypes{$a[1]}));
+  return "Usage: CULflash [FHEM-Device|none] TYPE>, ".
+                "where TYPE is one of ". join(" ", sort keys %ctypes)
+      if(int(@a)!=2 || !($a[0] eq "none" || $defs{$a[0]}) || !$ctypes{$a[1]});
 
-  my $cul  = $a[0];
-  my $target = $a[1];
+  my $device = $a[0];
+  my $type = $a[1];
+  my $filename = $a[1].".hex";
+  my $fwdir = $attr{global}{modpath} . "/FHEM/firmware";
+  my $filepath = "$fwdir/$filename";
 
   ################################
-  # First get the index file to prove the file size
-  my $filetimes = GetFileFromURL("$host$path$file");
-  return "Can't get $host$path$file" if(!$filetimes);
-
-  my %filesize;
-  foreach my $l (split("[\r\n]", $filetimes)) {
-    chomp($l);
-    next if ($l !~ m/^UPD (20\d\d-\d\d-\d\d_\d\d:\d\d:\d\d) (\d+) (.*)$/);
-    $filesize{$3} = $2;
+  # Get the firmware file:
+  if(! -d $fwdir) {
+    mkdir($fwdir) || return "$fwdir: $!";
   }
-  return "FHEM/$target.hex is not found in $host$path$file"
-        if(!$filesize{"FHEM/$target.hex"});
+  my $content = GetFileFromURL("$urlbase/FHEM/firmware/$filename");
+  return "Cannot get $urlbase/FHEM/firmware/$filename"
+    if(!$content);
+  if($content !~ m/:00000001FF/) {
+    Log3 undef, 3, $content;
+    return "The retrieved $filename is not a correct .hex file";
+  }
 
-  ################################
-  # Now get the firmware file:
-  my $content = GetFileFromURL("$host$path/FHEM/$target.hex");
-  return "File size for $target.hex does not correspond to $file entry"
-          if(length($content) ne $filesize{"FHEM/$target.hex"});
-  my $localfile = "$moddir/$target.hex";
+  my $localfile = "$filepath";
   open(FH,">$localfile") || return "Can't write $localfile";
   print FH $content;
   close(FH);
 
-  my $cmd = "($dfu MCU erase && $dfu MCU flash TARGET && $dfu MCU start) 2>&1";
-  my $mcu = $ctypes{$target};
-  $cmd =~ s/MCU/$mcu/g;
-  $cmd =~ s/TARGET/$localfile/g;
-
-  if($cul ne "none") {
-    CUL_SimpleWrite($defs{$cul}, "B01");
-    sleep(4);     # B01 needs 2 seconds for the reset
+  if($device ne "none" && $ctypes{$type}{flash}) {
+    my $ret = eval $ctypes{$type}{flash};
+    Log 1, "CULflash $device: $ret"
+      if($ret);
   }
+  my $cmd = eval "return \"$ctypes{$type}{cmd};\"";
   Log3 undef, 1, "CULflash $cmd";
-  my $result = `$cmd`;
+  my $result = `($cmd) 2>&1`;
   Log3 undef, 1, "CULflash $result";
   return $result;
 }
 
-# vim: ts=2:et
 1;
 
 =pod
@@ -95,15 +96,18 @@ CommandCULflash($$)
 <a name="CULflash"></a>
 <h3>CULflash</h3>
 <ul>
-  <code>CULflash &lt;CUL-Name&gt; &lt;CUL-Version&gt;</code> <br>
+  <code>CULflash [fhem-device|none]; &lt;TYPE&gt;</code> <br>
   <br>
-    Download the CUL firmware from a nightly SVN chekout and flash the
-    hardware. Currently only the CUL is supported with its versions:
+    Download the firmware from a nightly SVN chekout and flash the
+    hardware.
+    Currently the CUL is supported with its versions:
     CUL_V2, CUL_V2_HM, CUL_V3, CUL_V4.<br>
-    <b>Note:</b> dfu-programmer has to be installed in the path, this is
-    already the case with the Fritz!Box 7390 image from fhem.de<br>
+    If the fhem-device is none, than the inserted device must already be in the
+    flash-mode.<br>
+    <b>Note:</b>for flashing the CUL dfu-programmer has to be installed in the
+    path, this is already the case with the Fritz!Box 7390 image from
+    fhem.de<br>
 
-    If the CUL is not yet flashed, then specify "none" as CUL-Name.
     Example:
     <ul>
     <code>CULflash CUL CUL_V3<br>
@@ -113,6 +117,33 @@ CommandCULflash($$)
     normal on the FB7390.
 </ul>
 
-
 =end html
+
+=begin html_DE
+
+<a name="CULflash"></a>
+<h3>CULflash</h3>
+<ul>
+  <code>CULflash [fhem-device|none]; &lt;TYPE&gt;</code> <br>
+  <br>
+    L&auml;dt die spezifizierte Firmware von fhem.de und programmiert das
+    angeschlossene Ger&auml;t.
+    Z.Zt unterst&uuml;tzt wird das CUL und folgende Versionen:
+    CUL_V2, CUL_V2_HM, CUL_V3, CUL_V4.<br>
+    Falls als fhem-device none angegeben wurde, dann muss sich das
+    angeschlossene Ger&auml;t bereits in flash-mode befinden.<br>
+    <b>Achtung:</b>F&uuml;r CUL flashen muss dfu-programmer installiert und im
+    Pfad auffindbar sein, das ist der Fall bei dem Fritz!BOX 7390 Paket von
+    fhem.de<br>
+
+    Beispiele:
+    <ul>
+    <code>CULflash CUL CUL_V3<br>
+          CULflash none CUL_V3</code>
+    </ul>
+    Achtung: die Meldung "dfu-programmer: failed to release interface 0." ist
+    auf der FB7390 "normal".
+</ul>
+
+=end html_DE
 =cut
