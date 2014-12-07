7eeac61f69e0e3baacf1a4fcd68987358b5b2258
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2014-12-07 11:41:33 +0000

    HttpUtils.pm: Change HTTPHEADER to httpheader (Forum #29785)
    git-svn-id: https://svn.fhem.de/fhem/trunk@7155 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/HttpUtils.pm
+++ fhem/FHEM/HttpUtils.pm
@@ -293,7 +293,7 @@ HttpUtils_ParseAnswer($$)
   $ret=~ s/(.*?)\r\n\r\n//s; # Not greedy: switch off the header.
   return ("", $ret) if(!defined($1));
 
-  $hash->{HTTPHEADER} = $1;
+  $hash->{httpheader} = $1;
   my @header= split("\r\n", $1);
   my @header0= split(" ", shift @header);
   my $code= $header0[1];
