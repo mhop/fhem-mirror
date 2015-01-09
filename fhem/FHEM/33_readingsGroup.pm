94c2e4841af9defd6fdd595165bbe37f21b72c93
justme1968 <justme1968@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2015-01-09 11:56:01 +0000

    33_readingsGroup.pm: allow devspec :FILTER= expressions in device selection
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@7478 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/33_readingsGroup.pm
+++ fhem/FHEM/33_readingsGroup.pm
@@ -96,7 +96,19 @@ readingsGroup_updateDevices($;$)
     } else {
       my @device = split(":", $param);
 
-      if($device[0] =~ m/(.*)=(.*)/) {
+      if( $device[1] =~ m/^FILTER=/ ) {
+        my $devspec = shift(@device);
+        while( @device && $device[0] =~ m/^FILTER=/ ) {
+          $devspec .= ":";
+          $devspec .= shift(@device)
+        }
+        my $regex =  $device[0];
+        foreach my $d (devspec2array($devspec)) {
+          $list{$d} = 1;
+          push @devices, [$d,$regex];
+        }
+
+      } elsif($device[0] =~ m/(.*)=(.*)/) {
         my ($lattr,$re) = ($1, $2);
         foreach my $d (sort keys %defs) {
           next if( IsIgnored($d) );
@@ -105,6 +117,7 @@ readingsGroup_updateDevices($;$)
           $list{$d} = 1;
           push @devices, [$d,$device[1]];
         }
+
       } elsif($device[0] =~ m/(.*)&(.*)/) {
         my ($lattr,$re) = ($1, $2);
         foreach my $d (sort keys %attr) {
@@ -114,11 +127,14 @@ readingsGroup_updateDevices($;$)
           $list{$d} = 1;
           push @devices, [$d,$device[1]];
         }
+
       } elsif($device[0] =~ m/^<.*>$/) {
         push @devices, [$device[0]];
+
       } elsif( defined($defs{$device[0]}) ) {
         $list{$device[0]} = 1;
         push @devices, [@device];
+
       } else {
         foreach my $d (sort keys %defs) {
           next if( IsIgnored($d) );
@@ -966,7 +982,7 @@ readingsGroup_Notify($$)
                       || ($regex =~ m/@\{/ && $regex !~ m/}$/) )    #handle , in reading@{...}
                  && defined($list[++$i]) ) {
             $regex .= ",". $list[$i];
-          } 
+          }
           next if( $reading eq "state" && !$show_state && (!defined($regex) || $regex ne "state") );
           my $modifier = "";
           if( $regex && $regex =~ m/^([+?!]*)(.*)/ ) {
@@ -1217,6 +1233,7 @@ readingsGroup_Attr($$$;$)
       <li>&lt;device&gt; can be of the form ATTRIBUTE&VALUE where ATTRIBUTE is the name of an attribute and VALUE is a regex.</li>
       <li>&lt;device&gt; can be of the form &lt;STRING&gt; or &lt;{perl}&gt; where STRING or the string returned by perl is
           inserted as a line in the readings list. skipped if STRING is undef.</li>
+      <li>&lt;device&gt; can be a devspec (see <a href="#devspec">devspec</a>) with at least one FILTER expression.</li>
       <li>If regex is a comma separatet list the reading values will be shown on a single line.</li>
       <li>If regex starts with a '+' it will be matched against the internal values of the device instead of the readings.</li>
       <li>If regex starts with a '?' it will be matched against the attributes of the device instead of the readings.</li>
