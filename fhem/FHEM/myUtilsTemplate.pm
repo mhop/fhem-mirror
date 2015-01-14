c8f64ce81fa61545ce589c23293144329f98d2bc
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2015-01-14 18:31:44 +0000

    myUtilsTemplate.pm: remove 99_Utils from the FHEMWEB Edit list (Forum #31932)
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@7570 2b470e98-0d58-463d-a4d8-8e2adae1ed80

---
+++ fhem/FHEM/myUtilsTemplate.pm
@@ -0,0 +1,22 @@
+##############################################
+# $Id$
+#
+# Save this file as 99_myUtils.pm, and create your own functions in the new
+# file. They are then available in every Perl expression.
+
+package main;
+
+use strict;
+use warnings;
+use POSIX;
+
+sub
+myUtils_Initialize($$)
+{
+  my ($hash) = @_;
+}
+
+# Enter you functions below _this_ line.
+
+
+1;
