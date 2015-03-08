95189691ee04ffe1e196049f9730aaac15f636e8
loredo <loredo@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2015-03-08 11:41:16 +0000

    20_GUEST, 20_ROOMMATE: add FILTER to notify example in commandref
    git-svn-id: https://svn.fhem.de/fhem/trunk@8171 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/20_GUEST.pm
+++ fhem/FHEM/20_GUEST.pm
@@ -1068,7 +1068,7 @@ sub GUEST_StartInternalTimers($$) {
           <br>
           If you are using the <a href="#GEOFANCY">GEOFANCY</a> module, you can easily have your location updated with GEOFANCY events by defining a simple NOTIFY-trigger like this:<br>
           <br>
-          <code>define n_rg_Guest.location notify geofancy:currLoc_Guest.* set rg_Guest location $EVTPART1</code><br>
+          <code>define n_rg_Guest.location notify geofancy:currLoc_Guest.* set rg_Guest location:FILTER=STATE!=$EVTPART1 $EVTPART1</code><br>
           <br>
           By defining geofencing zones called 'home' and 'wayhome' in the iOS app, you automatically get all the features of automatic state changes described above.
         </div>
@@ -1338,7 +1338,7 @@ sub GUEST_StartInternalTimers($$) {
           <br>
           Wenn Sie auch das <a href="#GEOFANCY">GEOFANCY</a> Modul verwenden, können Sie das Reading 'location' ganz einfach über GEOFANCY Ereignisse aktualisieren lassen. Definieren Sie dazu einen NOTIFY-Trigger wie diesen:<br>
           <br>
-          <code>define n_rg_Manfred.location notify geofancy:currLoc_Manfred.* set rg_Manfred location $EVTPART1</code><br>
+          <code>define n_rg_Manfred.location notify geofancy:currLoc_Manfred.* set rg_Manfred location:FILTER=STATE!=$EVTPART1 $EVTPART1</code><br>
           <br>
           Durch das Anlegen von Geofencing-Zonen mit den Namen 'home' und 'wayhome' in der iOS App werden zukünftig automatisch alle Statusänderungen wie oben beschrieben durchgeführt.
         </div>
