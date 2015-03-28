6f6c513bb2bba0b594e3954d428c612fe96b5a40
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2015-03-28 20:32:59 +0000

    contrib/zwave_configconvert.pl: update xmls from the openzwave project (Forum #35416)
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@8316 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/zwave_configconvert.pl
+++ fhem/contrib/zwave_configconvert.pl
@@ -1,8 +1,35 @@
 #!/usr/bin/perl
 
-# Usage: contrib/zwave_configconvert.pl priv/zwave/open-zwave-read-only/config |
-#        gzip > FHEM/lib/openzwave_deviceconfig.xml.gz
-#
+# Details in Forum #35416
+
+if(@ARGV == 0 || ($ARGV[0] =~ m/^-/ && $ARGV[0] ne "-d")) {
+  print "Usage:\n".
+    "  cd open-zwave\n".
+    "  perl .../contrib/zwave_configconvert.pl config |\n".
+    "  gzip > .../FHEM/lib/openzwave_deviceconfig.xml.gz\n".
+    "or\n".
+    "  cd open-zwave\n".
+    "  gzip -d < .../FHEM/lib/openzwave_deviceconfig.xml.gz |\n".
+    "  perl .../contrib/zwave_configconvert.pl -d\n";
+  exit 1;
+}
+
+if($ARGV[0] eq "-d") {
+  while(my $l = <STDIN>) {
+    next if($l !~ m/<Product sourceFile="([^"]*)"/);
+    my $f = $1;
+    next if(-f "config/$f");
+    print("Creating: config/$f\n");
+    open(FH, ">config/$f") || die("open config/$f: $!\n");
+    print FH $l;
+    while($l = <STDIN>) {
+      print FH $l;
+      last if($l =~ m,</Product>,);
+    }
+    close(FH);
+  }
+  exit(0);
+}
 
 print '<?xml version="1.0" encoding="utf-8"?>', "\n";
 print "<ProductList>\n";
@@ -16,20 +43,20 @@ foreach my $file (`find $ARGV[0] -name \*.xml`) {
   while(my $l = <FH>) {
     next if($l =~ m/^<\?xml/);
     chomp($l);
-    $l =~ s/^<Product .*/<Product sourceFile="$name">/;
+    $l =~ s/^<Product.*xmlns.*/<Product sourceFile="$name">/;
     $l =~ s/\r//g;
-    $l =~ s/^[ \t]*//g;
-    $l =~ s/[ \t]*$//g;
+    $l =~ s/\t/  /g;
+    #$l =~ s/^ *//g;
+    $l =~ s/ *$//g;
     next if($l eq "");
     if($l !~ m/>$/ || $l =~ m/^<Help>$/) { $buffer .= " ".$l; next; }
     if($buffer && $l =~ m/>$/) { $l = "$buffer $l"; $buffer=""; }
     $l =~ s/<!--.*-->//g;
-    $l =~ s/^[ \t]*//g;
-    $l =~ s/[ \t]*$//g;
-    print "$l\n" if($l);
+    $l =~ s/ *$//g;
+    print $l,"\n" if($l);
   }
   close(FH);
   print $buffer if($buffer);
-  print "\n"; # Some files are not NL terminated!
+  print "\n"; # One empty line between products
 }
 print "</ProductList>\n";
