c345010426cfeee3db7a56b00407e8d79ca6fa51
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2015-06-22 19:21:15 +0000

    fhem.pl: typo (Forum #38276)
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@8804 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/fhem.pl
+++ fhem/fhem.pl
@@ -1492,7 +1492,7 @@ ReplaceSetMagic(@)       # Forum #38276
   my $a = join(" ", @_);
 
   $a =~ s/\[([a-z0-9._]+):([A-z0-9._]+)\]/{
-    my $x = ReadingsVal($1,$2,""); $x eq "" ? "$1:$2" : "[$x]"
+    my $x = ReadingsVal($1,$2,""); $x eq "" ? "[$1:$2]" : $x
   }/egi;
 
   $a =~ s/{([^}]+)}/{
