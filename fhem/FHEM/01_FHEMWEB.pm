98bb237c7a3f08a7ec03f27640c1a3addfcd58f1
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2015-06-26 05:39:23 +0000

    01_FHEMWEB.pm: remove duplicates in detail select by Markus (Forum #38513)
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@8828 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/01_FHEMWEB.pm
+++ fhem/FHEM/01_FHEMWEB.pm
@@ -1024,7 +1024,8 @@ FW_detailSelect(@)
 {
   my ($d, $cmd, $list, $param) = @_;
   return if(!$list || $FW_hiddenroom{input});
-  my @al = sort map { s/:.*//;$_ } split(" ", $list);
+  my @al = map { s/:.*//;$_ } split(" ", $list);
+  @al = sort keys { map { $_ => 1 } @al }; # remove duplicate items in list
 
   my $selEl = (defined($al[0]) ? $al[0] : " ");
   $selEl = $1 if($list =~ m/([^ ]*):slider,/); # promote a slider if available
