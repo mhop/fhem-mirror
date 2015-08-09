b4844097a9b87b5f89be44cca6e937837b014da6
loredo <loredo@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2015-08-09 13:16:53 +0000

    GEOFANCY: fix typo
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@9044 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_GEOFANCY.pm
+++ fhem/FHEM/98_GEOFANCY.pm
@@ -382,7 +382,7 @@ m/(19|20)\d\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T([0-2][0-3]):([0-5][0-9])
     readingsBeginUpdate($hash);
 
     # validate date
-    if ( $date != "" ) {
+    if ( $date ne "" ) {
         $hash->{".updateTime"}      = GEOFANCY_ISO8601UTCtoLocal($date);
         $hash->{".updateTimestamp"} = FmtDateTime( $hash->{".updateTime"} );
         $time                       = $hash->{".updateTimestamp"};
