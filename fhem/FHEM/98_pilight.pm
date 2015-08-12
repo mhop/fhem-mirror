98debc1cac856625e98af2ca03af8689d22a4790
andreas-fey <andreas-fey@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2015-08-12 13:46:42 +0000

    98_pilight: more protocols and rewrite
    git-svn-id: https://svn.fhem.de/fhem/trunk@9060 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_pilight.pm
+++ fhem/FHEM/98_pilight.pm
@@ -13,7 +13,7 @@ pilight_Initialize($)
 
   $hash->{SetFn}     = "pilight_Set";
   $hash->{DefFn}     = "pilight_Define";
-  $hash->{AttrList}  = "protocol housecode number systemcode unitcode id remote_ip remote_port useOldVersion rawCodeOn rawCodeOff";
+  $hash->{AttrList}  = "protocol housecode number systemcode unitcode id remote_ip remote_port useOldVersion rawCodeOn rawCodeOff follow-on-for-timer";
 }
 
 # housecode == id und number == unitcode
@@ -27,8 +27,9 @@ pilight_Set($@)
   return "no set value specified" if(int(@a) < 2);
   return "on off" if($a[1] eq "?");
   
-  shift @a;
-  my $command = shift @a;
+  my $command = $a[1];
+  my $c_timer= "";
+  if(defined($a[2])) { $c_timer=$a[2]; }
   
   Log 3, "pilight command: $command";
 
@@ -36,6 +37,13 @@ pilight_Set($@)
   {
     $rc = commit($hash, 1);
   } 
+  elsif($command eq "on-for-timer")
+  {
+    InternalTimer(gettimeofday()+$c_timer, "pilight_on_timeout",$hash, 0);
+    # on-for-timer is now a on.
+    $rc = commit($hash, 1);
+  }
+#  elsif
   else
   {
     $rc = commit($hash, 0);
@@ -50,6 +58,22 @@ pilight_Set($@)
   
   return undef;
 }
+
+###################################
+sub 
+pilight_on_timeout($)
+{
+  my ($hash) = @_;
+  my @a;
+
+  $a[0]=$hash->{NAME};
+  $a[1]="off"; 
+
+  pilight_Set($hash,@a);
+
+  return undef;
+}
+
 
 sub
 pilight_Define($$)
@@ -101,48 +125,33 @@ sub commit
         return undef
   };
 
-  my $data = '{ "message": "client sender" }';
+
+  my $data = $useOldVersion ? '{ "message": "client sender" }' : '{ "action": "identify" }';
   $socket->send($data);
   $socket->recv($data,1024);
 
   $data =~ s/\n/ /g;
-  if ( $data !~ /accept client/ ) {
-	Log 3, "pilight: ERROR. No handshake with pilight-daemon. Received: >>>$data<<<\n";
-        return undef
+  if ( $data !~ /accept client/ && $data !~ /success/)  # "accept client" < v6, 
+  { 
+      Log 3, "pilight: ERROR. No handshake with pilight-daemon. Received: >>>$data<<<\n";
+      return undef
   };
 
   my $code = "{\"protocol\":[ \"$protocol\" ],";
-  if($useOldVersion) {
-    switch( $protocol ) {
-	case 'kaku_switch' 	{ $code = $code . "\"id\":\"$housecode\", \"unit\":\"$unit\",\"$param\":\"1\""}
-	case 'quigg_switch' 	{ $code = $code . "\"id\":\"$housecode\", \"unit\":\"$unit\",\"$param\":\"1\""}
-	case 'elro'        	{ $code = $code . "\"systemcode\":\"$systemcode\", \"unitcode\":\"$unit\",\"$param\":\"1\""}
-	case 'elro_he'     	{ $code = $code . "\"systemcode\":\"$systemcode\", \"unitcode\":\"$unit\",\"$param\":\"1\""}
-	case 'elro_hc'     	{ $code = $code . "\"systemcode\":\"$systemcode\", \"unitcode\":\"$unit\",\"$param\":\"1\""}
-	case 'silvercrest'     	{ $code = $code . "\"systemcode\":\"$systemcode\", \"unitcode\":\"$unit\",\"$param\":\"1\""}
-	case 'pollin'     	{ $code = $code . "\"systemcode\":\"$systemcode\", \"unitcode\":\"$unit\",\"$param\":\"1\""}
-	case 'mumbi'     	{ $code = $code . "\"systemcode\":\"$systemcode\", \"unitcode\":\"$unit\",\"$param\":\"1\""}
-	case 'intertechno_old'  { $code = $code . "\"id\":\"$systemcode\", \"unit\":\"$unit\",\"$param\":\"1\""}
+  if( $protocol eq 'raw')
+  {
+    switch( $param ) {
+      case 'on' 		{ $code = $code . "\"code\":\"$rawCodeOn\""} # on
+      case 'off' 		{ $code = $code . "\"code\":\"$rawCodeOff\""} #off	
     }
-  } 
-  else {
-    if( $protocol eq 'raw')
-    {
-        Log 3, "pilight protocol: $protocol";
-        Log 4, "pilight raw param: $param";
-        Log 4, "pilight rawCodeOn: $rawCodeOn";
-        Log 4, "piligth rawCodeCff: $rawCodeOff";
-       
-	switch( $param ) {
-          case 'on' 		{ $code = $code . "\"code\":\"$rawCodeOn\""} # on
-          case 'off' 		{ $code = $code . "\"code\":\"$rawCodeOff\""} #off	
-        }
-    }
-    else
-    {
+  }
+  else
+  {
       switch( $protocol ) {
 	case 'kaku_switch' 	{ $code = $code . "\"id\":$housecode, \"unit\":$unit,\"$param\":1"}
 	case 'quigg_switch' 	{ $code = $code . "\"id\":$housecode, \"unit\":$unit,\"$param\":1"}
+	case 'quigg_gt7000' 	{ $code = $code . "\"id\":$housecode, \"unit\":$unit,\"$param\":1"}
+	case 'quigg_gt1000' 	{ $code = $code . "\"id\":$housecode, \"unit\":$unit,\"$param\":1"}
 	case 'elro'        	{ $code = $code . "\"systemcode\":$systemcode, \"unitcode\":$unit,\"$param\":1"}
 	case 'elro_he'     	{ $code = $code . "\"systemcode\":$systemcode, \"unitcode\":$unit,\"$param\":1"}
 	case 'elro_hc'     	{ $code = $code . "\"systemcode\":$systemcode, \"unitcode\":$unit,\"$param\":1"}
@@ -153,16 +162,15 @@ sub commit
 	case 'impuls'     	{ $code = $code . "\"systemcode\":$systemcode, \"programcode\":$unit,\"$param\":1"}
 	case 'rsl366'     	{ $code = $code . "\"systemcode\":$systemcode, \"programcode\":$unit,\"$param\":1"}
 	case 'intertechno_old'  { $code = $code . "\"id\":$systemcode, \"unit\":$unit,\"$param\":1"}
-	case 'clarus_switch'    { $code = $code . "\"id\":$systemcode, \"unit\":$unit,\"$param\":1"}
+	case 'clarus_switch'    { $code = $code . "\"id\":\"$systemcode\", \"unit\":$unit,\"$param\":1"}
 	case 'rev1_switch' 	{ $code = $code . "\"id\":\"$systemcode\", \"unit\":$unit,\"$param\":1"}
 	case 'rev2_switch'	{ $code = $code . "\"id\":\"$systemcode\", \"unit\":$unit,\"$param\":1"}
 	case 'rev3_switch'	{ $code = $code . "\"id\":\"$systemcode\", \"unit\":$unit,\"$param\":1"}
 	}
-     }
   }
   $code = $code . '}';
 		  
-  $data = "{ \"message\": \"send\", \"code\": $code}";
+  $data = $useOldVersion ? "{ \"message\": \"send\", \"code\": $code}" : "{ \"action\": \"send\", \"code\": $code}";
   Log 3, "pilight data: $data";
 
   $socket->send($data);
@@ -186,7 +194,7 @@ sub commit
     <br/>
     <br/>
     Defines a module for setting pilight compartible switches on or off. See <a href="http://www.sweetpi.de/blog/258/funksteckdosen-mit-dem-raspberry-pi-und-pilight-schalten">Sweetpi</a>.<br><br>
-    Supported protocols: kaku_switch, quigg_switch, elro_he, elro_hc, silvercrest, pollin, brennenstuhl, mumbi, impuls, rsl366, rev1_switch, rev2_switch, clarus_switch, raw, and intertechno_old. If you need more, just write an issue!<br/><br/>
+    Supported protocols: kaku_switch, quigg_*, elro_he, elro_hc, silvercrest, pollin, brennenstuhl, mumbi, impuls, rsl366, rev1_switch, rev2_switch, clarus_switch, raw, and intertechno_old. If you need more, just write an issue!<br/><br/>
     Example:
     <ul>
       <code>define Weihnachtsbaum pilight kaku_switch</code><br>
@@ -196,7 +204,7 @@ sub commit
     <br/>
 	If your pilight server does not run on localhost, please set both the attributes <b>remote_ip</b> and <b>remote_port</b>. If you are running pilight >3.0, then please <b>define the port used by pilight</b> settings: http://www.pilight.org/getting-started/settings/; fhem-plight uses 5000 by default.
     <br/>
-    <b>This version is written for pilight 5.0. If you run a version < 3.0, please set the following attribute:</b>
+    <b>This version is written for pilight 6.0. If you run a prior version, please set the following attribute:</b>
       <code> attr Weihnachtsbaum useOldVersion 1</code>
   </ul>
 
@@ -206,7 +214,7 @@ sub commit
     <li><a name="protocol"><code>attr &lt;name&gt; protocol &lt;string&gt;</code></a>
                 <br />Protocol used in pilight, e.g. "kaku_switch"</li>
     <li><a name="housecode"><code>attr &lt;name&gt; housecode &lt;string&gt;</code></a>
-                <br />Housecode used in pilight (for protocol kaku*, clarus_switch, rev1_switch, rev2_switch, rev3_switch, quigg_switch)</li>
+                <br />Housecode used in pilight (for protocol kaku*, clarus_switch, rev1_switch, rev2_switch, rev3_switch, quigg_switch, quigg_gt1000, quigg_gt7000)</li>
     <li><a name="systemcode"><code>attr &lt;name&gt; systemcode &lt;string&gt;</code></a>
                 <br />Systemcode of your switch (for protocol elso, elro_he, elro_hc, silvercrest, impuls, rsl366, pollin, mumbi, brennenstuhl, intertechno_old)</li>
     <li><a name="unitcode"><code>attr &lt;name&gt; unitcode &lt;string&gt;</code></a>
