78d74ecde8fc0297a6ae2f1ac9ef98c647af6cbc
wherzig <wherzig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2016-02-12 19:42:11 +0000

    45_TR6_TRX_ELSE.pm: regex TRX_ELSE fixed added
    git-svn-id: https://svn.fhem.de/fhem/trunk@10803 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/46_TRX_ELSE.pm
+++ fhem/FHEM/46_TRX_ELSE.pm
@@ -41,7 +41,7 @@ TRX_ELSE_Initialize($)
 {
   my ($hash) = @_;
 
-  $hash->{Match}     = "^..(0[0-f]|1[a-f]|2[1-f]|3[0-f]|4[0-d]|4f|53|59|5e|5f|[6-f][0-f]).*";
+  $hash->{Match}     = "^..(0[0-9a-f]|1[a-f]|2[1-9a-f]|3[0-9a-f]|4[0-9a-d]|4f|53|59|5e|5f|[6-9a-f][0-9a-f]).*"
   $hash->{DefFn}     = "TRX_ELSE_Define";
   $hash->{UndefFn}   = "TRX_ELSE_Undef";
   $hash->{ParseFn}   = "TRX_ELSE_Parse";
