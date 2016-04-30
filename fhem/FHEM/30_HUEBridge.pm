f78b6f27b603b0249c03bebb9697f2f8d2d99417
justme1968 <justme1968@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2016-04-30 20:10:10 +0000

    HUE: display group class
    git-svn-id: https://svn.fhem.de/fhem/trunk@11350 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/30_HUEBridge.pm
+++ fhem/FHEM/30_HUEBridge.pm
@@ -518,10 +518,11 @@ HUEBridge_Get($@)
       my $fhem_name ="";
       $fhem_name = $modules{HUEDevice}{defptr}{$code}->{NAME} if( defined($modules{HUEDevice}{defptr}{$code}) );
       $result->{$key}{type} = '' if( !defined($result->{$key}{type}) );     #deCONZ fix
+      $result->{$key}{class} = '' if( !defined($result->{$key}{class}) );   #deCONZ fix
       $result->{$key}{lights} = [] if( !defined($result->{$key}{lights}) ); #deCONZ fix
-      $ret .= sprintf( "%2i: %-15s %-15s %-15s %s\n", $key, $result->{$key}{name}, $fhem_name, $result->{$key}{type},  join( ",", @{$result->{$key}{lights}} ) );
+      $ret .= sprintf( "%2i: %-15s %-15s %-15s %-15s %s\n", $key, $result->{$key}{name}, $fhem_name, $result->{$key}{type}, $result->{$key}{class}, join( ",", @{$result->{$key}{lights}} ) );
     }
-    $ret = sprintf( "%2s  %-15s %-15s %-15s %s\n", "ID", "NAME", "FHEM", "TYPE", "LIGHTS" ) .$ret if( $ret );
+    $ret = sprintf( "%2s  %-15s %-15s %-15s %-15s %s\n", "ID", "NAME", "FHEM", "TYPE", "CLASS", "LIGHTS" ) .$ret if( $ret );
     return $ret;
 
   } elsif($cmd eq 'scenes') {
