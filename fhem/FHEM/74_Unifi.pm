55b9b46145d507394760365d3ee5e64ef24121be
rapster <rapster@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2016-08-19 18:11:24 +0000

    74_Unifi.pm: added summary for new commandref
    git-svn-id: https://svn.fhem.de/fhem/trunk@11990 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/74_Unifi.pm
+++ fhem/FHEM/74_Unifi.pm
@@ -1177,6 +1177,9 @@ sub Unifi_Whowasi() { return (split('::',(caller(2))[3]))[1] || ''; }
 
 
 =pod
+=item device
+=item summary    Interpret / control of Ubiquiti Networks UniFi-controller
+=item summary_DE Auswertung / Steuerung eines Ubiquiti Networks UniFi-Controller
 =begin html
 
 <a name="Unifi"></a>
