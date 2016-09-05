e403053ff5fa2f7e136f2eb4afdf10a4ae659f4c
tobiasfaust <tobiasfaust@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2016-09-05 07:02:35 +0000

    contrib/dblog/db_create_mysql.sql: changing datatypes to keep in sync contrib/dblog/db_create_sqlite.sql: changing datatypes to keep in sync
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@12118 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/dblog/db_create_mysql.sql
+++ fhem/contrib/dblog/db_create_mysql.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE `fhem` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE USER 'fhemuser'@'%' IDENTIFIED BY 'fhempassword';
-CREATE TABLE `fhem`.`history` (TIMESTAMP TIMESTAMP, DEVICE varchar(32), TYPE varchar(32), EVENT varchar(512), READING varchar(32), VALUE varchar(32), UNIT varchar(32));
-CREATE TABLE `fhem`.`current` (TIMESTAMP TIMESTAMP, DEVICE varchar(32), TYPE varchar(32), EVENT varchar(512), READING varchar(32), VALUE varchar(32), UNIT varchar(32));
+CREATE TABLE `fhem`.`history` (TIMESTAMP TIMESTAMP, DEVICE varchar(64), TYPE varchar(64), EVENT varchar(512), READING varchar(64), VALUE varchar(128), UNIT varchar(32));
+CREATE TABLE `fhem`.`current` (TIMESTAMP TIMESTAMP, DEVICE varchar(64), TYPE varchar(64), EVENT varchar(512), READING varchar(64), VALUE varchar(128), UNIT varchar(32));
 GRANT SELECT, INSERT, DELETE, UPDATE ON `fhem`.* TO 'fhemuser'@'%';
 CREATE INDEX Search_Idx ON `fhem`.`history` (DEVICE, READING, TIMESTAMP);
 
