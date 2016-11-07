5b251df2829961020ef9fc19bff0fc5ea2ecc20e
klauswitt <klauswitt@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2016-11-07 20:32:51 +0000

    52_I2C_MCP342x.pm: bugfix...again (definition parameters should work now)
    git-svn-id: svn://svn.code.sf.net/p/fhem/code/trunk@12524 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/52_I2C_MCP342x.pm
+++ fhem/FHEM/52_I2C_MCP342x.pm
@@ -90,22 +90,22 @@ sub I2C_MCP342x_Init($$) {
 	my ( $hash, $args ) = @_;
 	
 	my $name = $hash->{NAME};
-
-	 if (defined $args && int(@$args) > 1)
+	Log3 $hash, 5, "$hash->{NAME}: Init Argumente1: $args";
+	 if (defined $args && int(@$args) < 1)
  	{
-  	return "Define: Wrong syntax. Usage:\n" .
+  	Log3 $hash, 0, "Define: Wrong syntax. Usage:\n" .
          	"define <name> MCP342x [<i2caddress>] [<type>]";
  	}
 	 
  	if (defined (my $address = shift @$args)) {
-   	$hash->{I2C_Address} = $address =~ /^0.*$/ ? oct($address) : $address;
-   	return "$name I2C Address not valid" unless ($address < 128 && $address > 3);
+   	$hash->{I2C_Address} = $address =~ /^0x.*$/ ? oct($address) : $address;
+   	Log3 $hash, 0, "$name: I2C Address not valid" unless ($hash->{I2C_Address} < 128 && $hash->{I2C_Address} > 3);
  	} else {
 		$hash->{I2C_Address} = hex(MCP3422_I2C_ADDRESS);
 	}
 	
 	if (defined (my $channels = shift @$args)) {
-		$hash->{channels} = $channels if $channels == 2 || $channels == 4;
+		$hash->{channels} = ($channels == 4 ? 4 : 2);
 	} else {
 		$hash->{channels} = 2;
 	}
