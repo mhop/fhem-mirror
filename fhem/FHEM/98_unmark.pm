bf6779fff948ff3bc4b38c2e82e7f3f358cc8bca
betateilchen <betateilchen@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2017-01-22 18:51:42 +0000

    98_unmark.pm: counterpart to 98_mark.pm
    git-svn-id: https://svn.fhem.de/fhem/trunk@13191 2b470e98-0d58-463d-a4d8-8e2adae1ed80

---
+++ fhem/FHEM/98_unmark.pm
@@ -0,0 +1,48 @@
+# $Id$
+
+package main;
+use strict;
+use warnings;
+
+sub unmark_Initialize($$) {
+  $cmds{unmark} = {  Fn  => "CommandUnmark",
+                   Hlp =>"<TEMPORARY|VOLATILE>,unmark devices"};
+}
+
+sub CommandUnmark($$)
+{
+  my ($cl, $param) = @_;
+  my ($devspec,@marks) = split (" ",$param);
+  my @devices = devspec2array($devspec,undef);
+  my $ret = "";
+  
+  foreach my $m (@marks) {
+    $m = uc($m);
+    next if( $m ne "TEMPORARY" && $m ne "VOLATILE" );
+    foreach my $d (@devices){
+      delete $defs{$d}{$m}; 
+      $ret .= "$d unmarked as $m\n";
+    }
+  }  
+  return $ret;
+}
+
+1;
+
+=pod
+=item helper
+=item summary    mark devices for TEMPORARY or VOLATILE
+=item summary_DE markiert Ger&auml;te als TEMPORARY oder VOLATILE
+=item command
+=begin html
+
+<a name="unmark"></a>
+<h3>mark</h3>
+<ul>
+  <code>unmark &lt;devspec&gt; &lt;TEMPORARY|VOLATILE&gt;</code>
+  <br><br>
+</ul>
+
+=end html
+
+=cut
