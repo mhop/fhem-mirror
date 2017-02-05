c901b87ad541cd86620e72cdae440816bcd2eadf
loredo <loredo@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2017-02-05 14:39:57 +0000

    98_powerMap: fix sorting order, p.2
    git-svn-id: https://svn.fhem.de/fhem/trunk@13337 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_powerMap.pm
+++ fhem/FHEM/98_powerMap.pm
@@ -1398,11 +1398,7 @@ sub powerMap_power($$$;$) {
         elsif ( looks_like_number($num) ) {
             my ( $val1, $val2 );
 
-            foreach (
-                sort { $powerMap->{$reading}{$a} <=> $powerMap->{$reading}{$b} }
-                keys( %{ $powerMap->{$reading} } )
-              )
-            {
+            foreach ( sort { $a <=> $b } keys( %{ $powerMap->{$reading} } ) ) {
                 next unless ( looks_like_number($_) );
                 $val1 = $_ if ( $_ < $num );
                 $val2 = $_ if ( $_ > $num );
