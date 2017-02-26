6f2868f3857907c094e78103911b7d1481e772f0
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2017-02-26 12:30:46 +0000

    01_FHEMWEB.pm: fix typo (Forum #57377)
    git-svn-id: https://svn.fhem.de/fhem/trunk@13526 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/01_FHEMWEB.pm
+++ fhem/FHEM/01_FHEMWEB.pm
@@ -2708,7 +2708,7 @@ FW_longpollInfo($@)
   if($fmt && $fmt eq "JSON") {
     my @a;
     map { my $x = $_; #Forum 57377, ASCII 0-19 \ "
-          $x=~ s/([\x00-\x1f\x22\x5c\7f])/sprintf '\u%04x', ord($1)/ge;
+          $x=~ s/([\x00-\x1f\x22\x5c\x7f])/sprintf '\u%04x', ord($1)/ge;
           push @a,$x; } @_;
     return '["'.join('","', @a).'"]';
   } else {
