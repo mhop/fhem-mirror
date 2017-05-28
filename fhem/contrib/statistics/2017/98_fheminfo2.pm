d3f557e58a055f114246ec80d20d1d87dee41839
betateilchen <betateilchen@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2017-05-28 08:44:07 +0000

    fheminfo/statistics: changes after release tests
    git-svn-id: https://svn.fhem.de/fhem/trunk@14393 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/statistics/2017/98_fheminfo2.pm
+++ fhem/contrib/statistics/2017/98_fheminfo2.pm
@@ -1,22 +1,24 @@
-# $Id$
-################################################################
-#
-#  This script free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  The GNU General Public License can be found at
-#  http://www.gnu.org/copyleft/gpl.html.
-#  A copy is found in the textfile GPL.txt and important notices to the license
-#  from the author is found in LICENSE.txt distributed with these scripts.
-#
-#  This script is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-################################################################
+=for comment
+
+$Id$
+
+This script free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+The GNU General Public License can be found at
+http://www.gnu.org/copyleft/gpl.html.
+A copy is found in the textfile GPL.txt and important notices to the license
+from the author is found in LICENSE.txt distributed with these scripts.
+
+This script is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+=cut
+
 package main;
 use strict;
 use warnings;
@@ -25,16 +27,16 @@ use HttpUtils;
 
 my %fhemInfo =();
 
-sub fheminfo2_Initialize($$) {
+sub fheminfo_Initialize($$) {
   my %hash = (
     Fn  => "CommandFheminfo2",
-    uri => "http://fhem.j65.de/stats/statistics2.cgi",
+    uri => "https://fhem.de/stats/statistics2.cgi",
     Hlp => "[send],show or send Fhem statistics",
   );
-  $cmds{fheminfo2} = \%hash;
+  $cmds{fheminfo} = \%hash;
 }
 
-sub CommandFheminfo2($$) {
+sub CommandFheminfo($$) {
   my ($cl,$param) = @_;
   my @args = split("[ \t]+", $param);
   $args[0] = defined($args[0]) ? lc($args[0]) : "";
