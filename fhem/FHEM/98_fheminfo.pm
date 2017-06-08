ad2b8c189c8ac62fe3cf12b21c7bd3b7498f0cfd
betateilchen <betateilchen@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2017-06-08 17:33:13 +0000

    fheminfo: use new database model for statistics data
    git-svn-id: https://svn.fhem.de/fhem/trunk@14481 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_fheminfo.pm
+++ fhem/FHEM/98_fheminfo.pm
@@ -1,220 +1,181 @@
-################################################################
-#+$Id$
-#+vim: ts=2:et
-#
-#+ (c) 2012 Copyright: Martin Fischer (m_fischer at gmx dot de)
-#+ All rights reserved
-#
-#+ This script free software; you can redistribute it and/or modify
-#+ it under the terms of the GNU General Public License as published by
-#+ the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  The GNU General Public License can be found at
-#  http://www.gnu.org/copyleft/gpl.html.
-#  A copy is found in the textfile GPL.txt and important notices to the license
-#  from the author is found in LICENSE.txt distributed with these scripts.
-#
-#  This script is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-################################################################
+=for comment
+
+# $Id$
+
+This script free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+The GNU General Public License can be found at
+http://www.gnu.org/copyleft/gpl.html.
+A copy is found in the textfile GPL.txt and important notices to the license
+from the author is found in LICENSE.txt distributed with these scripts.
+
+This script is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+=cut
+
 package main;
 use strict;
 use warnings;
 use Config;
+use HttpUtils;
 
+my %fhemInfo =();
 
-sub CommandFheminfo($$);
-
-########################################
-sub
-fheminfo_Initialize($$)
-{
+sub fheminfo_Initialize($$) {
   my %hash = (
     Fn  => "CommandFheminfo",
+    uri => "https://fhem.de/stats/statistics2.cgi",
     Hlp => "[send],show or send Fhem statistics",
   );
   $cmds{fheminfo} = \%hash;
 }
 
-########################################
-sub
-CommandFheminfo($$)
-{
+sub CommandFheminfo($$) {
   my ($cl,$param) = @_;
-
-  # split arguments
-  my @args = split(/ +/,$param);
-
-  my $name = "fheminfo";
-  my %info;
-
-  my $module = "HTTP::Request::Common";
-  my $err = "Missing perl module '$module'. Please install this module first.";
-  if(!checkModule($module)) {
-    Log 1, "$name $err";
-    return $err;
-  }
-
-  $module = "LWP::UserAgent";
-  if(!checkModule($module)) {
-    Log 1, "$name $err";
-    return $err;
-  }
+  my @args = split("[ \t]+", $param);
+  $args[0] = defined($args[0]) ? lc($args[0]) : "";
+  my $doSend = ($args[0] eq 'send') ? 1 : 0;
 
   return "Unknown argument $args[0], usage: fheminfo [send]"
-    if(@args && lc($args[0]) ne "send");
+    if($args[0] ne "send" && $args[0] ne "");
 
   return "Won't send, as sendStatistics is set to 'never'."
-    if(@args && 
-       lc($args[0]) eq "send" &&
-       lc(AttrVal("global","sendStatistics","")) eq "never");
+    if($doSend &&  lc(AttrVal("global","sendStatistics","")) eq "never");
 
-  my $branch   = "DEVELOPMENT"; # UNUSED
-  my $release  = "5.8";
-  my $feature  = $featurelevel ? $featurelevel : $release;
-  my $os       = $^O;
-  my $arch     = $Config{"archname"};
-  my $perl     = $^V;
-  my $uniqueID = getUniqueId();
-  my $sendStatistics = AttrVal("global","sendStatistics",undef);
-  my $moddir   = $attr{global}{modpath}."/FHEM";
-  my $upTime = fhemUptime();
-  
-  my %official_module;
+  _fi2_Count();
 
-  opendir(DH, $moddir) || return("$moddir: $!");
-  foreach my $file (grep /^controls.*.txt$/, readdir(DH)) {
-    open(FH, "$moddir/$file") || next;
-    while(my $l = <FH>) {
-      $official_module{$1} = 1 if($l =~ m+^UPD.* FHEM/\d\d_(.*).pm+);
-    }
-    close(FH);
-  }
-  closedir(DH);
-  return "Can't read FHEM/controls_fhem.txt, execute update first."
-        if(!%official_module);
+  _fi2_Send() if $args[0] eq 'send';
 
-  foreach my $d (sort keys %defs) {
-    my $n = $defs{$d}{NAME};
-    my $t = $defs{$d}{TYPE};
-    my $m = "unknown";
-    $m = $defs{$d}{model} if( defined($defs{$d}{model}) );
-    $m = AttrVal($n,"model",$m);
-    if($official_module{$t} && !$defs{$d}{TEMPORARY} && !$attr{$d}{ignore}) {
-      $info{modules}{$t}{$n} = $m;
-    }
-  }
+  return _fi2_TelnetTable($doSend) if ($cl && $cl->{TYPE} eq 'telnet');
+  return _fi2_HtmlTable($doSend);
+}
 
-  $info{modules}{configDB}{configDB} = 'unknown' if (configDBUsed());
+################################################################
+# tools
+#
+sub _fi2_Count() {
+   my $uniqueID = getUniqueId();
+   my $release  = "5.8";
+   my $feature  = $featurelevel ? $featurelevel : $release;
+   my $os       = $^O;
+   my $arch     = $Config{"archname"};
+   my $perl     = sprintf("%vd", $^V);
 
-  my $str;
-  $str  = "Fhem info:\n";
-  $str .= sprintf("  Release%*s: %s FeatureLevel: %s\n",
-                        2, " ", $release, $feature);
-  $str .= sprintf("  OS%*s: %s\n",7," ",$os);
-  $str .= sprintf("  Arch%*s: %s\n",5," ",$arch);
-  $str .= sprintf("  Perl%*s: %s\n",5," ",$perl);
-  $str .= sprintf("  uniqueID%*s: %s\n",0," ",$uniqueID);
-  $str .= sprintf("  upTime%*s: %s\n",3,"  ",$upTime); 
-  $str .= "\n";
+   %fhemInfo = ();
 
-  my $contModules;
-  my $contModels;
-  my $modStr;
-  my @modules = keys %{$info{modules}};
-  my $length = (reverse sort { $a <=> $b } map { length($_) } @modules)[0];
+   $fhemInfo{'system'}{'uniqueID'} = $uniqueID;
+   $fhemInfo{'system'}{'release'}  = $release;
+   $fhemInfo{'system'}{'feature'}  = $feature;
+   $fhemInfo{'system'}{'os'}       = $os;
+   $fhemInfo{'system'}{'arch'}     = $arch;
+   $fhemInfo{'system'}{'perl'}     = $perl;
 
-  $str .= "Defined modules:\n";
-  foreach my $t (sort keys %{$info{modules}}) {
-    my $c = scalar keys %{$info{modules}{$t}};
-    my @models;
-    foreach my $n (sort keys %{$info{modules}{$t}}) {
-      my $model = $info{modules}{$t}{$n};
-      if($model ne "unknown" && $t ne "dummy") {
-        push(@models,$model) if(!grep {$_ =~ /$model/} @models);
-      }
-    }
-    $str .= sprintf("  %s%*s: %d\n",$t,$length-length($t)+1," ",$c);
-    if(@models != 0) {
-      $modStr .= sprintf("  %s%*s: %s\n",
-                        $t,$length-length($t)+1," ", join(",",sort @models));
-      $contModels .= join(",",sort @models)."|";
-    }
-    $contModules .= "$t:$c|";
-  }
+   foreach my $key ( keys %defs )
+   {
+      my $name  = $defs{$key}{NAME};
+      my $type  = $defs{$key}{TYPE};
+      my $model = 'noModel';
+         $model = defined($defs{$key}{model}) ? $defs{$key}{model} : $model;
+         $model = defined($defs{$key}{MODEL}) ? $defs{$key}{MODEL} : $model;
+         $model = AttrVal($name,'model',$model);
+#         $model = ReadingsVal($name,'type',$model);
+         $model = ReadingsVal($name,'model',$model);
+      $fhemInfo{$type}{$model}++ 
+         unless (defined($defs{$key}{'chanNo'}) || $name =~ m/^unknown_/); # exclude Homematic specials
+   }
 
-  if($modStr) {
-    $str .= "\n";
-    $str .= "Defined models per module:\n";
-    $str .= $modStr;
-  }
+   return;
+}
 
-  my $td = (lc(AttrVal("global", "sendStatistics", "")) eq "onupdate") ?
-                "yes" : "no";
+sub _fi2_Send() {
+   my $json = toJSON(\%fhemInfo);
 
-  $str .= "\n";
-  $str .= "Transmitting this information during an update: $td\n";
-  $str .= "You can change this via the global attribute sendStatistics\n";
+   Log3("fheminfo",4,"fheminfo: $json");
 
-  if(@args != 0 && $args[0] eq "send") {
-    my $uri = "http://fhem.de/stats/statistics.cgi";
-    my $req = HTTP::Request->new("POST",$uri);
-    $req->content_type("application/x-www-form-urlencoded");
-    my $contInfo;
-    $contInfo  = "Release:$release|";
-    $contInfo .= "Branch:$branch|";
-    $contInfo .= "OS:$os|";
-    $contInfo .= "Arch:$arch|";
-    $contInfo .= "Perl:$perl";
-    chop($contModules);
-    if(!$contModels) {
-      $req->content("uniqueID=$uniqueID&system=$contInfo&modules=$contModules");
-    } else {
-      chop($contModels);
-      $req->content("uniqueID=$uniqueID&system=$contInfo&modules=$contModules&models=$contModels");
-    }
- 
-    my $ua  = LWP::UserAgent->new(
-        agent => "Fhem/$release",
-        timeout => 60);
-    my $res = $ua->request($req);
+   my %hu_hash = ();
+   $hu_hash{url}      = $cmds{fheminfo}{uri};
+   $hu_hash{data}     = "uniqueID=".$fhemInfo{'system'}{'uniqueID'}."&json=$json";
+   $hu_hash{header}   = "User-Agent: FHEM/".$fhemInfo{'system'}{'release'};
+   $hu_hash{callback} = sub($$$) {
+        my ($hash, $err, $data) = @_;
+        if($err) {
+          Log 1, "fheminfo send: Server ERROR: $err";
+        } else {
+          Log3("fheminfo",4,"fheminfo send: Server RESPONSE: $data");
+        }
+      };
+   HttpUtils_NonblockingGet(\%hu_hash);
+   return;
+}
 
-    $str .= "\nserver response: ";
-    if($res->is_success) {
-      $str .= $res->content."\n";
-    } else {
-      $str .= $res->status_line."\n";
-    }
-  }
+sub _fi2_TelnetTable($) {
+  my ($doSend) = shift;
+  my $upTime = _fi2_Uptime();
+  my $str =  "Following statistics data will be sent to server:\n(see Logfile for server response)\n\n" if($doSend == 1);
+  $str .= "System Info\n";
+  $str .= sprintf("  Release%*s: %s\n",6," ",$fhemInfo{'system'}{'release'});
+  $str .= sprintf("  FeatureLevel%*s: %s\n",0," ",$fhemInfo{'system'}{'feature'});
+  $str .= sprintf("  OS%*s: %s\n",11," ",$fhemInfo{'system'}{'os'});
+  $str .= sprintf("  Arch%*s: %s\n",9," ",$fhemInfo{'system'}{'arch'});
+  $str .= sprintf("  Perl%*s: %s\n",9," ",$fhemInfo{'system'}{'perl'});
+  $str .= sprintf("  uniqueID%*s: %s\n",5," ",$fhemInfo{'system'}{'uniqueID'});
+  $str .= sprintf("  upTime%*s: %s\n",7,"  ",$upTime); 
+
+   my @keys = keys %fhemInfo;
+   foreach my $type (sort @keys)
+   {
+      next if $type eq 'system';
+      $str .= "\nType: $type ";
+      $str .= "Count: ".$fhemInfo{$type}{'noModel'} if defined $fhemInfo{$type}{'noModel'};
+      $str .= "\n";
+      while ( my ($model, $count) = each(%{$fhemInfo{$type}}) )
+      { $str .= "     $model = $fhemInfo{$type}{$model}\n" unless $model eq 'noModel'; }
+   }
 
   return $str;
 }
 
-########################################
-sub checkModule($) {
-  my $module = shift;
-  eval("use $module");
+sub _fi2_HtmlTable($) {
+   my ($doSend) = shift;
+   my $upTime = _fi2_Uptime();
+   my $result  = "<html><table>";
+      $result .= "<tr><td colspan='3'>Following statistics data will be sent to server:</br>(see Logfile for server response)</td></tr>" if($doSend == 1);
+      $result .= "<tr><td>System Info</td></tr>";
+      $result .= "<tr><td> </td><td>Release:</td><td>$fhemInfo{'system'}{'release'}</td></tr>";
+      $result .= "<tr><td> </td><td>FeatureLevel:</td><td>$fhemInfo{'system'}{'feature'}</td></tr>";
+      $result .= "<tr><td> </td><td>OS:</td><td>$fhemInfo{'system'}{'os'}</td></tr>";
+      $result .= "<tr><td> </td><td>Arch:</td><td>$fhemInfo{'system'}{'arch'}</td></tr>";
+      $result .= "<tr><td> </td><td>Perl:</td><td>$fhemInfo{'system'}{'perl'}</td></tr>";
+      $result .= "<tr><td> </td><td>uniqueId:</td><td>$fhemInfo{'system'}{'uniqueID'}</td></tr>";
+      $result .= "<tr><td> </td><td>upTime:</td><td>$upTime</td></tr>";
+      $result .= "<tr><td>Modules</td><td>Model</td><td>Count</td></tr>";
 
-  if($@) {
-    return(0);
-  } else {
-    return(1);
-  }
+   my @keys = keys %fhemInfo;
+   foreach my $type (sort @keys)
+   {
+      next if $type eq 'system';
+      $result .= "<tr><td>$type</td><td> </td><td>$fhemInfo{$type}{'noModel'}</td></tr>";
+      while ( my ($model, $count) = each(%{$fhemInfo{$type}}) )
+      { $result .= "<tr><td> </td><td>$model</td><td>$fhemInfo{$type}{$model}</td></tr>" unless $model eq 'noModel'; }
+   }  
+
+   $result .= "</table></html>";
+   return $result;
 }
 
-sub 
-fhemUptime()
-{
+sub _fi2_Uptime() {
   my $diff = time - $fhem_started;
   my ($d,$h,$m,$ret);
   
-  ($d,$diff) = _myDiv($diff,86400);
-  ($h,$diff) = _myDiv($diff,3600);
-  ($m,$diff) = _myDiv($diff,60);
+  ($d,$diff) = _fi2_Div($diff,86400);
+  ($h,$diff) = _fi2_Div($diff,3600);
+  ($m,$diff) = _fi2_Div($diff,60);
 
   $ret  = "";
   $ret .= "$d days, " if($d >  1);
@@ -224,9 +185,7 @@ fhemUptime()
   return $ret;
 }
 
-sub 
-_myDiv($$)
-{
+sub _fi2_Div($$) {
   my ($p1,$p2) = @_;
   return (int($p1/$p2), $p1 % $p2);
 }
@@ -266,47 +225,6 @@ _myDiv($$)
       <li>Defined models per module</li>
     </ul>
   <br>
-    Example:
-    <pre>
-      fhem&gt; fheminfo
-      Fhem info:
-        Release  : 5.3
-        OS       : linux
-        Arch     : i686-linux-gnu-thread-multi-64int
-        Perl     : v5.14.2
-        uniqueID : 87c5cca38dc75a4f388ef87bdcbfbf6f
-
-      Defined modules:
-        ACU        : 1
-        CUL        : 1
-        CUL_FHTTK  : 12
-        CUL_HM     : 66
-        CUL_WS     : 3
-        FHEM2FHEM  : 1
-        FHEMWEB    : 3
-        FHT        : 9
-      [...]
-        at         : 4
-        autocreate : 1
-        dummy      : 23
-        notify     : 54
-        structure  : 3
-        telnet     : 2
-        watchdog   : 9
-        weblink    : 17
-      
-      Defined models per module:
-        CUL        : CUN
-        CUL_FHTTK  : FHT80TF
-        CUL_HM     : HM-CC-TC,HM-CC-VD,HM-LC-DIM1T-CV,HM-LC-DIM1T-FM,HM-LC-SW1-PL,[...]
-        CUL_WS     : S555TH
-        FHT        : fht80b
-        FS20       : fs20pira,fs20s16,fs20s4a,fs20sd,fs20st
-        HMS        : hms100-mg,hms100-tf,hms100-wd
-        KS300      : ks300
-        OWSWITCH   : DS2413
-    </pre>
-  <br>
 
   <a name="fheminfoattr"></a>
   <b>Attributes</b>
@@ -331,98 +249,4 @@ _myDiv($$)
 </ul>
 
 =end html
-=begin html_DE
-
-<a name="fheminfo"></a>
-<h3>fheminfo</h3>
-<ul>
-  <code>fheminfo [send]</code>
-  <br>
-  <br>
-    fheminfo zeigt Informationen &uuml;ber das System und FHEM Definitionen an.
-  <br>
-  <br>
-    Der optionale Parameter <code>send</code> &uuml;bertr&auml;gt die Informationen
-    an einen zentralen Server um die Entwicklung von FHEM zu unterst&uuml;tzen.
-    Die &uuml;bermittelten Daten werden grafisch aufbereitet und k&ouml;nnen auf
-    <a href="http://fhem.de/stats/statistics.html">http://fhem.de/stats/statistics.html</a>
-    abgerufen werden. Anhand der IP-Adresse wird der ungef&auml;hre Standort mit
-    einer Genauigkeit von ca. 40-80 km ermittelt. Die IP-Adresse wird nicht gespeichert.
-  <br>
-  <br>
-    Eigenschaften:<br>
-    <ul>
-      <li>Eingesetztes Betriebssystem</li>
-      <li>Hardware Architektur</li>
-      <li>Installierte Perl Version</li>
-      <li>Installierte FHEM release</li>
-      <li>Definierte Module (nur offizielle FHEM Module werden ermittelt)</li>
-      <li>Definierte Modelle je Modul</li>
-    </ul>
-  <br>
-    Beispiel:
-    <pre>
-      fhem&gt; fheminfo
-      Fhem info:
-        Release  : 5.3
-        OS       : linux
-        Arch     : i686-linux-gnu-thread-multi-64int
-        Perl     : v5.14.2
-        uniqueID : 87c5cca38dc75a4f388ef87bdcbfbf6f
-
-      Defined modules:
-        ACU        : 1
-        CUL        : 1
-        CUL_FHTTK  : 12
-        CUL_HM     : 66
-        CUL_WS     : 3
-        FHEM2FHEM  : 1
-        FHEMWEB    : 3
-        FHT        : 9
-      [...]
-        at         : 4
-        autocreate : 1
-        dummy      : 23
-        notify     : 54
-        structure  : 3
-        telnet     : 2
-        watchdog   : 9
-        weblink    : 17
-      
-      Defined models per module:
-        CUL        : CUN
-        CUL_FHTTK  : FHT80TF
-        CUL_HM     : HM-CC-TC,HM-CC-VD,HM-LC-DIM1T-CV,HM-LC-DIM1T-FM,HM-LC-SW1-PL,[...]
-        CUL_WS     : S555TH
-        FHT        : fht80b
-        FS20       : fs20pira,fs20s16,fs20s4a,fs20sd,fs20st
-        HMS        : hms100-mg,hms100-tf,hms100-wd
-        KS300      : ks300
-        OWSWITCH   : DS2413
-    </pre>
-  <br>
-
-  <a name="fheminfoattr"></a>
-  <b>Attribute</b>
-  <br>
-  <br>
-    Die folgenden Attribute werden nur in Verbindung mit dem Parameter
-    <code>send</code> genutzt. Sie werden über <code>attr global</code> gesetzt.
-  <br>
-  <br>
-  <ul>
-    <li>sendStatistics<br>
-      Dieses Attribut wird in Verbindung mit dem <code>update</code> Befehl verwendet.
-      <br>
-      <code>onUpdate</code>: &Uuml;bertr&auml;gt die Daten bei jedem Update (empfohlene Einstellung).
-      <br>
-      <code>manually</code>: Manuelle &Uuml;bertr&auml;gung der Daten &uuml;ber <code>fheminfo send</code>.
-      <br>
-      <code>never</code>: Verhindert die &Uuml;bertr&auml;gung der Daten.
-    </li>
-    <br>
-  </ul>
-</ul>
-
-=end html_DE
 =cut
