11d222c1dd5cfb1c6ec80337d25a1b6010e9087d
neumann <neumann@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2017-06-09 05:33:50 +0000

    37_Spotify: fixed artist
    git-svn-id: https://svn.fhem.de/fhem/trunk@14484 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/37_Spotify.pm
+++ fhem/FHEM/37_Spotify.pm
@@ -575,7 +575,8 @@ sub Spotify_play($$$$$) { # any play command (colleciton or track)
 
         $data = {uris => $uris};
     } else {
-        $data = {context_uri => $context_uri, offset => {position => $position-1}};
+        $data = {context_uri => $context_uri};
+        $data->{offset} = {position => $position-1} if($position > 1);
     }
 
     $device_id = Spotify_getTargetDeviceID($hash, $device_id, 1);
