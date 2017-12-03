c27f8016e39b90baa6093011c1e242c950bba067
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2017-12-03 18:31:00 +0000

    pgm2/fhemweb.js: fix crash when URL contains a # (Forum #80460)
    git-svn-id: https://svn.fhem.de/fhem/trunk@15547 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/www/pgm2/fhemweb.js
+++ fhem/www/pgm2/fhemweb.js
@@ -981,7 +981,7 @@ FW_longpoll()
 
   var loc = (""+location).replace(/\?.*/,"");
   if(typeof WebSocket == "function" && FW_longpollType == "websocket") {
-    FW_pollConn = new WebSocket(loc.replace(/[&?].*/,'')
+    FW_pollConn = new WebSocket(loc.replace(/[#&?].*/,'')
                                    .replace(/^http/i, "ws")+query);
     FW_pollConn.onclose = 
     FW_pollConn.onerror = 
