d037917ce60927b00be8ed25175a6cc92476c566
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2019-01-22 10:35:55 +0000

    10_FBDECT.pm: add batteryPercent (Forum #96302)
    git-svn-id: https://svn.fhem.de/fhem/trunk@18371 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/10_FBDECT.pm
+++ fhem/FHEM/10_FBDECT.pm
@@ -309,6 +309,8 @@ FBDECT_ParseHttp($$$)
     readingsBulkUpdate($hash, $ptyp, $pyld);
     readingsBulkUpdate($hash, "batteryState", $pyld ? "low" : "ok")
         if($ptyp eq "batterylow");
+    readingsBulkUpdate($hash, "batteryPercent", $pyld) # 87575/96302
+        if($ptyp eq "battery");
   }
   readingsEndUpdate($hash, 1);
 
