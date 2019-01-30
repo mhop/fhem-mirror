896096b5da0f69b85205de9ea101a4176cfa1820
michael.winkler <michael.winkler@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2019-01-30 20:33:35 +0000

    BUFIX:   main::readingsBulkUpdateIfChanged   called by ./FHEM/70_NEUTRINO.pm (1191)
    git-svn-id: https://svn.fhem.de/fhem/trunk@18461 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/70_NEUTRINO.pm
+++ fhem/FHEM/70_NEUTRINO.pm
@@ -1,6 +1,9 @@
 ﻿# $Id$
 ############################################################################
-# 2017-09-11, v1.0.12
+# 2019-01-20, v1.0.13
+#
+# v1.0.13
+# - BUFIX:   main::readingsBulkUpdateIfChanged   called by ./FHEM/70_NEUTRINO.pm (1191)
 #
 # v1.0.12
 # - BUFIX:   $_ ersetzt durch $uResult
@@ -1185,11 +1188,12 @@ sub NEUTRINO_ReceiveCommand($$$) {
 						}
 
 						# find running record
-						if ($neutrinotime > $timerstartTime && $neutrinotime < $timerstopTime) {
-							readingsBulkUpdateIfChanged( $hash, "recordchannel", $timername );
-							NEUTRINO_SendCommand( $hash, "epginforecord","");
+						if ($timername ne "") {
+							if ($neutrinotime > $timerstartTime && $neutrinotime < $timerstopTime) {
+								readingsBulkUpdateIfChanged( $hash, "recordchannel", $timername );
+								NEUTRINO_SendCommand( $hash, "epginforecord","");
+							}						
 						}
-						
 						$i++;
 					}
 				}
