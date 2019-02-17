2bdf87d3dbfbec104539f45aff119f9b5a5abacb
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2019-02-17 19:34:50 +0000

    fhem.pl: change featurelevel order (Forum #97528)
    git-svn-id: https://svn.fhem.de/fhem/trunk@18623 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/fhem.pl
+++ fhem/fhem.pl
@@ -330,7 +330,7 @@ my @globalAttrList = qw(
   dnsServer
   dupTimeout
   exclude_from_update
-  featurelevel:5.5,5.6,5.7,5.8,5.9,99.99
+  featurelevel:5.9,5.8,5.7,5.6,5.5,99.99
   genericDisplayType:switch,outlet,light,blind,speaker,thermostat
   holiday2we
   httpcompress:0,1
