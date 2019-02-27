50a6bca8214dff39ac78b6d74be371c9d3b947e8
CoolTux <CoolTux@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2019-02-27 09:32:24 +0000

    73_AutoShuttersControl: fix little winrec bug then state opened (max winrec sensors)
    git-svn-id: https://svn.fhem.de/fhem/trunk@18750 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/73_AutoShuttersControl.pm
+++ fhem/FHEM/73_AutoShuttersControl.pm
@@ -41,7 +41,7 @@ package main;
 use strict;
 use warnings;
 
-my $version = '0.4.0.7';
+my $version = '0.4.0.8';
 
 sub AutoShuttersControl_Initialize($) {
     my ($hash) = @_;
@@ -843,7 +843,7 @@ sub EventProcessingWindowRec($@) {
         elsif (
             (
                 $1 eq 'tilted'
-                or ( $1 eq 'open' and $shutters->getSubTyp eq 'twostate' )
+                or ( ($1 eq 'open' or $1 eq 'opened') and $shutters->getSubTyp eq 'twostate' )
             )
             and $shutters->getVentilateOpen eq 'on'
             and $queryShuttersPosWinRecTilted
