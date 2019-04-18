6ab345c49c381c14dfef63df9a3ffaa8cb9b9308
loredo <loredo@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2019-04-18 11:37:10 +0000

    Meta.pm: fix import function for JSON::PP
    git-svn-id: https://svn.fhem.de/fhem/trunk@19214 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/Meta.pm
+++ fhem/FHEM/Meta.pm
@@ -1243,7 +1243,7 @@ m/(^#\s+(?:\d{1,2}\.\d{1,2}\.(?:\d{2}|\d{4})\s+)?[^v\d]*(v?(?:\d{1,3}\.\d{1,3}(?
         if ( keys %json > 0 ) {
             eval {
                 require JSON::PP;
-                JSON::PP::import();
+                JSON::PP->import();
                 1;
             };
 
