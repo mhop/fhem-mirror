ac98dc077943078f1a67c146a731b9c71f793a13
CoolTux <CoolTux@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2019-06-20 11:17:29 +0000

    GPUtils: add modul header
    git-svn-id: https://svn.fhem.de/fhem/trunk@19666 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/GPUtils.pm
+++ fhem/FHEM/GPUtils.pm
@@ -1,6 +1,33 @@
-##############################################
+###############################################################################
+#
+# Developed with Kate
+#
+#  Copyright: Norbert Truchsess
+#  All rights reserved
+#
+#       Contributors:
+#         - Marko Oldenburg (CoolTux)
+#
+#  This script is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License,or
+#  any later version.
+#
+#  The GNU General Public License can be found at
+#  http://www.gnu.org/copyleft/gpl.html.
+#  A copy is found in the textfile GPL.txt and important notices to the license
+#  from the author is found in LICENSE.txt distributed with these scripts.
+#
+#  This script is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#
 # $Id$
-##############################################
+#
+###############################################################################
+
 package GPUtils;
 use Exporter qw( import );
 
