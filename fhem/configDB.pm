cc5a106ad0ac1c615f12b103fba32fadf58e3e88
betateilchen <betateilchen@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2019-09-07 17:51:10 +0000

    configDB.pm: revert today's changes, they are not useful :)
    git-svn-id: https://svn.fhem.de/fhem/trunk@20123 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/configDB.pm
+++ fhem/configDB.pm
@@ -708,8 +708,6 @@ sub _cfgDB_Connect() {
 sub _cfgDB_InsertLine($$$$) {
 	my ($fhem_dbh, $uuid, $line, $counter) = @_;
 	my ($c,$d,$p1,$p2) = split(/ /, $line, 4);
-	$p2 //= "" if $c eq 'setuuid';
-	Log(5,"configDB Debug - c:$c d:$d p1:$p1 p2:$p2 l:$counter u:$uuid");
 	my $sth = $fhem_dbh->prepare('INSERT INTO fhemconfig values (?, ?, ?, ?, ?, ?)');
 	$sth->execute($c, $d, $p1, $p2, $counter, $uuid);
 	return;
