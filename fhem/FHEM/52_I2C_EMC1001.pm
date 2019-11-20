d1234b7284c362ceaf0844ae53488943a4174862
eisler <eisler@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2019-11-20 14:56:19 +0000

    52_I2C_EMC1001.pm: source code header corrected
    git-svn-id: https://svn.fhem.de/fhem/trunk@20550 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/52_I2C_EMC1001.pm
+++ fhem/FHEM/52_I2C_EMC1001.pm
@@ -1,28 +1,30 @@
-##############################################
-#
-# Modul for reading a EMC1001 digital temperature sensor via I2C
-# (see http://ww1.microchip.com/downloads/en/DeviceDoc/20005411A.pdf)
-#
-# Copyright (C) 2018 Stephan Eisler
-#
-#     This file is part of fhem.
-#
-#     Fhem is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 2 of the License, or
-#     (at your option) any later version.
-#
-#     Fhem is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-#
-#     You should have received a copy of the GNU General Public License
-#     along with fhem.  If not, see <http://www.gnu.org/licenses/>.
-#
 # $Id$
+#####################################################################################################
 #
-##############################################
+#       52_I2C_EMC1001.pm
+#
+#       Modul for reading a EMC1001 digital temperature sensor via I2C
+#       (see http://ww1.microchip.com/downloads/en/DeviceDoc/20005411A.pdf)
+#
+#       Copyright: Stephan Eisler
+#       Email: stephan@eisler.de
+#
+#       This file is part of fhem.
+#
+#       Fhem is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       Fhem is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with fhem.  If not, see <http://www.gnu.org/licenses/>.
+#
+####################################################################################################
 
 package main;
 
