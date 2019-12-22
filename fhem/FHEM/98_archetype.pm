712aafcd246b31d7c8a2f7d798f4881b77cac6dc
igami <igami@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2019-12-22 06:49:26 +0000

    98_archetype: fix perl evaluation for actual_attr
    git-svn-id: https://svn.fhem.de/fhem/trunk@20798 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_archetype.pm
+++ fhem/FHEM/98_archetype.pm
@@ -515,6 +515,10 @@ sub archetype_attrCheck($$$$;$) {
 
   return if(AttrVal($name, "attributesExclude", "") =~ /$attribute/);
 
+  if(AttrVal($SELF, "actual_$attribute", undef)){
+    $desired = eval($desired) if($desired =~ m/^\{.*\}$/);
+    $desired = archetype_evalSpecials($name, $desired) if($desired =~ m/%/);
+  }
 
   if($desired =~ m/^least(\((.*)\))?:(.+)/){
     my $seperator = $2 ? $2 : " ";
@@ -525,11 +529,6 @@ sub archetype_attrCheck($$$$;$) {
   elsif($desired =~ m/^undef/){
     return if(AttrVal($name, $attribute, undef));
     $desired = (split(":", $desired, 2))[1];
-  }
-
-  if($hash->{DEF} eq "derive attributes"){
-    $desired = eval($desired) if($desired =~ m/^\{.*\}$/);
-    $desired = archetype_evalSpecials($name, $desired) if($desired =~ m/%/);
   }
 
   return unless($desired);
