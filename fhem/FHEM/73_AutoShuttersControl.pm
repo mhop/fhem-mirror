70e7769e0babbfd0125e928e989dfbb97ce4f451
CoolTux <CoolTux@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-01-06 07:44:00 +0000

    73_AutoShuttersControl: fix adv icon for devStateIcon
    git-svn-id: https://svn.fhem.de/fhem/trunk@20895 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/73_AutoShuttersControl.pm
+++ fhem/FHEM/73_AutoShuttersControl.pm
@@ -443,12 +443,21 @@ sub Notify($$) {
           if ( $ascDev->getAutoShuttersControlShading eq 'none' );
         readingsSingleUpdate( $hash, 'ascEnable', 'on', 0 )
           if ( $ascDev->getASCenable eq 'none' );
-        CommandAttr(undef,$name . ' devStateIcon { AutoShuttersControl_DevStateIcon($name) }')
-          unless( AttrVal($name,'devStateIcon','{ AutoShuttersControl_DevStateIcon($name) }') eq '{ AutoShuttersControl_DevStateIcon($name) }' );
-        CommandDeleteAttr(undef,$name . ' event-on-change-reading')
-          unless( AttrVal($name,'event-on-change-reading','none') eq 'none' );
-        CommandDeleteAttr(undef,$name . ' event-on-update-reading')
-          unless( AttrVal($name,'event-on-update-reading','none') eq 'none' );
+        CommandAttr( undef,
+            $name
+              . ' devStateIcon { AutoShuttersControl_DevStateIcon($name) }' )
+          unless (
+            AttrVal(
+                $name, 'devStateIcon',
+                '{ AutoShuttersControl_DevStateIcon($name) }'
+            ) eq '{ AutoShuttersControl_DevStateIcon($name) }'
+          );
+        CommandDeleteAttr( undef, $name . ' event-on-change-reading' )
+          unless (
+            AttrVal( $name, 'event-on-change-reading', 'none' ) eq 'none' );
+        CommandDeleteAttr( undef, $name . ' event-on-update-reading' )
+          unless (
+            AttrVal( $name, 'event-on-update-reading', 'none' ) eq 'none' );
 
 # Ist der Event ein globaler und passt zum Rest der Abfrage oben wird nach neuen Rolläden Devices gescannt und eine Liste im Rolladenmodul sortiert nach Raum generiert
         ShuttersDeviceScan($hash)
@@ -4272,7 +4281,8 @@ sub DevStateIcon($) {
     elsif ( ReadingsVal( $name, 'state', undef ) =~ /.*asleep$/ ) {
         return '.*:scene_sleeping';
     }
-    elsif ( ReadingsVal( $name, 'state', undef ) =~ /^roommate(.come)?.(awoken|home)$/ )
+    elsif ( ReadingsVal( $name, 'state', undef ) =~
+        /^roommate(.come)?.(awoken|home)$/ )
     {
         return '.*:user_available';
     }
@@ -4307,6 +4317,9 @@ sub DevStateIcon($) {
     }
     elsif ( ReadingsVal( $name, 'state', undef ) =~ /.*privacy.*/ ) {
         return '.*:fts_shutter_50';
+    }
+    elsif ( ReadingsVal( $name, 'state', undef ) eq 'adv delay close' ) {
+        return '.*:christmas_tree';
     }
 
     return undef;
@@ -7903,7 +7916,7 @@ sub getblockAscDrivesAfterManual {
   ],
   "release_status": "under develop",
   "license": "GPL_2",
-  "version": "v0.8.9",
+  "version": "v0.8.10",
   "author": [
     "Marko Oldenburg <leongaultier@gmail.com>"
   ],
