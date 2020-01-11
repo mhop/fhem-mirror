60ccdc72964cd79e17742dbb6496fc194c00c467
DS_Starter <DS_Starter@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-01-11 16:17:32 +0000

    Tag.pm: fix warnings, Forum: 106763
    git-svn-id: https://svn.fhem.de/fhem/trunk@20939 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/lib/MP3/Tag.pm
+++ fhem/FHEM/lib/MP3/Tag.pm
@@ -2941,7 +2941,7 @@ sub format_time {
   local $self->{ms} = int($time * 1000 + 0.5) if defined $time;
   my ($out, %have, $c) = '';
   for my $f (@_) {
-    $have{$+}++ if $f =~ /^\??({([^{}]+)}|.)/;
+    $have{$+}++ if $f =~ /^\??(\{([^{}]+)}|.)/;
   }
   for my $f (@_) {
     if (!$c++ and $f =~ /^=>(\w)$/) {
@@ -2953,7 +2953,7 @@ sub format_time {
     }
     my $ff = $f;		# Modifiable
     my $opt = ($ff =~ s/^\?//);
-    $ff =~ s/^({[^{}]+}|\w)// or die "unexpected time format: <<$f>>";
+    $ff =~ s/^(\{[^{}]+}|\w)// or die "unexpected time format: <<$f>>";
     my ($what, $format) = ($1, '');
     if ($opt) {
       if ($what eq 'H') {
