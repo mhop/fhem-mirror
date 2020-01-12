716d9ad03bfbb04d9e8354029bc58388d8f3f972
loredo <loredo@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-01-12 09:53:11 +0000

    98_Installer: update to v0.5.7
    git-svn-id: https://svn.fhem.de/fhem/trunk@20949 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_Installer.pm
+++ fhem/FHEM/98_Installer.pm
@@ -1092,6 +1092,10 @@ sub CpanInstall($) {
     my $cmd = shift;
     my $h   = {};
     local $ENV{PATH} = __GetExtendedEnvPath();
+    eval {
+        umask 0022;
+        1;
+    };
     my $p = `$cmd->{installperl}`;
 
     if ( $p && $p ne '' ) {
@@ -5537,8 +5541,8 @@ sub __list_module {
       "abstract": "Modul zum Update von FHEM, zur Installation von Drittanbieter FHEM Modulen und der Verwaltung von Systemvoraussetzungen"
     }
   },
-  "version": "v0.5.6",
-  "release_status": "testing",
+  "version": "v0.5.7",
+  "release_status": "stable",
   "author": [
     "Julian Pawlowski <julian.pawlowski@gmail.com>"
   ],
