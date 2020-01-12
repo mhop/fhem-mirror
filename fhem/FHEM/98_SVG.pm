70e4078604e8ccc81a9f995639d694200bbc62b4
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-01-12 10:41:37 +0000

    98_SVG.pm: fix warning at month zoom (Forum #106914)
    git-svn-id: https://svn.fhem.de/fhem/trunk@20950 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_SVG.pm
+++ fhem/FHEM/98_SVG.pm
@@ -1015,19 +1015,19 @@ SVG_calcOffsets($$)
     $l[4] += $off;
     $l[4] += 12, $l[5]-- if($l[4] < 0);
     my @me = (31,28,31,30,31,30,31,31,30,31,30,31);
-    $me[1]++ if(($sy+1900)%4); # leap year. Ignore 1900 and 2100 :)
 
     if(SVG_Attr($FW_wname, $wl, "endPlotToday", undef)) {
       $sy = $ey = $l[5];
       $sm = $l[4]-1; $em = $l[4];
       $sm += 12, $sy-- if($sm < 0);
       $sd = $l[3]+1; $ed = $l[3];
+      $me[1]++ if(($sy+1900)%4); # leap year. Ignore 1900 and 2100 :)
       $sd=1, $sm=$em, $sy=$ey if($sd > $me[$sm]);
-      $ed = $me[$em] if($ed > $me[$em]);
 
     } else {
       $sy = $ey = $l[5];
       $sm = $em = $l[4];
+      $me[1]++ if(($sy+1900)%4); # leap year. Ignore 1900 and 2100 :)
       $sd = 1; $ed = $me[$l[4]];
     }
     $SVG_devs{$d}{from} = SVG_tspec( 0, 0, 0,$sd,$sm,$sy);
