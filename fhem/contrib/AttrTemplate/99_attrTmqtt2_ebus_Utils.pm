da59a8ec06d8eca93cbb131b3be5e3ea624f7525
Beta-User <Beta-User@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-01-21 09:32:14 +0000

    mqtt2.attrTemplate: add some ems-esp templates+bugfixes, add code and further templates in contrib/AttrTemplate
    git-svn-id: https://svn.fhem.de/fhem/trunk@21028 2b470e98-0d58-463d-a4d8-8e2adae1ed80

---
+++ fhem/contrib/AttrTemplate/99_attrTmqtt2_ebus_Utils.pm
@@ -0,0 +1,60 @@
+##############################################
+# $Id: attrTmqtt2_ebus_Utils.pm 2020-01-18 Beta-User $
+#
+
+package main;
+
+use strict;
+use warnings;
+use POSIX;
+
+sub
+attrTmqtt2_ebus_Utils_Initialize($$)
+{
+  my ($hash) = @_;
+}
+
+# Enter you functions below _this_ line.
+
+sub
+attrTmqtt2_ebus_createBarView($;$$) {
+  my ($val,$maxValue,$color) = @_;
+  $maxValue = $maxValue//100;
+  $color = $color//"red";
+  my $percent = $val / $maxValue * 100;
+  # Definition des valueStyles
+  my $stylestring = 'style="'.
+    'width: 200px; '.
+    'text-align:center; '.
+    'border: 1px solid #ccc ;'. 
+    "background-image: -webkit-linear-gradient(left,$color $percent".'%, rgba(0,0,0,0) '.$percent.'%); '.
+    "background-image:    -moz-linear-gradient(left,$color $percent".'%, rgba(0,0,0,0) '.$percent.'%); '. 
+    "background-image:     -ms-linear-gradient(left,$color $percent".'%, rgba(0,0,0,0) '.$percent.'%); '. 
+    "background-image:      -o-linear-gradient(left,$color $percent".'%, rgba(0,0,0,0) '.$percent.'%); '. 
+    "background-image:         linear-gradient(left,$color $percent".'%, rgba(0,0,0,0) '.$percent.'%);"';
+    # Rückgabe des definierten Strings
+  return $stylestring;
+}
+
+1;
+
+=pod
+=begin html
+
+<a name="attrTmqtt2_ebus_Utils"></a>
+<h3>attrTmqtt2_ebus_Utils</h3>
+<ul>
+  <b>Functions to support attrTemplates for ebusd</b><br> 
+</ul>
+<ul>
+  <b>attrTmqtt2_ebus_createBarView</b><br>
+  <code>attrTmqtt2_ebus_createBarView($,$$)</code><br>
+  Parameters are 
+  <ul>
+    <li>$value (required)</li> 
+    <li>$maxvalue (optional), defaults to 100</li> 
+    <li>$color, (optional), defaults to red</li> 
+  </ul>
+</ul><br>
+=end html
+=cut
---
+++ fhem/contrib/AttrTemplate/99_attrTmqtt2_roborock_Utils.pm
@@ -0,0 +1,79 @@
+##############################################
+# $Id: attrTmqtt2_roborock_Utils.pm 2020-01-19 Beta-User $
+#
+
+package main;
+
+use strict;
+use warnings;
+use POSIX;
+
+sub
+attrTmqtt2_roborock_Utils_Initialize($$)
+{
+  my ($hash) = @_;
+}
+
+# Enter you functions below _this_ line.
+
+sub
+attrTmqtt2_roborock_valetudo2svg($$$)
+{
+  my ($reading, $d, $filename) = @_;
+  my %ret;
+
+  if($d !~ m/height":(\d+),"width":(\d+).*?floor":\[(.*\])\]/) {
+    $ret{$reading} = "ERROR: Unknown format";
+    return \%ret;
+  }
+  my ($w,$h,$nums) = ($1, $2, $3);
+
+  my $svg=<<"EOD";
+<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
+<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="$w" height="$h" viewBox="0 0 $w $h">
+<g fill="#000000" stroke="none">
+  <rect x="0" y="0" width="$w" height="$h" stroke="black" stroke-width="1" fill="none"/>
+EOD
+
+  $nums =~ s/\[(\d+),(\d+)\]/
+    $svg .= "<rect x=\"$1\" y=\"$2\" width=\"1\" height=\"1\"\/>\n";
+    ""
+  /xge;
+  $svg .= "</g></svg>";
+
+  if(!open FD,">$filename") {
+    $ret{$reading} = "ERROR: $filename: $!";
+    return \%ret;
+  }
+  print FD $svg;
+  close(FD);
+  $ret{$reading} = "Wrote $filename";
+  return \%ret;
+}
+
+
+
+1;
+
+=pod
+=begin html
+
+<a name="attrTmqtt2_roborock_Utils"></a>
+<h3>attrTmqtt2_roborock_Utils</h3>
+<ul>
+  <b>Functions to support attrTemplates for roborock/valetudo</b><br> 
+</ul>
+<ul>
+  <b>attrTmqtt2_roborock_valetudo2svg</b><br>
+  <code>attrTmqtt2_roborock_valetudo2svg($$$)</code><br>
+  Parameters are 
+  <ul>
+    <li>map_data</li> 
+    <li>$EVENT</li> 
+    <li>SVG-filename and path</li> 
+  </ul>
+  See Rudolf Koenig's original post <a href https://forum.fhem.de/index.php/topic,104687.msg986304.html#msg986304>here</a>.
+</ul><br>
+=end html
+=cut
