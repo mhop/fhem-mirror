c592101849d42a113ba4bd9a3ca04efdcbfa6028
DS_Starter <DS_Starter@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-02-10 21:15:23 +0000

    93_DbRep: fix Perl warning
    git-svn-id: https://svn.fhem.de/fhem/trunk@21169 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/93_DbRep.pm
+++ fhem/FHEM/93_DbRep.pm
@@ -58,6 +58,7 @@ no if $] >= 5.017011, warnings => 'experimental::smartmatch';
 
 # Version History intern
 our %DbRep_vNotesIntern = (
+  "8.32.1"  => "08.02.2020  fix PERL WARNING: Argument \"\" isn't numeric in sprintf at ./FHEM/93_DbRep.pm line 10708 ",
   "8.32.0"  => "29.01.2020  new option \"deleteOther\" for minValue ",
   "8.31.0"  => "26.01.2020  new option \"deleteOther\" for maxValue ",
   "8.30.8"  => "25.01.2020  adjust SQL-Statements in OutputWriteToDB to avoid Duplicate entry errors and other fixes ",
@@ -10705,7 +10706,7 @@ sub DbRep_OutputWriteToDB($$$$$) {
       foreach my $row (@arr) {
           my @a              = split("#", $row);
           my $runtime_string = $a[0];                             # Aggregations-Alias (nicht benötigt)
-          $value             = defined($a[1])?sprintf("%.4f",$a[1]):undef;
+          $value             = looks_like_number($a[1])?sprintf("%.4f",$a[1]):undef;
           $rsf               = $a[2];                             # Runtime String first - Datum / Zeit für DB-Speicherung
           ($date,$time)      = split("_",$rsf);
           $time              =~ s/-/:/g if($time);
