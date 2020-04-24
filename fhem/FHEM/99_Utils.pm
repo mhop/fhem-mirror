305d4254be6978559cb4eca9e8b983140ae72351
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-04-24 14:22:12 +0000

    99_Utils.pm: remove Z from the time_str2num split (Forum #110545)
    git-svn-id: https://svn.fhem.de/fhem/trunk@21768 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/99_Utils.pm
+++ fhem/FHEM/99_Utils.pm
@@ -17,7 +17,7 @@ time_str2num($)
   my ($str) = @_;
   my @a;
   return time() if(!$str);
-  @a = split("[ZT: -]", $str); # 31652, 110545, 
+  @a = split("[T: -]", $str); # 31652, 110545, 
   return mktime($a[5],$a[4],$a[3],$a[2],$a[1]-1,$a[0]-1900,0,0,-1);
 }
 
