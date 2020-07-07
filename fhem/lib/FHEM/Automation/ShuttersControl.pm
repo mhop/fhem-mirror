eb2f1aaa67a1a2baa47d3b093d7f0e3f85826296
CoolTux <CoolTux@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-07-07 12:48:57 +0000

    73_AutoShuttersControl: fix wrong Skalar in Condition
    git-svn-id: https://svn.fhem.de/fhem/trunk@22367 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/lib/FHEM/Automation/ShuttersControl.pm
+++ fhem/lib/FHEM/Automation/ShuttersControl.pm
@@ -519,15 +519,15 @@ sub EventProcessingGeneral {
         while ( my ( $device, $deviceAttr ) =
             each %{ $hash->{monitoredDevs}{$devname} } )
         {
-            if ( $devname eq $name ) {
+            if ( $device eq $name ) {
                 EventProcessingResidents( $hash, $device, $events )
-                if ( $deviceAttr eq 'ASC_residentsDev' );
+                  if ( $deviceAttr eq 'ASC_residentsDev' );
                 EventProcessingRain( $hash, $device, $events )
-                if ( $deviceAttr eq 'ASC_rainSensor' );
+                  if ( $deviceAttr eq 'ASC_rainSensor' );
                 EventProcessingWind( $hash, $device, $events )
-                if ( $deviceAttr eq 'ASC_windSensor' );
+                  if ( $deviceAttr eq 'ASC_windSensor' );
                 EventProcessingTwilightDevice( $hash, $device, $events )
-                if ( $deviceAttr eq 'ASC_twilightDevice' );
+                  if ( $deviceAttr eq 'ASC_twilightDevice' );
             }
 
             EventProcessingWindowRec( $hash, $device, $events )
