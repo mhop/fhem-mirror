cd3042971d254ecd2e1a43c4a48a05644fed69d5
DS_Starter <DS_Starter@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-07-10 19:12:49 +0000

    49_SSCam: contrib 9.4.2
    git-svn-id: https://svn.fhem.de/fhem/trunk@22379 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/DS_Starter/49_SSCam.pm
+++ fhem/contrib/DS_Starter/49_SSCam.pm
@@ -7508,7 +7508,7 @@ return $cap;
 sub IsCapZoom {                                                           # PTZ Zoom Eigenschaft
   my $hash = shift;
   my $name = $hash->{NAME};
-return 1;
+
   my $cap = ReadingsVal($name, "CapPTZZoom", "false") ne "false" ? 1 : 0;
   
 return $cap;
