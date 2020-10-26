d44493b21978789f9cc53b0d742359635ba3014d
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-10-26 18:46:24 +0000

    fhem.pl: add GPL_V2, and change the comment accordingly
    git-svn-id: https://svn.fhem.de/fhem/trunk@23032 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/fhem.pl
+++ fhem/fhem.pl
@@ -8,22 +8,14 @@
 #  Copyright: Rudolf Koenig (rudolf dot koenig at fhem dot de)
 #  All rights reserved
 #
-#  This script free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
+#  This program free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License V2, which is also
+#  distributed together with this program in the file GPL_V2.txt
 #
-#  The GNU General Public License can be found at
-#  http://www.gnu.org/copyleft/gpl.html.
-#  A copy is found in the textfile GPL.txt and important notices to the license
-#  from the author is found in LICENSE.txt distributed with these scripts.
-#
-#  This script is distributed in the hope that it will be useful,
+#  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  This copyright notice MUST APPEAR in all copies of the script!
+#  GNU General Public License V2 for more details.
 #
 #  Homepage:  http://fhem.de
 #
