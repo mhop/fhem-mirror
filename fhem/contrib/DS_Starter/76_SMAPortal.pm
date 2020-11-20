2b60508bf235f6c4f3bfb9cec4a8bb76848e2c0b
DS_Starter <DS_Starter@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-11-20 21:53:51 +0000

    76_SMAPortal: contrib 3.7.0
    git-svn-id: https://svn.fhem.de/fhem/trunk@23199 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/DS_Starter/76_SMAPortal.pm
+++ fhem/contrib/DS_Starter/76_SMAPortal.pm
@@ -2329,7 +2329,7 @@ sub ___analyzeData {                   ## no critic 'complexity'
       
       $njdat = encode("utf8", $ad->decoded_content);
       Log3 ($name, 5, "$name - No JSON Data received:\n ".$njdat);
-      
+  Log3 ($name, 1, "$name - ADDON: ".$addon);
       if($rescode != 302 || $addon !~ /noJSONdata/x) {                                                       # 302 -> HTTP-Antwort liefert zusätzlich eine URL im Header-Feld Location. Es soll eine zweite, ansonsten identische Anfrage an die in Location angegebene neue URL gestellt werden.
           $errstate = 1; 
           $state    = "ERROR - see logfile for further information";
