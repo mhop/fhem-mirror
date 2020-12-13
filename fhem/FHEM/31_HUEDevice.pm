3e34637d4c2545d51630b9f365f3b5e09a6d7ef4
justme1968 <justme1968@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2020-12-13 17:05:33 +0000

    31_HUEDevice.pm: workaround for (#61636)
    git-svn-id: https://svn.fhem.de/fhem/trunk@23344 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/31_HUEDevice.pm
+++ fhem/FHEM/31_HUEDevice.pm
@@ -1,5 +1,4 @@
 
-
 # $Id$
 
 # "Hue Personal Wireless Lighting" is a trademark owned by Koninklijke Philips Electronics N.V.,
@@ -943,6 +942,7 @@ HUEDevice_Set($@)
 
   if( (my $joined = join(" ", @aa)) =~ /:/ ) {
     $joined =~ s/on-till\s+[^\s]+//g; #bad workaround for: https://forum.fhem.de/index.php/topic,61636.msg728557.html#msg728557
+    $joined =~ s/on-till-overnight\s+[^\s]+//g; #same bad workaround for: https://forum.fhem.de/index.php/topic,61636.msg1110193
     my @cmds = split(":", $joined);
     for( my $i = 0; $i <= $#cmds; ++$i ) {
       HUEDevice_SetParam($name, \%obj, split(" ", $cmds[$i]) );
