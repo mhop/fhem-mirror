599eef94c8cb28f277136fd103255bd1de6e9d1c
Sailor <Sailor@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2021-02-02 12:53:23 +0000

    73_WaterCalculator: Bugfix - use Time::Local
    git-svn-id: https://svn.fhem.de/fhem/trunk@23661 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/73_WaterCalculator.pm
+++ fhem/FHEM/73_WaterCalculator.pm
@@ -52,6 +52,7 @@
 package main;
 use strict;
 use warnings;
+use Time::Local;
 use FHEM::Meta;
 
 ###START###### Initialize module ##############################################################################START####
