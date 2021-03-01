0d173708a8843457385aacd4f3b107c51d9ea244
Otto123 <Otto123@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2021-03-01 09:11:26 +0000

    99_sonos2mqttUtils:bugfix in setup - multiple lines Reply in Bridge
    git-svn-id: https://svn.fhem.de/fhem/trunk@23871 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/AttrTemplate/99_sonos2mqttUtils.pm
+++ fhem/contrib/AttrTemplate/99_sonos2mqttUtils.pm
@@ -140,7 +140,7 @@ sub sonos2mqtt_setup
 my $devspec = shift @_ // 'a:model=sonos2mqtt_speaker';
 my $bridge = (devspec2array('a:model=sonos2mqtt_bridge'))[0];
 fhem("attr $devspec".q( devStateIcon {sonos2mqtt($name,'devStateIcon')}));
-sonos2mqtt_mod_list('a:model=sonos2mqtt_bridge','readingList','sonos/RINCON_([0-9A-Z]+)/Reply:.* Reply');
+#sonos2mqtt_mod_list('a:model=sonos2mqtt_bridge','readingList','sonos/RINCON_([0-9A-Z]+)/Reply:.* Reply');
 for ('stop:noArg','play:noArg','pause:noArg','toggle:noArg','volume:slider,0,1,100','volumeUp:noArg','volumeDown:noArg',
      'mute:true,false','next:noArg','previous:noArg','leaveGroup:noArg','setAVTUri:textField','playUri:textField',
      'notify:textField','x_raw_payload:textField','sayText:textField','speak:textField','input:Queue') {
