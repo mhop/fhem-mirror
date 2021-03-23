ed7678e3d93597aabe4920bbbfcdaa609c4e2714
Damian <Damian@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2021-03-23 22:17:53 +0000

    98_DOIF.pm: svg function ring: Min/Max depends on $lr
    git-svn-id: https://svn.fhem.de/fhem/trunk@24069 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/98_DOIF.pm
+++ fhem/FHEM/98_DOIF.pm
@@ -4889,8 +4889,8 @@ sub ring
                  ($icflag ? 53:47),color($currColor,$ln),($icflag ? 9:12),$unitformat,$unit) if (defined $unit);
   
   if (defined $minMax and $minMax) {
-    $out.= sprintf('<text text-anchor="middle" x="23" y="58" style="fill:%s;font-size:6px;%s">%s</text>',color($minCol,$ln),($minMax eq "1" ? "":$minMax),sprintf($format,$min));
-    $out.= sprintf('<text text-anchor="middle" x="59" y="58" style="fill:%s;font-size:6px;%s">%s</text>',color($maxColor,$ln),($minMax eq "1" ? "":$minMax),sprintf($format,$max));
+    $out.= sprintf('<text text-anchor="middle" x="23" y="58" style="fill:%s;font-size:6px;%s">%s</text>',color($minCol,$lr),($minMax eq "1" ? "":$minMax),sprintf($format,$min));
+    $out.= sprintf('<text text-anchor="middle" x="59" y="58" style="fill:%s;font-size:6px;%s">%s</text>',color($maxColor,$lr),($minMax eq "1" ? "":$minMax),sprintf($format,$max));
   }
   $out.= '</svg>';
  
