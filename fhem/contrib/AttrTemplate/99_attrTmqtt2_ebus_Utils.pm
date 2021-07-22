b7e4aecb0e1b6dab0e0932012d67da32c24bc304
Beta-User <Beta-User@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2021-07-22 06:29:50 +0000

    mqtt2.ebus.template: update (+ utils)
    git-svn-id: https://svn.fhem.de/fhem/trunk@24785 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/AttrTemplate/99_attrTmqtt2_ebus_Utils.pm
+++ fhem/contrib/AttrTemplate/99_attrTmqtt2_ebus_Utils.pm
@@ -9,6 +9,7 @@ use warnings;
 
 use JSON qw(decode_json);
 use Scalar::Util qw(looks_like_number);
+use List::Util 1.45 qw(uniq);
 
 use GPUtils qw(GP_Import);
 
@@ -24,6 +25,11 @@ BEGIN {
           InternalVal
           CommandGet
           CommandSet
+          CommandAttr
+          CommandDefine
+          CommandDeleteReading
+          CommandVersion
+          FmtDateTime
           readingsSingleUpdate
           readingsBulkUpdate
           readingsBeginUpdate
@@ -32,8 +38,10 @@ BEGIN {
           ReadingsNum
           ReadingsAge
           json2nameValue
+          addToDevAttrList
           defs
           Log3
+          trim
           )
     );
 }
@@ -57,6 +65,23 @@ sub j2nv {
     return if !length $EVENT;
     $EVENT=~ s,[{]"value":\s("?[^"}]+"?)[}],$1,g;
     return json2nameValue($EVENT, $pre, $filt, $not);
+}
+
+sub j2singleReading {
+    my $rName = shift // return;
+    my $EVENT = shift // return;
+    my $pre   = shift;
+    my $filt  = shift;
+    my $not   = shift;
+
+    return if !length $EVENT;
+    $EVENT=~ s,[{]"value":\s("?[^"}]+"?)[}],$1,g;
+    my $values = json2nameValue($EVENT, $pre, $filt, $not);
+    my @all;
+    for my $item ( sort keys %{$values} ) {
+        push @all, qq{$item: $values->{$item}};
+    }
+    return { $rName => join q{ - }, @all };
 }
 
 sub send_weekprofile {
@@ -159,11 +184,12 @@ sub send_weekprofile {
             CommandSet($defs{$name},"$name $Dl[$i] $payload") if ReadingsVal($name,$Dl[$i],'') ne $payload;
         } elsif ($i == 1) {
             $payload .=  defined $models[0] && $models[0] == 3 ? 'Mo-So' : defined $models[1] && $models[1] ? 'Mo-Fr' : 'selected';
+            CommandSet($defs{$name},"$name $Dl[$i] $payload") if defined $models[0] && $models[0] == 3 ||defined $models[1] && $models[1];
             CommandSet($defs{$name},"$name $Dl[$i] $payload") if ReadingsVal($name,$Dl[$i],'') ne $payload;
         } elsif ($i == 0 || $i == 6 ) {
             my $united = defined $models[0] && $models[0] == 2; 
             $payload .=  $united ? 'Sa-So' : 'selected';
-            CommandSet($defs{$name},"$name $Dl[$united ? 6 : $i] $payload") if ReadingsVal($name,$Dl[$united ? 6 : $i],'') ne $payload;
+            CommandSet($defs{$name},"$name $Dl[$united ? 6 : $i] $payload") if ReadingsVal($name,$Dl[$united ? 6 : $i],'') ne $payload || $united;
         } else {
             $payload .= 'selected';
             CommandSet($defs{$name},"$name $Dl[$i] $payload") if ReadingsVal($name,$Dl[$i],'') ne $payload;
@@ -187,6 +213,179 @@ sub _compareOnOff {
     return;
 }
 
+sub analyzeReadingList {
+    my $name   = shift // return;
+    my $setpre = shift // 0;
+
+    my $hash = $defs{$name} // return;
+
+    my $cid = $defs{$name}{CID};
+    my $dt = $defs{$name}{DEVICETOPIC};
+    my $revsn = (split q{\n}, CommandVersion(undef, 'attrTmqtt2_ebus_Utils noheader'))[0] // 'unknown';
+    $revsn = FmtDateTime(time) . " $revsn";
+    my $attrTemplt = q{ebus_analyzeReadingList};
+
+    my $rList_old = AttrVal( $name, 'readingList', '');
+    my $rList_new = q{};
+    my $firstprofile = 0;
+    my $dylist = 0;
+
+    my @need_prefix;
+    my @readings = keys %{$defs{$name}->{READINGS}};
+    for my $m (@readings){
+        if ($m =~ m{\A([^_]+_)_}){
+            push @need_prefix, $1;
+        }
+    }
+    @need_prefix = uniq @need_prefix;
+    my $needs_prefix = join q{|}, @need_prefix;
+    #Log3(undef,3,"präfix regex: $needs_prefix");
+
+    my $newline;
+    for my $line ( split q{\n}, $rList_old ) {
+        $line = trim($line);
+        next if $line eq '';
+        my $func;
+        my $prefix;
+
+        if ( $line =~ m{FHEM::aTm2u_ebus::}xm ) {
+            $rList_new .= $rList_new ? qq{\n$line} : $line;
+            next;
+        }
+        my ($re,$code) = split q{ }, $line, 2;
+        if ( !defined $code ) {
+            Log3($name, 3, "ERROR: deleted empty code in existing readingList line >$line< for $name");
+            next;
+        }
+
+        $re =~ s{\$DEVICETOPIC}{$dt}g;
+        $re =~ s{\A$cid:}{}g;
+        $code = trim($code);
+
+        #not Perl?
+        if($code !~ m{\A[{].*[}]\z}s) {
+            $rList_new .= $rList_new ? qq{\n$re $code} : qq{$re $code};
+            next;
+        }
+
+        my $newtop;
+        my $short;
+
+        #weekprofile type rL element?
+        if ( $re =~ m{(?<start>.+[/])(?<short>[^/:.]+)(?:[.]|\\x2e)(?<dy>[^.]+)(?:[.]|\\x2e)[1-3]:}xm ) {
+            $newtop = qq{$+{start}$+{short}.*:.*};
+            my $sLtop  = qq{$+{start}$+{short}};
+            $short  = $+{short};
+            my $dy     = $+{dy};
+            next if $firstprofile eq $short;
+            my @Dl = ("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+            my @dylists = qw(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday Sonntag|Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag Sun|Mon|Tue|Wed|Thu|Fri|Sat Son|Mon|Die|Mit|Don|Fre|Sam Su|Mo|Tu|We|Th|Fr|Sa So|Mo|Di|Mi|Do|Fr|Sa);
+            
+            for my $daylist (@dylists) {
+                if ( $dylist) {
+                    $func = "{ FHEM::aTm2u_ebus::upd_day_profile( \$NAME, \$TOPIC, \$EVENT, '" . $dylist . "' ) }";
+                } elsif ( $dy =~ m{$daylist}xms ) {
+                    $func = "{ FHEM::aTm2u_ebus::upd_day_profile( \$NAME, \$TOPIC, \$EVENT, '" . $daylist . "' ) }";
+                    $dylist = $daylist;
+                }
+            }
+            if ( !defined $func ) {
+                Log3(undef, 1, "error evaluating daylist, day is $dy");
+                next;
+            }
+            $newline = qq{$newtop $func};
+            #Log3(undef, 3, "topic: $newtop, function $func");
+
+            my @shD = split m{\|}xms, $dylist;
+            if ( !$firstprofile ) {
+                $rList_new .= $rList_new ? qq{\n$newline} : qq{$newline};
+                $firstprofile = $short;
+                my $sList_old = AttrVal( $name, 'setList', '');
+                my $sList_new = $sList_old;
+                for my $i (0..6) {
+                    my $sLline = qq{$Dl[$i] $sLtop.$shD[$i]/set};
+                    if ( index ($sList_new, $sLline) == -1 ) {
+                        $sList_new .= $sList_new ? qq{\n$sLline} : $sLline;
+                    }
+                }
+                CommandAttr(undef, "$name setList $sList_new") if $sList_new ne $sList_old;
+                addToDevAttrList($name, 'weekprofile', 'weekprofile');
+                CommandAttr(undef, "$name weekprofile $name") if !defined AttrVal($name, 'weekprofile', undef);
+                next;
+            }
+            my $newdev  = qq{${name}_${short}};
+            if ( !defined $defs{$newdev} ) {
+                CommandDefine( $defs{$name}, "$newdev MQTT2_DEVICE" );
+                readingsBeginUpdate($defs{$newdev});
+                readingsBulkUpdate($defs{$newdev}, 'associatedWith', $name);
+                readingsBulkUpdate($defs{$newdev}, 'IODev', InternalVal($name, 'IODev',undef)->{NAME});
+                readingsBulkUpdate($defs{$newdev}, 'attrTemplateVersion', $revsn);
+                readingsEndUpdate($defs{$newdev}, 0);
+                my $nroom = AttrVal($name, 'room','MQTT2_DEVICE');
+                CommandAttr(undef, "$newdev room $nroom");
+                my $sList;
+                for my $i (0..6) {
+                    my $sLlin = qq{$Dl[$i] $sLtop.$shD[$i]/set};
+                    $sList .= $sList ? qq{\n$sLlin} : $sLlin;
+                }
+                CommandAttr(undef, "$newdev setList $sList");
+                CommandAttr(undef, "$newdev model $attrTemplt");
+                addToDevAttrList($newdev, 'weekprofile', 'weekprofile');
+                CommandAttr(undef, "$newdev weekprofile $newdev");
+                my $ac = ReadingsVal($name, 'associatedWith','');
+                $ac .= $ac ? qq{,$newdev} : $newdev;
+                readingsSingleUpdate($defs{$name}, 'associatedWith', $ac, 0);
+                
+            }
+            my $rl2 = AttrVal($newdev, 'readingList', "");
+            $rl2 .= q{\n} if $rl2;
+            CommandAttr(undef, "$newdev readingList $rl2$newline") if index($rl2, $newtop) == -1;
+            next;
+        }
+
+        my $prefix ;
+
+        #json2nameValue type rL element with dot?
+        if ( $re =~ m{(?<start>.+[/])(?<short>[^/:]+)(?:[.]|\\x2e)(?<item>[^.:123]+):}xm ) {
+            $newtop = qq{$+{start}$+{short}.$+{item}:.*};
+            $prefix = qq{$+{short}_$+{item}_};
+            
+            $func = '{ FHEM::aTm2u_ebus::j2nv( $EVENT, ' . qq{'$prefix', } . '$JSONMAP ) }';
+            $newline = qq{$newtop $func};
+            $rList_new .= $rList_new ? qq{\n$newline} : qq{$newline};
+            next;
+        }
+
+        #json2nameValue type rL element with Error content? ebusd/sc/ErrorHistory
+        if ( $re =~ m{(?<start>.+[/])(?<short>ErrorHistory):}xm ) {
+            $newtop = $re;
+            $short  = $+{short};
+            $func = q<{ FHEM::aTm2u_ebus::j2singleReading( > . qq{'$short', }. q<$EVENT, '', $JSONMAP ) }>;
+            $newline = qq{$newtop $func};
+            $rList_new .= $rList_new ? qq{\n$newline} : qq{$newline};
+            next;
+        }
+
+        #json2nameValue type rL element w/o dot?
+        if ( $code =~ m{\A[{]\s+json2nameValue.*[}]\z}s) {
+            $func = q<{ FHEM::aTm2u_ebus::j2nv( $EVENT, '>;
+            my $funcb = q<', $JSONMAP ) }>;
+            my $mid  = q{};
+            $re =~ m{(?<start>.+[/])(?<pre>[^/:]+):}xm;
+            my $mid2 = qq{$+{pre}_};
+            $mid = $mid2 if $setpre || $mid2 =~ m{$needs_prefix}xms;
+            $newline = qq{$re $func${mid}${funcb}};
+            $rList_new .= $rList_new ? qq{\n$newline} : qq{$newline};
+            next;
+        }
+    }
+    #Log3(undef,3,"readingList new: $rList_new");
+    CommandAttr(undef, "$name readingList $rList_new") if index($rList_old, $rList_new) == -1;
+    CommandAttr(undef, "$name model $attrTemplt") if AttrVal($name, 'model', '') ne $attrTemplt;
+    CommandDeleteReading(undef, "$name .*_value");
+    readingsSingleUpdate($defs{$name}, 'attrTemplateVersion', $revsn,0);
+    return;
+}
 
 
 #ebusd/hc1/HP1.Mo.1:.* { json2nameValue($EVENT) }
@@ -259,20 +458,20 @@ __END__
   <b>Functions to support attrTemplates for ebusd</b><br> 
 </ul>
 <ul>
-  <li><b>aTm2u_ebus::j2nv</b><br>
-  <code>aTm2u_ebus::j2nv($,$$$)</code><br>
+  <li><b>FHEM::aTm2u_ebus::j2nv</b><br>
+  <code>FHEM::aTm2u_ebus::j2nv($,$$$)</code><br>
   This is just a wrapper to fhem.pl json2nameValue() to prevent the "_value" postfix. It will first clean the first argument by applying <code>$EVENT=~ s,[{]"value":\s("?[^"}]+"?)[}],$1,g</code>. 
   </li>
-  <li><b>aTm2u_ebus::upd_day_profile</b><br>
-  <code>aTm2u_ebus::upd_day_profile($$$,$)</code><br>
+  <li><b>FHEM::aTm2u_ebus::upd_day_profile</b><br>
+  <code>FHEM::aTm2u_ebus::upd_day_profile($$$,$)</code><br>
   Helper function to collect weekprofile info received over different topics. $NAME, $TOPIC and $EVENT are obligatory to be handed over, additionally you may provide a <i>daylist</i> as 4th argument. <i>daylist</i> defaults to Su|Mo|Tu|We|Th|Fr|Sa. Generated readings will be named Sunday, Monday, ..., so make sure to use different MQTT2-devices for each topic-group, if there's more than one item attached to your ebus capable to use weekly profiles.
   </li>
-  <li><b>aTm2u_ebus::send_weekprofile</b><br>
-  <code>aTm2u_ebus::send_weekprofile($$$,$$)</code><br>
+  <li><b>FHEM::aTm2u_ebus::send_weekprofile</b><br>
+  <code>FHEM::aTm2u_ebus::send_weekprofile($$$,$$)</code><br>
   Helper function that may be capable to translate a (temperature) <i>weekly profile<i> provided by a <i>weekprofile<i> TYPE device to the ebus format (max. three pairs of on/off switching times).
   </li>
-  <li><b>aTm2u_ebus::createBarView</b><br>
-  <code>aTm2u_ebus::createBarView($,$$)</code><br>
+  <li><b>FHEM::aTm2u_ebus::createBarView</b><br>
+  <code>FHEM::aTm2u_ebus::createBarView($,$$)</code><br>
   Parameters are 
   <ul>
     <li>$value (required)</li> 
