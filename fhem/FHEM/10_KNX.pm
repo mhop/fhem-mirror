a32a4a5e0ed03fbb55b9e1bd76580b2dd2db73d2
erwin <erwin@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2021-08-29 22:13:45 +0000

    10_KNX.pm: mayor rewrite of the Module, pls. check forum #122582
    git-svn-id: https://svn.fhem.de/fhem/trunk@24891 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/10_KNX.pm
+++ fhem/FHEM/10_KNX.pm
@@ -75,7 +75,7 @@
 #              new dpt7.600, dpt9.029,dpt9.030  
 # MH  20210818 E04.67 1st checkin SVN version
 #              docu correction
-# MH  20210829 fix crash when using Attr KNX_toogle (related to package)
+# MH  20210829 fix crash when using Attr KNX_toogle & on-until (related to package)
 
 
 #04.65 package main;
@@ -112,6 +112,7 @@ BEGIN {
           FW_detail FW_wname FW_directNotify
           readingFnAttributes
           InternalTimer RemoveInternalTimer
+          GetTimeSpec
           init_done
           IsDisabled IsDummy IsDevice
           deviceEvents devspec2array
