ac859eacd57ec31415dc9d19b415f8acdccbf725
Otto123 <Otto123@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2022-01-22 21:12:58 +0000

    mqtt2.template contrib/99_valetudoUtils.pm:new valetudo Template version
    git-svn-id: https://svn.fhem.de/fhem/trunk@25545 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/AttrTemplate/99_valetudoUtils.pm
+++ fhem/contrib/AttrTemplate/99_valetudoUtils.pm
@@ -8,7 +8,7 @@ package main;
 
 use strict;
 use warnings;
-use JSON;
+use JSON;Dumper;
 
 sub
 valetudoUtils_Initialize {
@@ -17,23 +17,25 @@ valetudoUtils_Initialize {
 }
 # Enter you functions below _this_ line.
 
-# valetudo_w return a string for dynamic selection setList
+#######
+# return a string for dynamic selection setList (widgets)
 sub valetudo_w {
 my $NAME = shift;
 my $setter = shift;
 # this part reads segments, it's only filled if Provide map data is enabled in connectivity
 if ($setter eq 'segments') {
-  my $json = ReadingsVal($NAME,'segments','select');
+  my $json = ReadingsVal($NAME,'.segments','{}');
+  if ($json eq '{}') {$json = '{"1":"no_Segment_or_not_supported"}'};
   my $decoded = decode_json($json);
   return join ',', sort values %$decoded
   }
-# this part read preset which contains a full json for preset zones
-if ($setter eq 'presets') {
-  my $json = ReadingsVal($NAME,'presets','select');
+# this part read presets which contains a full json for preset zones or locations
+if ($setter eq 'zones' or $setter eq 'locations') {
+  my $json = ReadingsVal($NAME,'.'.$setter.'Presets','');
   my $decoded = decode_json($json);
-  my %t;
-  for (keys %$decoded) { $t{$decoded->{$_}->{'name'}} = $_ } # build a new hash only with zone id and zone name
-  return join ',', sort keys %t 
+  my @array;
+  for (keys %$decoded) { push @array, $decoded->{$_}->{'name'} }
+  return join',',@array
   }
 # this part is for study purpose to read the full json segments with the REST API like
 # setreading alias=DreameL10pro json_segments {(qx(wget -qO - http://192.168.90.21/api/v2/robot/capabilities/MapSegmentationCapability))}
@@ -46,8 +48,8 @@ if ($setter eq 'json_segments') {
   return join ',', sort keys %t 
   }
 }
-##############################################################
-# valetudo_w return a complete string for setList right part
+#######
+# valetudo_c return a complete string for setList right part
 sub valetudo_c {
 my $NAME = shift;
 my $EVENT = shift;
@@ -62,22 +64,28 @@ if ($cmd eq 'x_raw_payload') {$ret=$devicetopic.$load}
 # this part return an array of segment id's according to selected Names from segments (simple json)
 if ($cmd eq 'clean_segment') {
   my @rooms = split',', $load;
-  my $json = ReadingsVal($NAME,'segments','');
+  my $json = ReadingsVal($NAME,'.segments','');
   my $decoded = decode_json($json);
-  my %t = reverse %$decoded;
   my @ids;
-  for ( @rooms ) {push @ids, $t{$_}}
+  for ( @rooms ) { push @ids,{reverse %$decoded}->{$_} }
   my %Hcmd = ( clean_segment => {segment_ids => \@ids,iterations => 1,customOrder => 'true' } );
   $ret = $devicetopic.'/MapSegmentationCapability/clean/set '.toJSON $Hcmd{$cmd}
   }
 
-# this part return the zone id according to the selected Name from presets (zones) (more complex json)
+# this part return the zone/location id according to the selected Name from presets (zones/locations) (more complex json)
 if ($cmd eq 'clean_zone') {
-  my $json = ReadingsVal($NAME,'presets','');
+  my $json = ReadingsVal($NAME,'.zonesPresets','');
   my $decoded = decode_json($json);
-  my %t;
-  for (keys %$decoded) { $t{$decoded->{$_}->{'name'}} = $_ }
-  $ret = $devicetopic.'/ZoneCleaningCapability/start/set '.$t{$load}
+  for (keys %$decoded) { 
+    if ( $decoded->{$_}->{'name'} eq $load ) {$ret = $devicetopic.'/ZoneCleaningCapability/start/set '.$_ } 
+	}
+  }
+if ($cmd eq 'goto') {
+  my $json = ReadingsVal($NAME,'.locationsPresets','');
+  my $decoded = decode_json($json);
+  for (keys %$decoded) { 
+    if ( $decoded->{$_}->{'name'} eq $load ) {$ret = $devicetopic.'/GoToLocationCapability/go/set '.$_ } 
+	}
   }
 
 # this part is for study purpose to read the full json segments with the REST API
@@ -95,8 +103,91 @@ if ($cmd eq 'clean_segment_j') {
   my %Hcmd = ( clean_segment => {segment_ids => \@ids,iterations => 1,customOrder => 'true' } );
   $ret = $devicetopic.'/MapSegmentationCapability/clean/set '.toJSON $Hcmd{$cmd}
   }
-
 return $ret
+}
+####### 
+# ask the robot via REST API for Featurelist and feature and return true false
+sub valetudo_f {
+my $NAME = shift;   # Devicename of the robot
+my $substr = shift; # requested Feature like GoToLocation or MapSegmentation
+my $ip=ReadingsVal($NAME,'ip4',(split ',',ReadingsVal($NAME,'ips','error'))[0]);
+my $string = GetHttpFile($ip, '/api/v2/robot/capabilities');
+index($string, $substr) == -1 ? '0':'1';
+}
+#######
+# add a line to multiline Attribute setList or regList
+# CommandAttr_multiline( 'MQTT2_valetudo_xxx','setList',q(  clean_segment:{"multiple-strict,".valetudo_w($name,"segments")} { valetudo_c($NAME,$EVENT) }) )
+sub CommandAttr_multiline {
+my $NAME = shift;
+my $attr = shift;
+my $item = shift;
+if ($attr ne 'setList' and $attr ne 'readingList') {return 'use only for multiline attrib'}
+my $val = AttrVal($NAME,$attr,'')."\n".$item;
+CommandAttr(undef, "$NAME $attr $val");
+}
+
+################ 
+# is never used, was in a first version used to preread the json in valetudo_c
+# return simpel json pairs from presets format of valetudo 
+sub valetudo_r {
+my $setter = shift;
+my $payload = shift;
+my $ret = 'error';
+my %t;
+if ($setter eq 'presets') {
+  my $decoded = decode_json($payload);
+  for (keys %$decoded) { $t{$decoded->{$_}->{'name'}} = $_ } # build a new hash only with names and ids pairs
+  $ret = toJSON(\%t); # result is sorted
+  }
+  return $ret
+}
+
+
+####### Aus dem Forum funktioniert aber nicht
+# Zeigt aber wie man Readings zurück gibt 
+sub
+valetudo2svg($$$)
+{
+  my ($reading, $d, $filename) = @_;
+  my %ret;
+
+  if(!open FD,">$filename") {
+    $ret{$reading} = "ERROR: $filename: $!";
+    return \%ret;
+  }
+  print FD $d;
+  close(FD);
+  $ret{$reading} = "Wrote $filename";
+  return \%ret;
+
+  if($d !~ m/height":(\d+),"width":(\d+).*?floor":\[(.*\])\]/) {
+    $ret{$reading} = "ERROR: Unknown format";
+    return \%ret;
+  }
+  my ($w,$h,$nums) = ($1, $2, $3);
+
+  my $svg=<<"EOD";
+<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
+<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="$w" height="$h" viewBox="0 0 $w $h">
+<g fill="#000000" stroke="none">
+  <rect x="0" y="0" width="$w" height="$h" stroke="black" stroke-width="1" fill="none"/>
+EOD
+
+  $nums =~ s/\[(\d+),(\d+)\]/
+    $svg .= "<rect x=\"$1\" y=\"$2\" width=\"1\" height=\"1\"\/>\n";
+    ""
+  /xge;
+  $svg .= "</g></svg>";
+
+  if(!open FD,">$filename") {
+    $ret{$reading} = "ERROR: $filename: $!";
+    return \%ret;
+  }
+  print FD $svg;
+  close(FD);
+  $ret{$reading} = "Wrote $filename";
+  return \%ret;
 }
 
 1;
