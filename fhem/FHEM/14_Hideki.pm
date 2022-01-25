15a64e993933454c0facdd1e44103e1839d73bef
Sidey <Sidey@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2022-01-25 21:43:09 +0000

    14_Hideki.pm: fix match regex
    - avoid "Unknown code P12#751CBA4A31BFC751F4, help me!" errors in logfile
    - forum:#125679
    
    git-svn-id: https://svn.fhem.de/fhem/trunk@25560 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/14_Hideki.pm
+++ fhem/FHEM/14_Hideki.pm
@@ -5,6 +5,7 @@
 # and was modified by a few additions
 # to support Hideki Sensors
 # S. Butzek, HJGode, Ralf9 2015-2017
+# S. Butzek 2018-2022
 #
 # changed the way crc and decrypt is used hjgode 20171129
 
@@ -24,10 +25,10 @@ Hideki_Initialize($)
   my ($hash) = @_;
 
 
-  $hash->{Match}     = "^P12#75[A-F0-9]{17,30}";   # Laenge (Anhahl nibbles nach 0x75 )noch genauer spezifizieren
-  $hash->{DefFn}     = "Hideki_Define";
-  $hash->{UndefFn}   = "Hideki_Undef";
-  $hash->{ParseFn}   = "Hideki_Parse";
+  $hash->{Match}     = qr/^P12#75[A-F0-9]{14,30}/;   # Laenge (Anhahl nibbles nach 0x75 )noch genauer spezifizieren
+  $hash->{DefFn}     = \&Hideki_Define;
+  $hash->{UndefFn}   = \&Hideki_Undef;
+  $hash->{ParseFn}   = \&Hideki_Parse;
   $hash->{AttrList}  = "do_not_notify:0,1 showtime:0,1"
                        ." ignore:0,1"
                        ." windDirCorr windSpeedCorr"
