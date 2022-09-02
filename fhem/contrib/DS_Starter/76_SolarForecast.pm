201148293a40e6c360f9afd09a1075268992b15d
DS_Starter <DS_Starter@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2022-09-02 20:54:24 +0000

    76_SolarForecast.pm: contrib 0.67.6
    git-svn-id: https://svn.fhem.de/fhem/trunk@26374 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/DS_Starter/76_SolarForecast.pm
+++ fhem/contrib/DS_Starter/76_SolarForecast.pm
@@ -5861,8 +5861,9 @@ sub __weatherOnBeam {
                                     weather_icon($hfcg->{$i}{weather}-100) : 
                                     weather_icon($hfcg->{$i}{weather});
                                     
-          #my $wcc                 = $hfcg->{$i}{wcc};                                                        # Bewölkungsgrad ergänzen
-          #$title                 .= ': '.$wcc;
+          my $wcc                 = $hfcg->{$i}{wcc};                                                        # Bewölkungsgrad ergänzen
+          #Log3 ($name, 2, "$name - wcc: $wcc");
+          $title                 .= ': '.$wcc if(IsNumeric ($wcc));
           
           if($icon_name eq 'unknown') {              
               Log3 ($name, 4, "$name - unknown weather id: ".$hfcg->{$i}{weather}.", please inform the maintainer");
