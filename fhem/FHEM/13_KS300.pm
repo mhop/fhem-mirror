219ea2eaf6212781136cb8c2700d97cf9ab0c38c
rudolfkoenig <rudolfkoenig@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2022-12-16 10:23:28 +0000

    KS300: negative temp & strangeTemp fix
    git-svn-id: https://svn.fhem.de/fhem/trunk@26862 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/13_KS300.pm
+++ fhem/FHEM/13_KS300.pm
@@ -205,6 +205,7 @@ KS300_Parse($$)
     $v[3] = "$a[22]$a[21]" + 0;
     $v[4] = "$a[20]$a[19].$a[18]" + 0;
     $v[4] = sprintf("%0.1f", $v[4]);
+    $v[4] = -$v[4] if(hex($v[8]) & 8); # Negative temp
     $v[5] = ((hex($a[17]) & 0x2) || $haverain) ? "yes" : "no";
     $v[6] = $a[29];
     $v[7] = $a[16];
@@ -220,8 +221,6 @@ KS300_Parse($$)
       }
     }
     
-    # Negative temp
-    $v[4] = -$v[4] if(hex($v[8]) & 8);
 
     Log3 $def, 4, "KS300 $dev: $msg";
 
