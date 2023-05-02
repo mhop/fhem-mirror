7e3b34d8a9a2c98e2ce00c112ca17037deb10a55
Beta-User <Beta-User@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2023-05-02 16:53:59 +0000

    mqtt2.template: changes to Hoymiles inverter
    git-svn-id: https://svn.fhem.de/fhem/trunk@27513 2b470e98-0d58-463d-a4d8-8e2adae1ed80

---
+++ fhem/contrib/AttrTemplate/99_attrT_Ahoy_Utils.pm
@@ -0,0 +1,85 @@
+##############################################
+# $Id$
+#
+
+package FHEM::attrT_Ahoy_Utils;    ## no critic 'Package declaration'
+
+use strict;
+use warnings;
+
+use Color;
+use GPUtils qw(GP_Import);
+
+## Import der FHEM Funktionen
+#-- Run before package compilation
+BEGIN {
+
+    # Import from main context
+    GP_Import(
+        qw(
+          FW_makeImage
+          AttrVal
+          InternalVal
+          ReadingsVal
+          ReadingsNum
+          ReadingsAge
+          defs
+          )
+    );
+}
+
+sub main::attrT_Ahoy_Utils_Initialize { goto &Initialize }
+
+# initialize ##################################################################
+sub Initialize {
+  my $hash = shift;
+  return;
+}
+
+# Enter you functions below _this_ line.
+
+sub devStateIcon {
+  my $devname = shift // return;
+  my $panels  = shift // 4;
+  my $colors  = shift // 0;
+
+  return if !defined $defs{$devname};
+
+  my $ret = ReadingsNum($devname,'available',0);
+  $ret = $ret > 1 ? '10px-kreis-gruen' : isday() ? '10px-kreis-rot' :'10px-kreis-gelb'; 
+  $ret = FW_makeImage($ret, 'edit_settings');
+  my $col = substr(Color::pahColor(-10,50,70,ReadingsNum($devname,'temperature',0),$colors),0,6);
+  $ret .= FW_makeImage("sani_solar_temp\@$col",'file_unknown@grey');
+  $ret .= ' ';
+  $ret .= ReadingsNum($devname,'temperature',0,1);
+  $ret .= ' °C<br>';
+  $ret .= ReadingsNum($devname,'P_AC',0);
+  $ret .= ' W / ';
+  $ret .= ReadingsNum($devname,'YieldDay',0);
+  $ret .= ' Wh';
+
+  my $total = ReadingsNum($devname,'YieldTotal',0,1);
+  if ( $total > 0 ) {
+    $ret .= ' / ';
+    $ret .= $total;
+    $ret .= ' kWh';
+  }
+  
+  for (1..$panels) {
+    $ret .= '<br>';
+    $col = substr(Color::pahColor(0,50,100,ReadingsNum($devname,"Irradiation$_",0),$colors),0,6);
+    $ret .= FW_makeImage("solar\@$col",'file_unknown@grey');
+    $ret .= ' ';
+    $ret .= ReadingsNum($devname,"P_DC$_",0);
+    $ret .= ' W / ';
+    $ret .= ReadingsNum($devname,"YieldDay$_",0);
+    $ret .= ' Wh';
+    $total = ReadingsNum($devname,"YieldTotal$_",0,1);
+    if ( $total > 0 ) {
+      $ret .= ' / ';
+      $ret .= $total;
+      $ret .= ' kWh';
+    }
+  }
+  return qq(<div><p style="text-align:right">$ret</p></div>);
+}
