de39c9d36bf88e81cb403ae23ecb48d3b3cfc3b7
KernSani <KernSani@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2023-12-19 19:48:23 +0000

    46_TRX_WEATHER.pm: Added TFA_RAIN as a supported device (thanks to Adimarantis)
    git-svn-id: https://svn.fhem.de/fhem/trunk@28295 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/46_TRX_WEATHER.pm
+++ fhem/FHEM/46_TRX_WEATHER.pm
@@ -217,6 +217,7 @@ my %types =
    0x540d => { part => 'TEMPHYDROBARO', method => \&TRX_WEATHER_common_temphydrobaro, },
    # RAIN
    0x550b => { part => 'RAIN', method => \&TRX_WEATHER_common_rain, },
+   0x5509 => { part => 'RAIN', method => \&TRX_WEATHER_common_rain, },   
    # WIND
    0x5610 => { part => 'WIND', method => \&TRX_WEATHER_common_anemometer, },
    # UV
@@ -1015,6 +1016,7 @@ sub TRX_WEATHER_common_rain {
 	0x05 => "WS2300_RAIN", # WS2300
 	0x06 => "TX5_RAIN", # La Crosse TX5
 	0x07 => "WS4500_RAIN", # Alecto WS4500, Auriol H13726, Hama EWS1500, Meteoscan W155/W160,
+  0x09 => "TFA_RAIN",
   );
 
   if (exists $devname{$bytes->[1]}) {
