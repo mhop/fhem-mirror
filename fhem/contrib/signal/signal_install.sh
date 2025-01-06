c59d63bfb4dc0fadd3803d842e2aad479a4490fc
Adimarantis <Adimarantis@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2025-01-06 17:19:55 +0000

    50_Signalbot: Installer signal-cli 0.13.11 update
    git-svn-id: https://svn.fhem.de/fhem/trunk@29483 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/contrib/signal/signal_install.sh
+++ fhem/contrib/signal/signal_install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #$Id:$
-SCRIPTVERSION="3.29"
+SCRIPTVERSION="3.30"
 # Author: Adimarantis
 # License: GPL
 #Install script for signal-cli 
 SIGNALPATH=/opt
 SIGNALUSER=signal-cli
 LIBPATH=/usr/lib
-SIGNALVERSION="0.13.9"
+SIGNALVERSION="0.13.11"
 #Check for latest valid version at https://github.com/AsamK/signal-cli/releases
-LIBRARYVERSION="0.58.2"
+LIBRARYVERSION="0.64.1"
 #Check for latest valid version at https://github.com/exquo/signal-libs-build/releases
 #Make sure this matches the required version for signal-cli (see lib/libsignal-client-0.xx.x.jar version in signal-cli)
 LIBSIG=libsignal_jni.tgz
