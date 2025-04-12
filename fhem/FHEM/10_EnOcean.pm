ba1d2bbfdd87631aabf67ad9fd2b7bd50e051bdd
klaus.schauer <klaus.schauer@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2025-04-12 18:25:12 +0000

    10_EnOcean: refID: 00000488 changed to 000004B8
    git-svn-id: https://svn.fhem.de/fhem/trunk@29848 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/FHEM/10_EnOcean.pm
+++ fhem/FHEM/10_EnOcean.pm
@@ -537,7 +537,7 @@ my %EnO_mscRefID = (
   "0000045C" => {model => "Eltako_F4CT55", version => "0101", attr => {eep => "G6.02.01"}},
   "0000043E" => {model => "Eltako_FRM60", teachIn => "FFF80D80", version => "0100", attr => {eep => "I5.3F.7F"}},
   "00000484" => {model => "Eltako_FL62", teachIn => "E0400D80", version => "0104", attr => {eep => "N5.38.08"}},
-  "00000488" => {model => "Eltako_FL62", teachIn => "E0400D80", version => "0106", attr => {eep => "N5.38.08"}},
+  "000004B8" => {model => "Eltako_FL62", teachIn => "E0400D80", version => "0106", attr => {eep => "N5.38.08"}},
   "000004F8" => {model => "Eltako_FJ62", teachIn => "FFF80D80", version => "0105", attr => {eep => "K5.3F.7F"}}
 );
 
@@ -551,7 +551,7 @@ my %EnO_productID = (
             "0000045C" => {model => "Eltako_F4CT55", version => "0101", attr => {eep => "G6.02.01"}},
             "0000043E" => {model => "Eltako_FRM60", teachIn => "FFF80D80", version => "0100", attr => {eep => "I5.3F.7F"}}},
             "00000484" => {model => "Eltako_FL62", teachIn => "E0400D80", version => "0104", attr => {eep => "N5.38.08"}},
-            "00000488" => {model => "Eltako_FL62", teachIn => "E0400D80", version => "0106", attr => {eep => "N5.38.08"}},
+            "000004B8" => {model => "Eltako_FL62", teachIn => "E0400D80", version => "0106", attr => {eep => "N5.38.08"}},
             "000004F8" => {model => "Eltako_FJ62", teachIn => "FFF80D80", version => "0105", attr => {eep => "K5.3F.7F"}},
   "05C" => {"0000000A" => {model => "Hoppe_eHandle", version => "00", attr => {eep => "D2.06.40", subType =>"multisensor.40"}}}
 );
