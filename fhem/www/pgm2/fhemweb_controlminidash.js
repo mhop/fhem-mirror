80853c5ba8a20b5e5a6ef785e5781997820bfe3e
schwatter <schwatter@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2025-12-06 10:03:11 +0000

    fhemweb_controlminidash: add minimal doc
    git-svn-id: https://svn.fhem.de/fhem/trunk@30587 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/www/pgm2/fhemweb_controlminidash.js
+++ fhem/www/pgm2/fhemweb_controlminidash.js
@@ -1,4 +1,4 @@
-FW_version["fhemweb_controlminidash.js"] = "$Id: controlminidash.js 0.2.5 schwatter $";
+FW_version["fhemweb_controlminidash.js"] = "$Id: controlminidash.js 0.2.6 schwatter $";
 FW_widgets['controlminidash'] = { createFn: controlMiniDashCreate };
 
 function controlMiniDashCreate(elName, devName, vArr, currVal, set, params, cmd) {
@@ -748,3 +748,18 @@ function controlMiniDashCreate(elName, devName, vArr, currVal, set, params, cmd)
 
     return wrapper[0];
 }
+
+/*
+=pod
+
+=begin html
+
+  <li>controlminidash,&lt;measured-temp&gt;,&lt;humidity&gt;,&lt;desired-temp&gt;,&lt;info&gt;,
+      &lt;btn1&gt;,&lt;btn2&gt;,&lt;btn3&gt;,&lt;btn4&gt;,&lt;btn5&gt;,&lt;btn6&gt;
+      - create a compact mini dashboard for a device<br>
+      see <a href='https://wiki.fhem.de/wiki/FHEMWEB/ControlMiniDash'>FHEMWEB/ControlMiniDash</a> for documentation and examples.</li><br>
+
+=end html
+
+=cut
+*/
