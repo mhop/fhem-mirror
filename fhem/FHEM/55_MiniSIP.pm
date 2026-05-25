7ffc151997e68f74a740a509356491f0fb87bd64
betateilchen <betateilchen@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2026-05-25 12:45:47 +0000

    55_MiniSIP.pm: initial commit to production
    git-svn-id: https://svn.fhem.de/fhem/trunk@31283 2b470e98-0d58-463d-a4d8-8e2adae1ed80

---
+++ fhem/FHEM/55_MiniSIP.pm
@@ -0,0 +1,215 @@
+#  $Id$
+
+################################################################
+#
+#  Copyright notice
+#
+#  (c) 2026 - today
+#  Copyright: betateilchen (betateilchen dot quantentunnel dot de)
+#  All rights reserved
+#
+#  This program is part of FHEM; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License V2.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+#  See the GNU General Public License V2 for more details.
+#
+################################################################
+
+package FHEM::MiniSIP;
+
+use strict;
+use warnings;
+
+require FHEM::Core::MiniSIP;
+
+sub ::MiniSIP_Initialize { goto &Initialize }
+
+sub Initialize($) {
+  my ($hash) = @_;
+  $hash->{parseParams} = 1;
+  $hash->{DefFn}       = \&FHEM::Core::MiniSIP::Define;
+  $hash->{ReadFn}      = \&FHEM::Core::MiniSIP::Read;
+  $hash->{SetFn}       = \&FHEM::Core::MiniSIP::Set;
+  $hash->{GetFn}       = \&FHEM::Core::MiniSIP::Get;
+#  $hash->{AttrFn}      = \&FHEM::Core::MiniSIP::Attr;
+  $hash->{ShutdownFn}  = \&FHEM::Core::MiniSIP::Shutdown;
+  $hash->{UndefFn}     = \&FHEM::Core::MiniSIP::Undef;
+  $hash->{DeleteFn}    = \&FHEM::Core::MiniSIP::Delete;
+
+  $hash->{AttrList}    = ""
+                        ."logFullMessage:0,1 "
+                        ."showFullMessage:0,1 "
+                        ."parseFn "
+                        ."disable:1,0 "
+                        ."disabledForIntervals "
+                        .$::readingFnAttributes;
+}
+
+1;
+
+
+__END__
+
+=pod
+=item helper
+=item summary    build SIP endpoint to receive and answer SIP requests
+=item summary_DE erstellt einen SIP Endpunkt f&uuml;r SIP Nachrichten
+=begin html
+
+<a id="MiniSIP"></a>
+<h3>MiniSIP</h3>
+<br><br>
+This FHEM module allows you to create a SIP endpoint capable of  
+processing incoming SIP messages and events.<br>
+This enables you to trigger specific actions by sending some codes from 
+a registered VoIP phone.<br>
+<br>
+Two methods are available for receiving such codes:
+<ul>
+  <li><b>Method 1:</b> Simply dialing a 'number' e.g. **55<br>
+  This message arrives as a SIP INVITE and generates a value for the 'input' reading.<br>
+  This method is vendor-independent.</li><br>
+<br>
+  <li><b>Method 2:</b> Snom SIP phones can also send SIP MESSAGES<br>
+  when a function key — previously configured for this purpose — is pressed on the phone.<br>
+  This MESSAGE also generates a value for the 'input' reading.<br>
+  In this case, the value is always the immutable ID of the specific function key.</li><br>
+</ul>
+
+The 'input' reading can be evaluated and processed using 'notify', 'DOIF', <br> 
+or any other event-processing mechanisms in FHEM.<br>
+<br>
+The SIP endpoint performs no authentication whatsoever.<br>
+This means that the username and password used for registration<br>
+can be chosen arbitrarily.<br>
+Every incoming SIP request of the types REGISTER, INVITE, MESSAGE, SUBSCRIBE, BYE<br>
+is answered with "200 OK" and subsequently processed.<br>
+<br>
+<b>Prerequisits</b><br>
+<br>
+This module needs additional perl module Net::SIP (libnet-sip-perl).<br>
+All other dependencies should be fulfilled in a standard FHEM installation.<br>
+
+<ul>
+  <br>
+  <a id="MiniSIP-define"></a>
+  <b>Define</b>
+  <br><br>
+  <ul>
+    <code>define &lt;name&gt; MiniSIP port=&lt;port&gt; from=&lt;from-uri&gt;</code>
+    <br><br>
+  </ul>
+  <br>
+
+  <a id="MiniSIP-set"></a>
+  <b>Set</b>
+  <br><br>
+  <ul>
+		<a id="MiniSIP-set-backup_peers"></a>
+		<li><b>backup_peers</b><br>
+			Used to backup all registered peers into FHEM keystore.<br>
+			<br>
+			Will be called automatically on FHEM shutdown.
+		</li>
+		<br>
+		<a id="MiniSIP-set-restore_peers"></a>
+		<li><b>restore_peers</b><br>
+			Used to restore peers from FHEM keystore.<br>
+			A peer will only be restored if
+			<ul>
+			<li>- last registration not expired</li>
+			<li>- peer not registered already</li>
+			</ul><br>
+			Will be called automatically on FHEM start.
+		</li>
+		<br>
+		<a id="MiniSIP-set-sendmsg"></a>
+		<li><b>sendmsg</b><br>
+			Will be <br>
+			documented later.
+		</li>
+		<br/>
+  </ul>
+  <br>
+
+  <a id="MiniSIP-get"></a>
+  <b>Get</b>
+  <br><br>
+  <ul>
+		<a id="MiniSIP-get-peer"></a>
+		<li><b>peer &lt;peerName&gt;</b><br>
+			Returns peer data for a given peer if the peer exists.
+		</li>
+		<br>
+		<a id="MiniSIP-get-peers"></a>
+		<li><b>peers [table|json]</b><br>
+			Returns the list of all registered peers.
+		</li>
+  </ul>
+  <br>
+
+  <a id="MiniSIP-attr"></a>
+  <b>Attributes</b>
+  <br><br>
+  <ul>
+    <li><a href="#disable">disable</a></li><br>
+    <li><a href="#disabledForIntervals">disabledForIntervals</a></li><br>
+
+    <a id="MiniSIP-attr-logFullMessage"></a>
+    <li><b>logFullMessage</b> 0|1<br>
+      Decides wether the full SIP message will be logged,<br>
+      otherwise only the infoline will be logged.<br>
+      Default: 0 (short message)<br>
+      <br>
+      Note: logging will use loglevel 4 except for errors.
+    </li>
+    <br>
+    <a id="MiniSIP-attr-showFullMessage"></a>
+    <li><b>showFullMessage</b> 0|1<br>
+      Decides wether the full SIP message will be shown in reading,<br>
+      otherwise only the infoline will be stored in a reading.<br>
+      Default: 0 (short message)
+    </li>
+    <br>
+    <a id="MiniSIP-attr-parseFn"></a>
+    <li><b>parseFn</b><br>
+      Defines the name of an alternative function used to parse<br>
+      incoming MESSAGE requests. This function should return <br>
+      a valid value for the reading "input".<br>
+      <br>
+      <b>The attribut value should contain a package name!</b><br>
+      e.g. a function named 'sipParser' in 99_myUtils.pm should be given as<br>
+      <br>
+      <code>attr &lt;device&gt; parseFn main::sipParser</code><br>
+    </li>
+    <br>
+  </ul>
+  <br>
+
+  <b>Readings</b>
+  <br><br>
+  <ul>
+    <li><b>input</b><br>
+      Contains the input value found in either INVITE or MESSAGE request.
+    </li><br>
+    <li><b>state</b>
+    <br>
+      Contains the input value found in either INVITE or MESSAGE request.
+    </li>
+    <br>
+    <li><b>REGISTER | INVITE | MESSAGE | SUBSCRIBE | BYE</b><br>
+      Contains the SIP message from corresponding request either<br>
+      in short or full format, depending on attr 'showFullMessage'.<br>
+    </li>
+    <br>
+  </ul>
+  <br>
+
+</ul>
+
+=end html
+=cut
+
---
+++ fhem/lib/FHEM/Core/MiniSIP.pm
@@ -0,0 +1,287 @@
+#  $Id$
+
+################################################################
+#
+#  Copyright notice
+#
+#  (c) 2026 - today
+#  Copyright: betateilchen (betateilchen dot quantentunnel dot de)
+#  All rights reserved
+#
+#  This program is part of FHEM; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License V2.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+#  See the GNU General Public License V2 for more details.
+#
+################################################################
+
+package FHEM::Core::MiniSIP;
+
+use strict;
+use warnings;
+use MIME::Base64;
+use Data::Dumper;
+use Socket;
+use IO::Socket::INET;
+
+use Net::SIP;
+use Net::SIP::Packet;
+use Net::SIP::Request;
+use Net::SIP::Response;
+
+use FHEM::MiniSIP::Utils qw(:all);
+
+use Exporter ('import');
+our @EXPORT_OK = qw(sendmsg);
+our %EXPORT_TAGS = (all => [@EXPORT_OK]);
+
+use GPUtils qw(GP_Import);
+# Import from main context
+BEGIN {
+	GP_Import(
+	 qw(readingFnAttributes
+	    data
+	    defs
+	    modules
+	    snom
+			selectlist
+		  AttrVal
+			Debug
+      FileWrite
+      IsDisabled
+		  Log3
+      contains_string
+      readingsSingleUpdate
+      setKeyValue
+      toJSON
+      )
+	);
+}
+
+my $p = __PACKAGE__;
+$::data{modules}{version}{$p} = '$Id$';
+
+sub Define {
+  my ($hash, $a, $h) = @_; #parseParams
+  my $name = $a->[0];
+  return "Only one device with TYPE=MiniSIP can be defined" 
+      if($modules{MiniSIP}{inUse});
+
+  $hash->{server}->{port} = $h->{port};
+  $hash->{server}->{from} = $h->{from};
+  $hash->{server}->{local} = "<sip:$h->{from}>";
+  $hash->{server}->{proto} = 'udp';
+
+  my $sock = IO::Socket::INET->new(
+    LocalPort => $hash->{server}->{port},
+    Proto     => $hash->{server}->{proto},
+    Reuse     => 1,
+ #   Blocking  => 0
+  );
+  return "$name: $!" unless $sock;
+  $hash->{SOCK} = $sock;
+  my $fh = $sock->fileno();
+  $hash->{FD} = $fh;
+  $selectlist{$fh} = $hash;
+
+  #FHEM::MiniSIP::Utils::restore_peers($hash);
+  
+  readingsSingleUpdate($hash,'state','initialized',1);
+  $modules{MiniSIP}{inUse} = 1;
+  return undef;
+}
+
+sub Read {
+  my ($hash) = @_;
+  return if(IsDisabled($hash->{NAME}));
+  my $sock = $hash->{SOCK};
+  my $res = $sock->recv($hash->{server}->{buf}, 4096);
+  processmsg($hash);
+}
+
+sub Set {
+  my ($hash,$a,$h) = @_;
+  my $name = $hash->{NAME};
+
+  my %cmd = ("sendmsg"      => "",
+             "backup_peers" => ":noArg", 
+             "restore_peers" => ":noArg", 
+            );
+  return ("Unknown argument $a->[1], choose one of ".
+        join(" ", map { "$_$cmd{$_}" } sort keys %cmd))
+    if(!defined($cmd{$a->[1]}));
+
+  if( $a->[1] eq 'sendmsg' )       {
+    my $peer    = $h->{peer};
+    my $type    = $h->{type};
+    my $payload = $h->{msg};
+    my $msg;
+    
+    if ($type eq 'base64') {
+       $payload .= "==";
+       $msg = decode_base64($payload);
+    } elsif ($type eq 'data') {
+       $msg  = $data{$payload};
+    }
+
+    sendmsg($hash,$peer,$msg);
+  }
+  if( $a->[1] eq 'backup_peers' )  { FHEM::MiniSIP::Utils::backup_peers($hash); }
+  if( $a->[1] eq 'restore_peers' ) { FHEM::MiniSIP::Utils::restore_peers($hash); }
+
+  return undef;
+}
+
+sub Get {
+  my ($hash,$a,$h) = @_;
+  my $name = $hash->{NAME};
+  my %cmd = ("peer"  => "",
+             "peers" => ":table,json",
+            );
+
+  return ("Unknown argument $a->[1], choose one of ".
+        join(" ", map { "$_$cmd{$_}" } sort keys %cmd))
+    if(!defined($cmd{$a->[1]}));
+
+  if( $a->[1] eq 'peers' ) {
+    if (!havepeer($hash,undef)) {
+      return "no peer registered";
+    } elsif (lc($a->[2]) eq 'table') {
+      return FHEM::MiniSIP::Utils::makeTable($hash,$hash->{peers});
+    } elsif (lc($a->[2]) eq 'json') {
+      return toJSON($hash->{peers});
+    }
+  }
+  if( $a->[1] eq 'peer' ) {
+    return "no peer given" unless $a->[2];
+    return getpeer($hash,$a->[2]);
+  }
+}
+
+sub Undef {
+  my ($hash, $arg) = @_;
+  my $sock = $hash->{SOCK};
+  if ($sock) {
+    close($sock);
+  }
+  delete $selectlist{$hash->{FD}} if defined $hash->{FD};
+  delete $hash->{peer};
+  delete $hash->{server};
+  delete $modules{MiniSIP}{inUse};
+  return undef;
+}
+
+sub Delete {
+  my ($hash, $arg) = @_;
+  my $name = $hash->{NAME};
+  setKeyValue($name,undef);
+  return Undef($hash,$arg);
+}
+
+sub Shutdown {
+  my ($hash) = @_;
+  _log3($hash,4,"ShutdownFn called");
+  FHEM::MiniSIP::Utils::backup_peers($hash);
+}
+
+###------------------------------------------------------------------
+
+sub sendmsg {
+  my ($hash,$peer,$msg) = @_;
+  my $name = $hash->{NAME};
+
+#  my $count = scalar keys %{$hash->{peers}};
+#  if (!$count) {
+#    _log3($hash,1,"no peer registered");
+#    return;
+#  } elsif (!defined($hash->{peers}->{$peer})) {
+#    _log3($hash,1,"$name: unknown peer >$peer<");
+#    return;
+#  }
+
+  if (!havepeer($hash,undef)) {
+    return _log3($hash,1,"no peer registered");
+#    return;
+  } elsif (!havepeer($hash,$peer)) {
+    return _log3($hash,1,"$name: unknown peer >$peer<");
+#    return;
+  }
+
+
+  (AttrVal($name,'logFullMessage',0))?  
+    _log3($hash,4,"out to $peer:\n$msg"):
+    _log3($hash,4,"out to $peer: ".(split(/\n/,$msg))[0]);
+  
+  (AttrVal($name,'showFullMessage',0))?  
+     readingsSingleUpdate($hash, "lastMsgOut", $msg, 0):
+     readingsSingleUpdate($hash, "lastMsgOut", (split(/\n/,$msg))[0], 1);
+
+  my $sock = new IO::Socket::INET (
+    PeerAddr => $hash->{peers}->{$peer}->{peer_ip},
+    PeerPort => $hash->{peers}->{$peer}->{peer_port},
+    Proto    => $hash->{server}->{proto});
+
+  if($sock) {
+    print $sock $msg;
+    close($sock);
+  } else {
+    _log3($hash,1,"$!");
+  }
+}
+
+sub processmsg {
+  my ($hash)  = @_;
+  my $name = $hash->{NAME};
+  
+  my $pkt     = eval { Net::SIP::Packet->new( $hash->{server}->{buf} ) };
+
+  (AttrVal($name,'logFullMessage',0))?
+    _log3($hash,4,"in:\n".$pkt->as_string):
+    _log3($hash,4,"in: ".(split(/\n/,$pkt->as_string))[0]);
+
+  #my $method  = $pkt->method(); # not working as expected
+    
+  my $method =  $pkt->as_string;
+  $method    =~ s/^([^ ]*) .*$/$1/s;
+
+  if ($method ne "SIP/2.0") {
+		(AttrVal($name,'showFullMessage',0))?
+			readingsSingleUpdate($hash, lc($method), $pkt->as_string, 0):
+			readingsSingleUpdate($hash, lc($method), (split(/\n/,$pkt->as_string))[0], 1);
+  } elsif ($pkt->is_response) {
+    return; # end processing if 200 OK received as response
+  }
+
+  my ($peer,$ip,$port) = FHEM::MiniSIP::Utils::extract_peer($hash,$pkt,0);
+
+  if ($method eq 'REGISTER') {
+    FHEM::MiniSIP::Utils::savepeer($hash,$pkt);
+  }
+
+  my @known_methods = qw(REGISTER INVITE BYE MESSAGE SUBSCRIBE);
+  if (contains_string($method,@known_methods)) {
+    my $resp = FHEM::MiniSIP::Utils::build_200_short($hash,$pkt);
+    sendmsg($hash,$peer,$resp->as_string);
+  }
+
+  if ($method eq 'INVITE') {
+    my ($info,$user,$header,$body) = $pkt->as_parts;
+    $user =~ m/sip:([*#\d]+)@/;
+    my $input = $1 // "?";
+    readingsSingleUpdate($hash, "input", $input, 1);
+  }
+
+  if ($method eq 'MESSAGE') {
+    my ($info,$user,$header,$body) = $pkt->as_parts;
+    my $input = FHEM::MiniSIP::Utils::parsemsgbody($hash,$peer,$body);
+    readingsSingleUpdate($hash, "input", $input, 1);
+  }
+  delete $hash->{server}->{buf};
+}
+
+1;
+
+#
