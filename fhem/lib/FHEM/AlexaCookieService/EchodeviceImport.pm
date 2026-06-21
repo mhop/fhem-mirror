1cfc70927bc42c4dc75c0bbf159f27b0eb4c1303
Sidey <Sidey@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2026-06-21 11:17:50 +0000

    EchodeviceImport.pm: Added functionality to save cookie local
    git-svn-id: https://svn.fhem.de/fhem/trunk@31377 2b470e98-0d58-463d-a4d8-8e2adae1ed80

--- fhem/lib/FHEM/AlexaCookieService/EchodeviceImport.pm
+++ fhem/lib/FHEM/AlexaCookieService/EchodeviceImport.pm
@@ -9,11 +9,16 @@ package FHEM::AlexaCookieService::EchodeviceImport;
 use strict;
 use warnings;
 
+use File::Basename qw(dirname);
+use File::Path qw(make_path);
+use File::Spec;
+use JSON::PP qw(decode_json encode_json);
+
 sub export_name_for_hash {
   my ($hash) = @_;
 
   return if ref $hash ne 'HASH';
-  return if !defined $hash->{NR} || $hash->{NR} eq q{};
+  return if !defined $hash->{NR} || $hash->{NR} !~ /\A\d+\z/;
 
   return $hash->{NR} . 'result.json';
 }
@@ -33,9 +38,92 @@ sub validate_target {
   return q[missing echodevice hash] if ref $hash ne 'HASH';
   return q[missing device name] if !$hash->{NAME};
   return q[device is not an echodevice] if ($hash->{TYPE} || q{}) ne 'echodevice';
-  return q[missing internal FHEM NR] if !defined $hash->{NR} || $hash->{NR} eq q{};
+  return q[missing internal FHEM NR] if !defined $hash->{NR} || $hash->{NR} !~ /\A\d+\z/;
 
   return;
+}
+
+sub export_path_for_hash {
+  my ($hash, %args) = @_;
+
+  my $export_name = export_name_for_hash($hash);
+  return if !defined $export_name;
+
+  my $export_dir = $args{export_dir} || $args{base_dir} || $args{dir};
+  return if !defined $export_dir || $export_dir eq q{};
+
+  return File::Spec->catfile($export_dir, $export_name);
+}
+
+sub write_cookie_export {
+  my ($hash, $payload, %args) = @_;
+
+  my $error = validate_target($hash);
+  return $error if $error;
+
+  my $path = export_path_for_hash($hash, %args);
+  return q[missing export dir] if !defined $path;
+
+  my $cookie = _normalize_export_payload($payload);
+  return q[invalid cookie export payload] if ref $cookie ne 'HASH';
+
+  my $dir = dirname($path);
+  if (!-d $dir) {
+    make_path($dir) or return qq[failed to create export dir $dir];
+  }
+
+  my $tmp_path = $path . qq{.tmp.$$};
+  open my $fh, '>', $tmp_path or return qq[failed to open $tmp_path: $!];
+  binmode $fh, ':raw';
+
+  my $json = encode_json($cookie);
+  if (!print {$fh} $json) {
+    my $err = $!;
+    close $fh;
+    unlink $tmp_path;
+    return qq[failed to write $tmp_path: $err];
+  }
+
+  if (!close $fh) {
+    my $err = $!;
+    unlink $tmp_path;
+    return qq[failed to close $tmp_path: $err];
+  }
+
+  if (!rename $tmp_path, $path) {
+    my $err = $!;
+    unlink $tmp_path;
+    return qq[failed to rename $tmp_path to $path: $err];
+  }
+
+  return;
+}
+
+sub write_cookie_export_for_device {
+  my ($name, $payload, %args) = @_;
+
+  return q[missing echodevice name] if !$name;
+
+  no warnings 'once';
+  return write_cookie_export($main::defs{$name}, $payload, %args);
+}
+
+sub write_cookie_export_and_trigger_import {
+  my ($hash, $payload, %args) = @_;
+
+  my $error = write_cookie_export($hash, $payload, %args);
+  return $error if $error;
+
+  return trigger_import($hash, %args);
+}
+
+sub write_cookie_export_and_trigger_import_for_device {
+  my ($name, $payload, %args) = @_;
+
+  return q[missing echodevice name] if !$name;
+
+  no warnings 'once';
+  return write_cookie_export_and_trigger_import($main::defs{$name}, $payload, %args);
 }
 
 sub trigger_import {
@@ -66,6 +154,19 @@ sub trigger_import_for_device {
 
   no warnings 'once';
   return trigger_import($main::defs{$name}, %args);
+}
+
+sub _normalize_export_payload {
+  my ($payload) = @_;
+
+  return $payload if ref $payload eq 'HASH';
+
+  if (!ref $payload && defined $payload && $payload ne q{}) {
+    my $decoded = eval { decode_json($payload) };
+    return $decoded if !$@ && ref $decoded eq 'HASH';
+  }
+
+  return;
 }
 
 1;
